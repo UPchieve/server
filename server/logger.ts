b58a9a0c0aecb361448781a63e8ba89d6aac3f3e
Trey Stevens <treykstevens.tks@gmail.com>
2021-03-23 15:35:59 -0400

    added new logger helpers for logging email jobs

--- server/logger.ts
+++ server/logger.ts
@@ -1,5 +1,13 @@
 import pino from 'pino'
+import { Types } from 'mongoose'
 import config from './config'
+import { Jobs } from './worker/jobs'
+
+export interface LogEmailJob {
+  job: Jobs
+  userId: Types.ObjectId | string
+  error?: Error
+}
 
 function newLogger() {
   let logger
@@ -22,5 +30,10 @@ function newLogger() {
 
 const logger = newLogger()
 
+export const logEmailJobSent = ({ job, userId }: LogEmailJob) =>
+  logger.info(`Sent ${job} to user ${userId}`)
+
+export const logEmailJobError = ({ job, userId, error }: LogEmailJob) =>
+  logger.error(`Failed to send ${job} to user ${userId}: ${error}`)
+
 export default logger
-module.exports = logger
