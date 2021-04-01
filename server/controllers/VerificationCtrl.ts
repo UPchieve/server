93212fd18a9f1945bc101993e178954b9b3007dc
Trey Stevens <treykstevens.tks@gmail.com>
2021-04-01 14:43:03 -0400

    added type annotations to verificationctrl

--- server/controllers/VerificationCtrl.ts
+++ server/controllers/VerificationCtrl.ts
@@ -1,44 +1,59 @@
-const StudentService = require('../services/StudentService')
-const TwilioService = require('../services/twilio')
-const { VERIFICATION_METHOD } = require('../constants')
+import UserService from '../services/UserService'
+import TwilioService from '../services/twilio'
+import { VERIFICATION_METHOD } from '../constants'
 
-module.exports = {
-  initiateStudentVerification: async ({
-    firstName,
+export interface InitiateVerificationOptions {
+  sendTo: string
+  verificationMethod: VERIFICATION_METHOD
+  firstName: string
+}
+
+export interface ConfirmVerificationOptions {
+  userId: string
+  sendTo: string
+  verificationMethod: VERIFICATION_METHOD
+  verificationCode: string
+}
+
+export const initiateVerification = ({
+  firstName,
+  sendTo,
+  verificationMethod
+}: InitiateVerificationOptions) =>
+  TwilioService.sendStudentVerification({
     sendTo,
-    verificationMethod
-  }) => {
-    return TwilioService.sendStudentVerification({
+    verificationMethod,
+    firstName
+  })
+
+export const confirmVerification = async ({
+  userId,
+  verificationCode,
+  sendTo,
+  verificationMethod
+}: ConfirmVerificationOptions) => {
+  const isPhoneVerification = verificationMethod === VERIFICATION_METHOD.SMS
+  try {
+    const verificationResult = await TwilioService.confirmStudentVerification(
       sendTo,
-      verificationMethod,
-      firstName
-    })
-  },
-
-  confirmStudentVerification: async ({
-    userId,
-    verificationCode,
-    sendTo,
-    verificationMethod
-  }) => {
-    const isPhoneVerification = verificationMethod === VERIFICATION_METHOD.SMS
-    try {
-      const verificationResult = await TwilioService.confirmStudentVerification(
-        sendTo,
-        verificationCode
-      )
-      const isVerified = verificationResult.valid
-      if (isVerified) {
-        const update = { verified: true }
-        if (isPhoneVerification) {
-          update.verifiedPhone = true
-          update.phone = sendTo
-        } else update.verifiedEmail = true
-        await StudentService.updateStudent({ _id: userId }, update)
-      }
-      return isVerified
-    } catch (error) {
-      throw error
+      verificationCode
+    )
+    const isVerified = verificationResult.valid
+    if (isVerified) {
+      const update: {
+        verified: boolean
+        phone?: string
+        verifiedPhone?: boolean
+        verifiedEmail?: boolean
+      } = { verified: true }
+      if (isPhoneVerification) {
+        update.verifiedPhone = true
+        update.phone = sendTo
+      } else update.verifiedEmail = true
+      await UserService.updateUser({ _id: userId }, update)
     }
+    return isVerified
+  } catch (error) {
+    throw error
   }
 }
