3fab4df494abc0d1f8e3e07933321676ea196b9c
Dave Sudia <dave.sudia@upchieve.org>
2021-05-19 10:00:40 -0600

    pack build with paketo fails on a missing css config in dependency

--- bin/pack_build.sh
+++ bin/pack_build.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/env bash
 
 pack build "registry.gitlab.com/upchieve/subway/local:latest" \
+  --clear-cache \
   --builder paketobuildpacks/builder:full \
   --buildpack registry.gitlab.com/upchieve/doppler-buildpack \
   --buildpack gcr.io/paketo-buildpacks/nodejs \
-  --buildpack gcr.io/paketo-buildpacks/procfile
+  --buildpack gcr.io/paketo-buildpacks/procfile \
+  --env "BP_NODE_OPTIMIZE_MEMORY=true" \
+  --env "NODE_ENV=development"
