2786ea4c2c2a70aa7cef002bb279f8e9d0c8f32a
Fern Sanchez <fern.sanchez@upchieve.org>
2021-06-16 20:34:15 -0400

    Basically everything except a cell unit test is working

---
+++ tests/unit/components/Cell.spec.js
@@ -0,0 +1,33 @@
+import { shallowMount } from '@vue/test-utils';
+
+import Cell from '../../../src/components/AvailabilityGrid/Cell.vue'
+
+//👇 Imports a specific story for the test
+import {
+  Unselectable,
+  Selectable_Unselected,
+  Selectable_Selected,
+} from '../../../src/stories/Cell.stories';
+
+it('renders the cell in the unselectable state', () => {
+  const wrapper = shallowMount(Cell, {
+    propsData: Unselectable.args,
+  });
+  expect(wrapper.classes()).toContain('Cell-unselectable');
+});
+
+it('renders the cell in the selectable + unselected state', () => {
+  const wrapper = shallowMount(Cell, {
+    propsData: Selectable_Unselected.args,
+  });
+  expect(wrapper.classes()).toContain('Cell-selectable');
+});
+
+it('renders the cell in the selectable + selected state', () => {
+  const wrapper = shallowMount(Cell, {
+    propsData: Selectable_Selected.args,
+  });
+  expect(wrapper.classes()).toContain('Cell-selectable--selected');
+});
+
+// TODO: test flagged state via query selector for ClockIcon component?
