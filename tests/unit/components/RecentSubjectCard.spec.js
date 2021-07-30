5fa2baf5cb652edc17755bdb953741fca0eadfcc
Saumyaa Mehra <saumyaa.mehra@gmail.com>
2021-07-30 16:50:19 -0400

    added comments for unit tests - to be picked up next sprint

--- tests/unit/components/RecentSubjectCard.spec.js
+++ tests/unit/components/RecentSubjectCard.spec.js
@@ -5,6 +5,8 @@ import RecentSubjectCard from '../../../src/views/DashboardView/StudentDashboard
 //👇 Imports a specific story for the test
 import { PrimaryButton, SecondaryButton } from '../../../src/stories/RecentSubjectCard.stories';
 
+//test for default recent subject card -> this test fails
+// @todo: figure out why checking for svg object is failing when it exists; unit testing for component tags?
 it('renders recent subject card', () =>{
   const wrapper = shallowMount(RecentSubjectCard, {
     propsData: PrimaryButton.args,
@@ -12,12 +14,12 @@ it('renders recent subject card', () =>{
   expect(wrapper.is(RecentSubjectCard));
   expect(wrapper.props('title')).toBe('Algebra 1');
   expect(wrapper.find("#SubjectCard-desktop-column").exists()).toBeTruthy();
-  //expect(wrapper.find('svg').exists()).toBeTruthy();
   expect(wrapper.find('svg')).toBe('MathSVG');
   expect(wrapper.find('ArrowIcon').exists()).toBeTruthy();
   expect(wrapper.find('ArrowIcon').isVisible()).toBeTruthy();
 });
 
+//test for disabled recent subject card -> this test passes
 it('renders the recent subject button in the disabled state', () => {
   const wrapper = shallowMount(RecentSubjectCard, {
     propsData: SecondaryButton.args,
