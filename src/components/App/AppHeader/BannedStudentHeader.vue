af1d7038bf22b6c69abaf30be73e917566b3dc4c
Saumyaa Mehra <saumyaa.mehra@gmail.com>
2021-08-18 14:07:19 -0400

    top banned status header attempt

---
+++ src/components/App/AppHeader/BannedStudentHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="studentIsBanned" class="top-banner-notice">
+      Your account is under review
+    </div>
+</template>
+
+<script>
+//import { defineComponent } from '@vue/composition-api'
+
+// export default({
+//   name:'banned-student-header',
+//   computed() {
+//     ...mapState({
+//       user: state => state.user.user
+//     })
+//      studentIsBanned(){
+//       return this.user.isBanned;
+//     }
+//   }
+// })
+// </script>
+
+// <style lang="scss" scoped>
+
+// .top-banner-notifice{
+//   display: flex;
+//   background-color: #8B939F;
+//   height: 64px;
+//   @include breakpoint-above('medium') {
+//     display: inline-flex;
+//     min-width: 100%;
+//     //padding: 40px;
+//   }
+// }
+// </style>
