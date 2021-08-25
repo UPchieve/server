19194f93b281365a3b835e2101ede93b5a0e3f9d
Trey Stevens <treykstevens.tks@gmail.com>
2021-08-25 11:44:46 -0400

    create whiteboard pub sub clients

--- server/services/RedisService.ts
+++ server/services/RedisService.ts
@@ -6,3 +6,7 @@ export const redisClient = new Redis(config.redisConnectionString)
 export const socketIoPubClient = new Redis(config.redisConnectionString)
 
 export const socketIoSubClient = new Redis(config.redisConnectionString)
+
+export const whiteboardPubClient = new Redis(config.redisConnectionString)
+
+export const whiteboardSubClient = new Redis(config.redisConnectionString)
