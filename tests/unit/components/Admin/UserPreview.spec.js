d5f4c691aa0829970b3f0a799d563ad4e9b152a9
Saumyaa Mehra <saumyaa.mehra@gmail.com>
2021-09-14 15:23:19 -0400

    testing layout for user preview

--- tests/unit/components/Admin/UserPreview.spec.js
+++ tests/unit/components/Admin/UserPreview.spec.js
@@ -1,10 +1,28 @@
-import { shallowMount } from '@vue/test-utils';
-
-import UserPreview from '../../../src/components/Admin/UserPreview'
+import UserPreview from '@/components/Admin/UserPreview'
+import { mount } from "@vue/test-utils";
 
 describe("UserPreview", ()=> {
   it("layout", () => {
-    const wrapper = shallowMount(UserPreview);
+    const wrapper = mount(UserPreview);
+    expect(wrapper.classes("user-preview")).toBe(true);
+    expect(wrapper.find(".user-preview__icon").exists()).toBe(true);
+    expect(wrapper.find(".user-preview__middle").exists()).toBe(true);
+    expect(wrapper.find(".user-preview__title").exists()).toBe(true);
+    expect(wrapper.find(".user-preview__subtitle").exists()).toBe(true);
+    expect(wrapper.find(".user-preview__right").exists()).toBe(true);
   });
 
-})
+  // it("renders the pastSessions correctly", () => {
+  //   const wrapper = mount(UserPreview, {
+  //     propsData: {
+  //       user: Object
+  //     }
+  //   })
+  //   user.pastSessions = 10;
+  //  // const pastSessions = user.pastSessions;
+  //   //expect(wrapper.find(".user-preview__right")).toBe("true");
+  //   expect(wrapper.text()).toBe("10 sessions")
+
+  // });
+
+});
