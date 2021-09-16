a97ffc6efeaa35fdd41d1afea417d24af833f24c
Fern Sanchez <fernando.sanchez@upchieve.org>
2021-09-16 01:40:36 +0000

    Removedu nnecessary packages form class-transformer

--- server/services/listeners/index.ts
+++ server/services/listeners/index.ts
@@ -1,8 +1,5 @@
 import { listeners as SessionServiceListeners } from './SessionServiceListeners'
 import { listeners as USMServiceListeners } from './USMServiceListeners'
-// Imports required by class-transformer
-import 'reflect-metadata'
-import 'es6-shim'
 
 export function registerListeners() {
   SessionServiceListeners()
