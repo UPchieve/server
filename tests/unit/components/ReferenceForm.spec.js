e1466e47e21457c0989f1e4bd47ae113d0b2b15f
Saumyaa Mehra <saumyaa.mehra@gmail.com>
2021-09-18 21:27:07 -0400

    more tests to reference form

--- tests/unit/components/ReferenceForm.spec.js
+++ tests/unit/components/ReferenceForm.spec.js
@@ -1,29 +1,72 @@
 import ReferenceForm from '@/components/ReferenceForm';
-import { shallowMount } from '@vue/test-utils';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Vuex from "vuex";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
 const getWrapper = (options = {}) => {
   options = {
     didSubmit: true,
+    isNoLongerReference: false,
+    isAdminReview: false,
+    affiliation: '',
+    relationshipLength: '',
+    rejectionReason: '',
+    additionalInfo: '',
     ...options
   };
 
   return shallowMount(ReferenceForm, {
+    localVue,
     propsData: {
-      didSubmit: options.didSubmit
+      isAdminReview: options.isAdminReview
+    },
+    data() {
+      return {
+        didSubmit: options.didSubmit,
+        isNoLongerReference: options.isNoLongerReference,
+        affiliation: options.affiliation,
+        relationshipLength: options.relationshipLength,
+        rejectionReason: options.rejectionReason,
+        additionalInfo: options.additionalInfo
+      }    
     }
   });
 };
 
 describe("ReferenceForm", () => {
    it("layout", () => {
-      const reference = getWrapper( { didSubmit: true });
-      const referenceMessage = reference.find('.helper-message');
+      const wrapper = getWrapper({ });
+      const referenceMessage = wrapper.find('.helper-message');
       expect(referenceMessage.exists()).toBe(true);
       expect(referenceMessage.text()).toBe('Reference submitted!');
 
-      // const noReference = getWrapper( { didSubmit: false });
-      // const noReferenceMessage = noReference.find('.helper-message');
-      // expect(noReferenceMessage.exists()).toBe(true);
-      // expect(noReferenceMessage.text()).toBe("Sorry, you've been removed as a reference.");
+  //    const container = getWrapper({})
+  //    container.setProps({isAdminReview: true})
+  //    console.log(contain)
+  //  // expect(container.find('.admin-review').exists()).toBe(true);
+  //  //  expect(container.find('.heading-legend').exists()).toBe(true);
+     
    })
+
+   it("renders reference message", () => {
+    const wrapper = getWrapper({});
+    const msg = wrapper.find('.helper-message');
+    expect(msg.exists()).toBe(true);
+    expect(msg.text()).toBe('Reference submitted!');
+   });
+
+   it("renders not a reference message", () => {
+    const wrapper = getWrapper({ 
+      didSubmit: false, 
+      isNoLongerReference: true
+    });
+    const msg = wrapper.find('.helper-message');
+     expect(msg.exists()).toBe(true);
+  //   console.log(wrapper.vm.didSubmit);
+     //why failing here?
+   expect(msg.text()).toBe("Sorry, you've been removed as a reference.");
+   });
+
 });
