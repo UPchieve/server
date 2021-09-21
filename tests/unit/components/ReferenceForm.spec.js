7f8851e739da8532c9655eabd94cf006b8be5d0c
Saumyaa Mehra <saumyaa.mehra@gmail.com>
2021-09-21 18:19:08 -0400

    mocking mapGetters

--- tests/unit/components/ReferenceForm.spec.js
+++ tests/unit/components/ReferenceForm.spec.js
@@ -1,11 +1,22 @@
 import ReferenceForm from '@/components/ReferenceForm';
 import { createLocalVue, mount } from '@vue/test-utils';
+import appModule from "@/store/modules/app";
 import Vuex from "vuex";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
-const getWrapper = (options = {}) => {
+const getWrapper = (mobileMode = false, options = {}) => {
+  const store = new Vuex.Store({
+    modules: {
+      app: {
+        ...appModule,
+        getters: {
+          mobileMode: () => mobileMode
+        }
+      }
+    }});
+
   options = {
     didSubmit: false,
     isNoLongerReference: false,
@@ -19,6 +30,7 @@ const getWrapper = (options = {}) => {
 
   return mount(ReferenceForm, {
     localVue,
+    store,
     propsData: {
       isAdminReview: options.isAdminReview
     },
@@ -45,24 +57,24 @@ describe("ReferenceForm", () => {
     expect(wrapper.isVueInstance()).toBe(true);
   });
 
-   it("layout", () => {
-      const wrapper = getWrapper({didSubmit: true });
-      const referenceMessage = wrapper.find('.helper-message');
-      expect(referenceMessage.exists()).toBe(true);
-      expect(referenceMessage.text()).toBe('Reference submitted!');
+  //  it("layout", () => {
+  //     const wrapper = getWrapper({didSubmit: true });
+  //     const referenceMessage = wrapper.find('.helper-message');
+  //     expect(referenceMessage.exists()).toBe(true);
+  //     expect(referenceMessage.text()).toBe('Reference submitted!');
 
-  //    const container = getWrapper({})
-  //    await container.setProps({isAdminReview: true})
-  //    console.log(contain)
-  //  // expect(container.find('.admin-review').exists()).toBe(true);
-  //  //  expect(container.find('.heading-legend').exists()).toBe(true);
+  // //    const container = getWrapper({})
+  // //    await container.setProps({isAdminReview: true})
+  // //    console.log(contain)
+  // //  // expect(container.find('.admin-review').exists()).toBe(true);
+  // //  //  expect(container.find('.heading-legend').exists()).toBe(true);
      
-   })
+  //  });
 
    it("renders reference message", () => {
-    const wrapper = getWrapper({ didSubmit: true});
+    const wrapper = getWrapper({ didSubmit: true });
     const msg = wrapper.find('.helper-message');
-    console.log("test 1: didSubmit" + wrapper.vm.didSubmit); //true, should be true
+  //  console.log("test 1: didSubmit" + wrapper.vm.didSubmit); //true, should be true
     expect(msg.exists()).toBe(true);
     expect(msg.text()).toBe('Reference submitted!');
    });
@@ -72,21 +84,27 @@ describe("ReferenceForm", () => {
       isNoLongerReference: true
     });
     // console.log(wrapper.vm.didSubmit); //currently: 
-     console.log("test2: noLongerReference" + wrapper.vm.isNoLongerReference); //true, should be true
-     console.log("test2: didSubmit " + wrapper.vm.didSubmit); //false, should be false
-    const msg = wrapper.find('.helper-message');
-      expect(msg.exists()).toBe(true);
+     console.log("test2: isNoLongerReference, should be true :" + wrapper.vm.isNoLongerReference); //true, should be true
+     console.log(wrapper.html());
+     //console.log("test2: didSubmit " + wrapper.vm.didSubmit); //false, should be false
+      const msg = wrapper.find('.helper-message');
+      expect(msg.exists()).toBe(true); 
       expect(msg.text()).toBe("Sorry, you've been removed as a reference.");
       // @todo: access classes inside wrapper? why is wrapper.classes() = []
       //
    });
 
-   it("volunteer is on reference form for the first time", () => {
-    const wrapper = getWrapper({});
-    console.log(wrapper.classes());
-    console.log(wrapper.vm.didSubmit); //false, should be false
+   it("reference form", () => {
+    const wrapper = getWrapper({ 
+      isNoLongerReference: false 
+    });
+    // console.log(wrapper.html());
+    // console.log(wrapper.classes());
+    //console.log(wrapper.vm.didSubmit); //false, should be false
+   
+    
     console.log("test3: isNoLongerRef: " + wrapper.vm.isNoLongerReference); //true, should be false
-    expect(wrapper.classes('questions-container')).toBe(true);
+     expect(wrapper.classes()).toContain('questions-container');
    
    });
 
