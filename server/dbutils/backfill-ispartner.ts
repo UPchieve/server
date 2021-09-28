1cf587716701a4ef91f4166744cf12d0eda2db90
Saumyaa Mehra <saumyaa.mehra@gmail.com>
2021-09-28 13:07:32 -0400

    updated backfill script

--- server/dbutils/backfill-ispartner.ts
+++ server/dbutils/backfill-ispartner.ts
@@ -1,25 +1,60 @@
 import mongoose from 'mongoose'
-import User from '../models/User'
 import * as db from '../db'
+import SchoolModel from '../models/School'
 
 // Run:
 // npx ts-node server/dbutils/backfill-ispartner.ts
-const main = async () => {
+async function upgrade(): Promise<void> {
+  let exitCode = 0
   try {
     await db.connect()
    
-    const result = await User.updateMany(
-      //should we filter by if the user is a student?
-//      { isStudent: true },
-      { $set: { 'school.isPartner': false } },
-      { strict: false }
+    const result = await SchoolModel.updateMany(
+      { isPartner: {
+        $exists: false
+      }},
+      { $set: { isPartner: false } }
     )
     console.log(result)
   } catch (error) {
     console.error(error)
-  }
-
-  mongoose.disconnect()
+    exitCode = 1
+  } finally {
+    mongoose.disconnect()
+    process.exit(exitCode)
+  } 
 }
 
-main()
+async function downgrade(): Promise<void> {
+  let exitCode = 0;
+  try {
+    await db.connect();
+    const result = await SchoolModel.updateMany(
+      { isPartner: {
+        $exists: true
+      }},
+      {
+        $unset: {
+          isPartner: ''
+        }
+      }
+    );
+    console.log(result);
+
+  } catch (error) {
+    console.error(error);
+    exitCode = 1;
+  } finally {
+    mongoose.disconnect()
+    process.exit(exitCode)
+  } 
+}
+
+// Run:
+// DOWNGRADE = true npx ts-node server/dbutils/backfill-ispartner.ts
+if(process.env.DOWNGRADE){
+  downgrade();
+}
+else {
+  upgrade();
+}
