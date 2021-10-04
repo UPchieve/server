53a6a68fbd1f52ac2514350eb96f248154569826
Fern Sanchez <fernando.sanchez@upchieve.org>
2021-10-04 15:50:08 +0000

    Small fixes plus explanation comment

--- server/router/app.ts
+++ server/router/app.ts
@@ -2,6 +2,12 @@ import { Request } from 'express'
 import { Student } from '../models/Student'
 import { Volunteer } from '../models/Volunteer'
 
+/**
+ * LoadedRequest represents an authenticated request that has gone through
+ * passport middleware
+ *
+ * TODO: find a way to extends the Express namespace to get the typing built into default express request so we don't need to import this everywhere
+ */
 export interface LoadedRequest extends Request {
   user?: Student | Volunteer
   login?: Function
