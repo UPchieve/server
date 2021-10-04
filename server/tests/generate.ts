e567306137b1b8e6cca9940cb4425fa4483b5925
Saumyaa Mehra <saumyaa.mehra@gmail.com>
2021-10-04 12:40:30 -0400

    added currentGrade to buildStudent

--- server/tests/generate.ts
+++ server/tests/generate.ts
@@ -224,6 +224,7 @@ export const buildStudent = (overrides = {}): Student => {
     zipCode: '11201',
     studentPartnerOrg: 'example',
     partnerSite: '',
+    // currentGrade: '',
     ...overrides
   }
 
