a4bda3177d839acbef097f273fd4343adc68069d
Fern Sanchez <fernando.sanchez@upchieve.org>
2021-10-07 16:07:57 +0000

    frontend lintfix and typeerror

--- src/stories/storybook-router.js
+++ src/stories/storybook-router.js
@@ -1,14 +1,14 @@
-import VueRouter from 'vue-router';
-import Vue from 'vue';
+import VueRouter from 'vue-router'
+import Vue from 'vue'
 
-import { action } from '@storybook/addon-actions';
+import { action } from '@storybook/addon-actions'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const storyRouterDecorator = (links = {}, routerProps = {}) => {
   return story => {
-    const router = new VueRouter(routerProps);
-    router.replace(routerProps.initialEntry ? routerProps.initialEntry : '/');
+    const router = new VueRouter(routerProps)
+    router.replace(routerProps.initialEntry ? routerProps.initialEntry : '/')
 
     const getLocation = location => {
       // The location can be a simple string if you are using directly one of the
@@ -16,12 +16,12 @@ const storyRouterDecorator = (links = {}, routerProps = {}) => {
       // or it can be an object, having the name or the path depending if you
       // are using named routes or not.
       if (typeof location === 'object') {
-        return location.path ? location.path : `name: ${location.name}`;
+        return location.path ? location.path : `name: ${location.name}`
       }
-      return location;
-    };
+      return location
+    }
 
-    let replaced;
+    let replaced
 
     // We want to log every action performed on the navigation router with the only
     // exception of links replaced with the linkTo callback.
@@ -30,43 +30,43 @@ const storyRouterDecorator = (links = {}, routerProps = {}) => {
     // Replacing the original push / replace router methods workaround the issue
     // with the assumption that the afterEach global guard is called from those
     // methods.
-    const originalPush = router.push.bind(router);
+    const originalPush = router.push.bind(router)
 
     router.push = (location, success, abort) => {
-      replaced = false;
-      originalPush(location, success, abort);
+      replaced = false
+      originalPush(location, success, abort)
 
       if (!replaced) {
-        action('PUSH')(getLocation(location));
+        action('PUSH')(getLocation(location))
       }
-    };
+    }
 
-    const originalReplace = router.replace.bind(router);
+    const originalReplace = router.replace.bind(router)
 
     router.replace = (location, success, abort) => {
-      replaced = false;
-      originalReplace(location, success, abort);
+      replaced = false
+      originalReplace(location, success, abort)
 
       if (!replaced) {
-        action('REPLACE')(getLocation(location));
+        action('REPLACE')(getLocation(location))
       }
-    };
+    }
 
     if (routerProps.globalBeforeEach) {
-      router.beforeEach(routerProps.globalBeforeEach);
+      router.beforeEach(routerProps.globalBeforeEach)
     }
 
     router.afterEach(to => {
       for (const link in links) {
         if (to.fullPath === link) {
-          links[link](to.fullPath);
-          replaced = true;
-          return;
+          links[link](to.fullPath)
+          replaced = true
+          return
         }
       }
-    });
+    })
 
-    const WrappedComponent = story();
+    const WrappedComponent = story()
     return Vue.extend({
       router,
       components: { WrappedComponent },
@@ -76,10 +76,10 @@ const storyRouterDecorator = (links = {}, routerProps = {}) => {
         // accumulate callbacks called for every route action (in practice
         // this means that without this the action is executed as many
         // times as the VueRouter instance has been created)
-        this.$options.router.afterHooks = [];
-      },
-    });
-  };
-};
+        this.$options.router.afterHooks = []
+      }
+    })
+  }
+}
 
-export default storyRouterDecorator;
+export default storyRouterDecorator
