97e4ca094d9576bc935a807ff6b54d7eda7c171a
Saumyaa Mehra <saumyaa.mehra@gmail.com>
2021-10-12 12:53:18 -0400

    trimming current grade to match the backend

--- src/views/SignupView/StudentForm.vue
+++ src/views/SignupView/StudentForm.vue
@@ -535,7 +535,12 @@ export default {
   computed: {
     ...mapState({
       isMobileApp: state => state.app.isMobileApp
-    })
+    }),
+    trimCurrentGrade() {
+      // extracting the first word out of the gradeLevels
+      // example: "8th grade" --> "8th"
+      return this.profile.currentGrade.split(" ")[0]
+    }
   },
   watch: {
     'eligibility.email': function(currentValue, oldValue) {
@@ -687,7 +692,7 @@ export default {
         zipCode: this.eligibility.zipCode,
         email: this.eligibility.email,
         referredByCode: window.localStorage.getItem('upcReferredByCode'),
-        currentGrade: this.profile.currentGrade
+        currentGrade: this.trimCurrentGrade 
       })
         .then(async response => {
           const isEligible = response.body.isEligible
@@ -783,7 +788,7 @@ export default {
         lastName: this.profile.lastName,
         highSchoolId: this.eligibility.highSchool.upchieveId,
         zipCode: this.eligibility.zipCode,
-        currentGrade: this.profile.currentGrade,
+        currentGrade: this.trimCurrentGrade,
         referredByCode: window.localStorage.getItem('upcReferredByCode')
       })
         .then(() => {
