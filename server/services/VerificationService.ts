d661735fba3dc5e01ccd7ebe7db4d6861392533e
Fern Sanchez <fernando.sanchez@upchieve.org>
2021-10-18 13:50:49 -0400

    lint fix

--- server/services/VerificationService.ts
+++ server/services/VerificationService.ts
@@ -68,7 +68,9 @@ export async function initiateVerification(data: unknown): Promise<void> {
   if (existingUser && userId !== existingUser._id.toString())
     throw new LookupError(existingUserErrorMessage)
   if (lookupField === 'email' && !existingUser)
-    throw new LookupError('The email address you entered does not match your account email address')
+    throw new LookupError(
+      'The email address you entered does not match your account email address'
+    )
 
   await TwilioService.sendVerification({
     sendTo,
