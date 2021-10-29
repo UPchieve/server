554163b4a9ab7f53cebc7c2be31abe4190fd7bd7
Trey Stevens <treykstevens.tks@gmail.com>
2021-10-29 18:22:02 -0400

    remove logs

--- server/models/Message.ts
+++ server/models/Message.ts
@@ -34,8 +34,6 @@ const messageSchema = new Schema(
 )
 
 messageSchema.virtual('userId').get(function(this: MessageDocument) {
-  console.log('this ,', this)
-  console.log('User', this.user)
   return this.user instanceof Types.ObjectId ? this.user : (this.user as User)._id
 })
 
