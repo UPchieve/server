bacf2061298b110aad646562c63047b8dad3a548
Fern Sanchez <fernando.sanchez@upchieve.org>
2021-10-29 12:51:34 -0400

    Fix parallel tests

--- jest.config.server.js
+++ jest.config.server.js
@@ -3,7 +3,10 @@ const { defaults: tsjPreset } = require('ts-jest/presets')
 module.exports = {
   preset: "@shelf/jest-mongodb",
   setupFiles: [
-    "<rootDir>/server/tests/setup.ts"
+    "<rootDir>/server/tests/setup.ts",
+  ],
+  setupFilesAfterEnv: [
+    "<rootDir>/server/tests/force-gc.ts",
   ],
   watchPathIgnorePatterns: ["globalConfig"],
   roots: ["<rootDir>/server"],
