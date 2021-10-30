1c2b3d7d6d5bb0b999ed68c0eb057d4f7b52ca14
Fern Sanchez <fernando.sanchez@upchieve.org>
2021-10-29 20:12:25 -0400

    Fixed UPchieve101

--- server/services/TrainingCourseService.ts
+++ server/services/TrainingCourseService.ts
@@ -1,54 +1,20 @@
-import cloneDeep from 'lodash'
 import { Volunteer, TrainingCourses } from '../models/Volunteer'
 import { updateVolunteerTrainingById } from '../models/Volunteer/queries'
 import * as TrainingUtils from '../utils/training-courses'
 
 // @note: this type was derived from how the return type is used by the frontend
 // TODO: come back and verify this is the return shape we want
-export interface EditableTrainingCourseData {
-  modules: EditableModule[]
-  courseKey: string
-  quizKey: string
-  progress: number
-  isComplete: boolean
-}
-
-interface EditableModule {
-  moduleKey: string
-  materials: EditableMaterial[]
-}
-
-interface EditableMaterial {
-  materialKey: string
-  isCompleted: boolean
-}
-
-function insertEditableFields(
-  data: TrainingUtils.TrainingCourse
-): EditableTrainingCourseData {
-  const course = cloneDeep(data) as any
-  course.isComplete = false
-  course.progress = 0
-  course.modules.forEach((mod: any) => {
-    mod.materials.forEach((mat: any) => {
-      mat.isCompleted = false
-    })
-  })
-  return course as EditableTrainingCourseData
-}
-
 export function getCourse(
   volunteer: Volunteer,
   courseKey: keyof TrainingCourses
-): EditableTrainingCourseData | undefined {
-  const courseData = TrainingUtils.getCourse(courseKey)
-  if (!courseData) return
+): any {
+  const course: any = TrainingUtils.getCourse(courseKey)
+  if (!course) return
   const courseProgress = volunteer.trainingCourses[courseKey]
-  const course = insertEditableFields(courseData)
   course.isComplete = courseProgress.isComplete
   course.progress = courseProgress.progress
-  course.modules.forEach(mod => {
-    mod.materials.forEach(mat => {
+  course.modules.forEach((mod: any) => {
+    mod.materials.forEach((mat: any) => {
       mat.isCompleted = courseProgress.completedMaterials.includes(
         mat.materialKey
       )
