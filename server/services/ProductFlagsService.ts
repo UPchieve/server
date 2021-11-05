e18ced1f6c916fbbf9c93f9617f9c611b3d999ab
Trey Stevens <treykstevens.tks@gmail.com>
2021-11-05 12:29:36 -0400

    remove product flag service

--- server/services/ProductFlagsService.ts
+++
@@ -1,6 +0,0 @@
-import { Types } from 'mongoose'
-import * as UserProductFlagsRepo from '../models/UserProductFlags/queries'
-
-export async function getPublicUPFByUserId(userId: Types.ObjectId) {
-  return UserProductFlagsRepo.getPublicUPFByUserId(userId)
-}
