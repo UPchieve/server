94532539071be6c4697aef15913b93b4b873bbc1
Trey Stevens <treykstevens.tks@gmail.com>
2021-11-05 13:29:59 -0400

    fix up backfill script

--- server/dbutils/backfill-gates-students.ts
+++ server/dbutils/backfill-gates-students.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 import { GRADES, MATH_SUBJECTS } from '../constants'
 import * as db from '../db'
+import logger from '../logger'
 import StudentModel from '../models/Student'
 import UserProductFlagsModel from '../models/UserProductFlags'
 
@@ -29,10 +30,7 @@ async function upgrade(): Promise<void> {
         },
       },
       {
-        $unwind: {
-          path: '$highSchool',
-          preserveNullAndEmptyArrays: true,
-        },
+        $unwind: '$highSchool',
       },
       {
         $match: {
@@ -48,12 +46,19 @@ async function upgrade(): Promise<void> {
 
     const eligibleStudentIds = eligibleStudents.map(student => student._id)
 
+    logger.info(
+      `Attempting to update gatesQualified for ${eligibleStudentIds.length} students`
+    )
+
     await UserProductFlagsModel.updateMany(
       { user: { $in: eligibleStudentIds } },
       { gatesQualified: true }
     )
+    logger.info(
+      `Successfully updated gatesQualified for ${eligibleStudentIds.length} students`
+    )
   } catch (error) {
-    console.error(error)
+    logger.error(error)
     exitCode = 1
   } finally {
     mongoose.disconnect()
@@ -65,7 +70,7 @@ async function downgrade(): Promise<void> {
   let exitCode = 0
   try {
     await db.connect()
-    const qualifiedStudents = await UserProductFlagsModel.aggregate([
+    const gatesQualifiedFlags = await UserProductFlagsModel.aggregate([
       {
         $match: { gatesQualified: true },
       },
@@ -78,59 +83,65 @@ async function downgrade(): Promise<void> {
         },
       },
       {
-        $unwind: {
-          path: '$user',
-        },
+        $unwind: '$user',
       },
       {
         $lookup: {
           from: 'sessions',
           localField: 'user.pastSessions',
           foreignField: '_id',
-          as: 'pastSessions',
+          as: 'user.pastSessions',
         },
       },
       {
         $project: {
-          pastSessions: 1,
-          user: 1,
+          student: '$user',
         },
       },
     ])
 
     const notQualifiedStudentIds = []
 
-    for (const student of qualifiedStudents) {
+    /**
+     * Previosuly, a Gates-qualified session is defined as a session with the following:
+     *
+     * - Not a school partner student
+     * - Not a nonprofit partner student
+     * - Brand new student, must be first session on the platform
+     * - In 9th grade or 10th grade
+     * - Not reported by time of ending
+     * - Math tutoring session
+     *
+     * We can assume that if they already have a gates qualified flag,
+     * they are not a partner student or from a partner school, and
+     * that they are in 9th or 10th grade. The only checks that need to be
+     * made are that their first session was a math tutoring session, and
+     * that it was not reported
+     */
+    for (const { student } of gatesQualifiedFlags) {
       const firstSession = student.pastSessions[0]
-      /**
-       * Previosuly, a Gates-qualified session is defined as a session with the following:
-       *
-       * - Not a school partner student
-       * - Not a nonprofit partner student
-       * - Brand new student, must be first session on the platform
-       * - In 9th grade or 10th grade
-       * - Not reported by time of ending
-       * - Math tutoring session
-       *
-       * We can assume that if they already have a gates qualified flag,
-       * they are not a partner student or from a partner school, and
-       * that they are in 9th or 10th grade. The only checks that need to be
-       * made are that their first session was a math tutoring session, and 
-       * that it was not reported
-       */
-      if (
+      if (!firstSession) notQualifiedStudentIds.push(student._id)
+      else if (
         firstSession.isReported ||
         !Object.values<string>(MATH_SUBJECTS).includes(firstSession.subTopic)
       )
         notQualifiedStudentIds.push(student._id)
     }
 
+    logger.info(
+      `Attempting to downgrade gatesQualified for ${notQualifiedStudentIds.length} students`
+    )
+
     await UserProductFlagsModel.updateMany(
       { user: { $in: notQualifiedStudentIds } },
       { gatesQualified: false }
     )
+
+    logger.info(
+      `Successfully downgraded gatesQualified for ${notQualifiedStudentIds.length} students`
+    )
   } catch (error) {
-    console.error(error)
+    logger.error(error)
     exitCode = 1
   } finally {
     mongoose.disconnect()
