6d04bb0d1cf255c3ec865d8da2349e319a2832b4
Saumyaa <saumyaa.mehra@gmail.com>
2021-11-17 16:14:50 -0500

    added sessionid on notification model and backfill script

---
+++ server/dbutils/add-sessionid-to-notifications.ts
@@ -0,0 +1,73 @@
+import mongoose from 'mongoose';
+import Notification from '../models/Notification';
+import Session from '../models/Session';
+import * as db from '../db';
+
+// Run:
+// npx ts-node dbutils/add-sessionid-to-notifications.ts
+async function upgrade(): Promise<void> {
+  try {
+    await db.connect();
+    const sessions: any = await Session.find({}).lean().exec();
+
+    for(const session of sessions){
+      if(session.notifications.length>0){
+        const result = await Notification.updateMany(
+          { 
+            _id: 
+            { 
+              $in: session.notifications 
+            }
+          },
+          { 
+            sessionId: session._id 
+          }
+        );
+        console.log('Updated: ', result);
+      } 
+    }
+  } catch (error) {
+      console.error(error);
+  }
+
+  mongoose.disconnect();
+}
+
+// npx ts-node dbutils/add-sessionid-to-notifications.ts
+async function downgrade(): Promise<void> {
+  try {
+    await db.connect();
+    const sessions: any = await Session.find({}).lean().exec();
+
+    for(const session of sessions){
+      if(session.notifications.length>0){
+        const result = await Notification.updateMany(
+          { 
+            _id: 
+            { 
+              $in: session.notifications 
+            }
+          },
+          { 
+            $unset: {
+              sessionId: ''
+            }
+          }
+        );
+        console.log('Updated: ', result);
+      } 
+    }
+  } catch (error) {
+      console.error(error);
+  }
+
+  mongoose.disconnect();
+}
+
+// To downgrade the migration run:
+// DOWNGRADE=true npx ts-node dbutils/add-sessionid-to-notifications.ts
+if (process.env.DOWNGRADE) {
+  downgrade();
+} else {
+  upgrade();
+}
