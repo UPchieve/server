ff07c2b0edb75d909a36ad21cb9dce180a022b9a
Saumyaa <saumyaa.mehra@gmail.com>
2021-11-21 13:41:06 -0500

    added projection and filter for sessions

--- server/dbutils/add-sessionid-to-notifications.ts
+++ server/dbutils/add-sessionid-to-notifications.ts
@@ -8,7 +8,7 @@ import * as db from '../db';
 async function upgrade(): Promise<void> {
   try {
     await db.connect();
-    const sessions: any = await Session.find({}).lean().exec();
+    const sessions: any = await Session.find({'notifications.0': { $exists: true }}).lean().exec();
 
     for(const session of sessions){
       if(session.notifications.length>0){
@@ -20,8 +20,14 @@ async function upgrade(): Promise<void> {
             }
           },
           { 
-            sessionId: session._id 
-          }
+            $set: {
+              sessionId: session._id 
+            },
+            $project: {
+                _id: 1,
+                sessionId: 1
+              }
+          },
         );
         console.log('Updated: ', result);
       } 
