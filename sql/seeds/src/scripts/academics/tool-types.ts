024e533d704745797c8251028423fb270a7859b6
Fern Sanchez <fjorn@Ferns-MacBook-Pro.local>
2021-12-06 11:03:56 -0500

    Deleted old sql files

--- sql/seeds/src/scripts/academics/tool-types.ts
+++
@@ -1,9 +0,0 @@
-import pool from '../../pg-pool'
-import * as db from 'zapatos/db'
-
-export async function toolTypes() {
-    await db.insert('tool_types', [
-        { updated_at: new Date(), created_at: new Date(), name: 'whiteboard' },
-        { updated_at: new Date(), created_at: new Date(), name: 'documenteditor' },
-    ]).run(pool)
-}
