3568cb644504aae1f41498b00a8da134cc611028
Fern Sanchez <fern.sanchez@upchieve.org>
2021-12-13 17:09:47 -0500

    Added student, volunteer, and school test data seeds

--- database/seeds/scripts/testData/schools.ts
+++ database/seeds/scripts/testData/schools.ts
@@ -1,1 +1,24 @@
-// keep
+import pool from '../../pg-pool'
+import * as db from 'zapatos/db'
+import { getDbUlid } from '../utils'
+
+export async function schools() {
+  await db
+    .insert('schools', [
+      {
+        id: getDbUlid(),
+        name: 'test data',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: getDbUlid(),
+        name: 'Legacy Signup High School',
+        created_at: new Date(),
+        updated_at: new Date(),
+        approved: true,
+        partner: true,
+      },
+    ])
+    .run(pool)
+}
---
+++ database/seeds/scripts/testData/students.ts
@@ -0,0 +1,71 @@
+import pool from '../../pg-pool'
+import * as db from 'zapatos/db'
+import { getDbUlid } from '../utils'
+
+export async function students() {
+  const student1 = getDbUlid()
+  const student2 = getDbUlid()
+  const student3 = getDbUlid()
+
+  await db
+    .insert('users', [
+      {
+        id: student1,
+        created_at: new Date(),
+        updated_at: new Date(),
+        email: 'student1@upchieve.org',
+        password:
+          '$2a$10$z.JMHnbX9IubnNZtqI.FOecTPVY1VTU1DJ6AJGIOT/x/OyAtdw3.y',
+        first_name: 'Student',
+        last_name: 'UPchieve',
+        referral_code: 'A',
+        verified: true,
+      },
+      {
+        id: student2,
+        created_at: new Date(),
+        updated_at: new Date(),
+        email: 'student2@upchieve.org',
+        password:
+          '$2a$10$z.JMHnbX9IubnNZtqI.FOecTPVY1VTU1DJ6AJGIOT/x/OyAtdw3.y',
+        first_name: 'Student',
+        last_name: 'UPchieve',
+        referral_code: 'F',
+        verified: true,
+      },
+      {
+        id: student3,
+        created_at: new Date(),
+        updated_at: new Date(),
+        email: 'student3@upchieve.org',
+        password:
+          '$2a$10$z.JMHnbX9IubnNZtqI.FOecTPVY1VTU1DJ6AJGIOT/x/OyAtdw3.y',
+        first_name: 'Student',
+        last_name: 'UPchieve',
+        referral_code: 'G',
+        verified: true,
+        test_user: true,
+      },
+    ])
+    .run(pool)
+
+  await db
+    .insert('student_profiles', [
+      {
+        user_id: student1,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        user_id: student2,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        user_id: student3,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ])
+    .run(pool)
+}
