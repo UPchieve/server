34570d5aeaa5364d4eca1363b82c89bfa3d2a716
Saumyaa <saumyaa.mehra@gmail.com>
2021-12-15 10:58:22 +0530

    script for updating questions and volunteer certifications and subjects

--- server/dbutils/remove-algebra-subject-and-certification.ts
+++ server/dbutils/remove-algebra-subject-and-certification.ts
@@ -1,25 +1,36 @@
 import mongoose from 'mongoose'
-import Volunteer from '../models/Volunteer'
+import Volunteer, { Certifications } from '../models/Volunteer'
 import Question from '../models/Question'
 import * as db from '../db';
 
-function removeAlgebra(volunteer){
-  Volunteer.updateOne(
-    // https://stackoverflow.com/questions/16959099/how-to-remove-array-element-in-mongodb
-  )
-}
-
+// https://stackoverflow.com/questions/16959099/how-to-remove-array-element-in-mongodb
 
 async function upgrade(): Promise<void> {
   try{
     await db.connect();
 
-    const volunteers = Volunteer.find({})
+    // deleting all algebra questions from collection
+    // Model.deleteMany or Model.remove()?
+    const deletedQuestions = Question.deleteMany({ category: 'algebra'})
 
-    for(var volunteer in volunteers){
-      removeAlgebra(volunteer)
-    }
+    const removedCertification = Volunteer.updateMany({}, {
+      $pull: {
+        'certifications': { Certifications : 'algebra' }
+      }
+    })
+
+    const removedSubject = Volunteer.updateMany({}, {
+      $pull: {
+        'subjects': { string: 'algebraTwo-temporary' }
+      }
+    })  
+    
+    console.log('Questions deleted', deletedQuestions);
+    console.log('Volunteers with algebra certification removed', removedCertification);
+    console.log('Volunteers with algebra subject removed', removedSubject);
   } catch (err) {
-
+    console.log(err);
   }
 }
+
+upgrade()
