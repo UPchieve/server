525a873c3fe2c85aaf6f3088b12fbcb99fdae60d
Trey Stevens <treykstevens.tks@gmail.com>
2022-01-13 20:49:26 -0500

    forcibly close remaining connections when receiving SIGTERM

--- server/main.ts
+++ server/main.ts
@@ -8,6 +8,8 @@ import logger from './logger'
 import { registerListeners } from './services/listeners'
 import { setTimeout } from 'timers/promises'
 import { Mongoose } from 'mongoose'
+import { Socket } from 'net'
+import { promisify } from 'util'
 
 async function main() {
   try {
@@ -33,25 +35,49 @@ async function main() {
   const server = app.listen(port, () => {
     logger.info('api server listening on port ' + port)
   })
+  const shutDownSocketServer = promisify(io.close).bind(io)
 
-  process.on('SIGTERM', () => {
+  let connections: Socket[] = []
+
+  server.on('connection', connection => {
+    connections.push(connection)
+    connection.on(
+      'close',
+      () => (connections = connections.filter(curr => curr !== connection))
+    )
+  })
+
+  process.on('SIGTERM', async () => {
     logger.info('SIGTERM signal received')
-    server.close(err => {
+    // immediately stop accepting new connections to the server
+    server.close(async err => {
       if (err) {
         logger.error(err as Error)
         process.exit(1)
       }
       logger.info('api server closed')
 
-      // close the socket server
-      io.close(async () => {
-        logger.info('socket server closed')
+      await shutDownSocketServer()
+      logger.info('socket server closed')
 
-        // allow time for events to finish processing and making db calls before exiting
-        await setTimeout(1000 * 5)
-        await dbConn.disconnect()
-        process.exit(0)
-      })
+      // allow time for events to finish processing and making db calls before exiting
+      await setTimeout(5000)
+      await dbConn.disconnect()
+      process.exit(0)
+    })
+
+    /**
+     *
+     * The API server doesn't close until all connections are closed. When we
+     * call `server.close()` above, we stop receiving new connections, but the
+     * remaining connections are open indefinitely because of keep-alive connections.
+     * In order to close the server, we have to terminate those remaining connections ourselves.
+     *
+     */
+    connections.forEach(conn => conn.end())
+    // destroy any running connections that may have not been ended
+    await setTimeout(5000, () => {
+      connections.forEach(conn => conn.destroy())
     })
   })
 }
