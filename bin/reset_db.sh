8ca4159f43578b7e77c1efa54c66e84ebc7fe374
Fern Sanchez <fernando.sanchez@upchieve.org>
2022-02-23 11:16:13 -0500

    Clean up script for readability

--- bin/reset_db.sh
+++ bin/reset_db.sh
@@ -5,8 +5,16 @@ set -o errexit
 
 docker ps -q --filter "name=subway_pgadmin_1" | grep -q . && docker stop subway_pgadmin_1
 
-dbmate --url 'postgres://admin:Password123@localhost:5432/upchieve?sslmode=disable' drop
-dbmate --no-dump-schema --url 'postgres://admin:Password123@localhost:5432/upchieve?sslmode=disable' up > /dev/null
-PGPASSFILE="database/.pgpass" psql -w -h localhost -p 5432 -d upchieve -f ./database/db_init/auth.sql -U admin > /dev/null
+PGURL='postgres://admin:Password123@localhost:5432/upchieve?sslmode=disable' 
+
+dbmate --url $PGURL drop
+
+dbmate \
+  --no-dump-schema \
+  --url $PGURL \
+  up \
+  > /dev/null
+
+PGPASSFILE="database/.pgpass" psql -w -h localhost -p 5432 -d upchieve -f ./database/db_init/auth.sql -U admin >/dev/null
 
 docker ps -q --all --filter "name=subway_pgadmin_1" | grep -q . && docker start subway_pgadmin_1
