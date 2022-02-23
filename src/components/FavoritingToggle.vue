b67d934c118865a760d688aa655bc24ee945b02f
Saumyaa <saumyaa.mehra@gmail.com>
2022-02-23 17:09:12 -0500

    using svgs for component

--- src/components/FavoritingToggle.vue
+++ src/components/FavoritingToggle.vue
@@ -1,116 +1,85 @@
 <template>
-  <div class="heart">
-
+  <div>
+    <component :class="favoritedStatus.class" v-bind:is="svg" v-on:click="changeFavoritedStatus"/>
   </div>
 </template>
 
 <script>
+import FavoritedSVG from '@/assets/favorited.svg'
+import UnfavoritedSVG from '@/assets/unfavorited.svg'
+
 export default {
-  name: 'favoriting-toggle'
+  name: 'favoriting-toggle',
+  components: { FavoritedSVG, UnfavoritedSVG },
+  props: {
+    svg: {
+      type: Object,
+      required: true,
+      default: UnfavoritedSVG
+    },
+    isFavorited: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    changeFavoritedStatus() {
+      this.isFavorited = !this.isFavorited
+     // this.favoritedStatus()
+     // NetworkService.saveFavoritedStatus(this.isFavorited)
+    }
+  },
+  computed: {
+    favoritedStatus() {
+      const status = {
+        class: 'UnfavoritedToggle',
+    }   
+
+    if(this.isFavorited){
+      status.class = 'FavoritedToggle',
+      this.svg = FavoritedSVG
+    }
+    else {
+      status.class = 'UnfavoritedToggle',
+      this.svg = UnfavoritedSVG
+    }
+    return status
+  }   
+}
 }
 </script>
 
 <style lang="scss">
-
-html {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.heart {
-  position: relative;
-  height: 20px;
-  width: 20px;
-  display: flex;
-  justify-content: center;
-  text-align: center;
-  border-radius: 0 0 0 1px;
-  transform: rotate(-45deg);
-  border: 1px solid #000000;
-  border-top-color: transparent;
-  border-right-color: transparent;
- }
-
-.heart:hover {
-  border-left-color: #F41717;
-  border-bottom-color: #F41717;
-}
-.heart:active {
-  background: linear-gradient(45deg, #F41717 50%, white 0%); //#F41717; 
-  border-top-color: white;
-  border-right-color: white;
-  height: 18px;
-  width: 18px;
-}
-
-.heart:before {
-  position: absolute;
-  border: 1px solid #000000;
-  content: "";
-  width: 18.6px;
+.FavoritedToggle {
+  fill: #F44747;
   height: 18.6px;
-  top: -48%;
-  //left: -1px;
-  left: -1px;
-  background-color: transparent;
-  border-radius: 50%;
-  transform: rotate(-41deg);
-  border-bottom-color: transparent;
-  border-left-color: transparent;
-}
+  width: 20px;
 
-  .heart:hover:before {
-  border-top-color: #F41717 ;
-  border-right-color: #F41717 ;
-  border-bottom-color: transparent;
-  border-left-color: transparent;
- // left: 0;
+  &:hover {
+    fill: #F41717;
   }
 
-  .heart:active:before {
-  background-color: #F44747;
-  height: 16px;
-  width: 16px;
-  //top: 0;
+  &:active {
+    height: 16px;
+    width: 18px;
+  }
 }
 
-.heart:after {
-  position: absolute;
-  border: 1px solid #000000;
-  content: "";
-  //top: -1px;
-  top: 0.5px;
-  right: -48%;
-  width: 18.6px;
+.UnfavoritedToggle {
   height: 18.6px;
-  background: transparent;
-  border-radius: 50%;
-  transform: rotate(-139deg);
-  border-top-color: transparent;
-  border-right-color: transparent;
-}
+  width: 20px;
 
-.heart:hover:after {
-  border-bottom-color: #F41717;
-  border-left-color: #F41717;
-  border-top-color: transparent;
-  border-right-color: transparent;
-  //top: 0;
-}
+ &:hover {
+   fill: white;
+    stroke: #F41717
+  }
 
-.heart:active:after {
-  border-color: #F44747;
-  background-color: #F44747;
-  height: 16px;
-  width: 16px;
-  top: 1px;
+  &:active {
+    fill: none;
+    height: 16px;
+    width: 18px;
+  }
+
 }
 
 </style>
--- src/stories/FavoritingToggle.stories.js
+++ src/stories/FavoritingToggle.stories.js
@@ -9,7 +9,10 @@ const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { FavoritingToggle },
   template:
-    '<favoriting-toggle v-bind="$props" />'
+    '<favoriting-toggle @onClick="handleClick" v-bind="$props" />'
 })
 
 export const Vanilla = Template.bind({})
+// Vanilla.args = {
+//   isFavorited: false
+// }
