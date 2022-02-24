c5384dd8bd58fcb87083bd7e62268126b9e18cdd
Trey Stevens <treykstevens.tks@gmail.com>
2022-02-24 18:00:41 -0500

    added students endpoints

---
+++ server/router/api/students.ts
@@ -0,0 +1,58 @@
+import { Router } from 'express'
+import config from '../../config'
+import * as StudentRepo from '../../models/Student/queries'
+import { asNumber, asString } from '../../utils/type-utils'
+import { extractUser } from '../extract-user'
+import { resError } from '../res-error'
+
+export function routeStudents(router: Router): void {
+  router.get('/students/remaining-favorite-volunteers', async function(
+    req,
+    res
+  ) {
+    try {
+      const user = extractUser(req)
+      const totalFavoriteVolunteers: number = (await StudentRepo.getTotalFavoriteVolunteers(
+        String(user._id)
+      )) as number
+      res.json({
+        remaining: config.favoriteVolunteerLimit - totalFavoriteVolunteers,
+      })
+    } catch (error) {
+      resError(res, error)
+    }
+  })
+
+  router.get('/students/favorite-volunteers/:volunteerId', async function(
+    req,
+    res
+  ) {
+    try {
+      const volunteerId = asString(req.params.volunteerId)
+      const user = extractUser(req)
+      const isFavorite = await StudentRepo.isFavoriteVolunteer(
+        String(user._id),
+        volunteerId
+      )
+      res.json({
+        isFavorite,
+      })
+    } catch (error) {
+      resError(res, error)
+    }
+  })
+
+  router.get('/students/favorite-volunteers', async function(req, res) {
+    try {
+      const page = asNumber(req.query.page)
+      const user = extractUser(req)
+      const result = await StudentRepo.getFavoriteVolunteers(
+        String(user._id),
+        page
+      )
+      res.json(result)
+    } catch (error) {
+      resError(res, error)
+    }
+  })
+}
