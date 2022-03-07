8f474993af884fb5a8457d8d1c25313ccf599bb6
Dave Sudia <dave.sudia@upchieve.org>
2022-03-07 14:51:08 -0700

    draft of push notification changes

--- server/constants/feature-flags.ts
+++ server/constants/feature-flags.ts
@@ -2,4 +2,5 @@ export enum FEATURE_FLAGS {
   GATES_STUDY = 'gates-study',
   CHATBOT = 'chatbot',
   ALGEBRA_TWO_LAUNCH = 'algebra-two-launch',
+  NEW_MOBILE_APP = 'new-mobile-app'
 }
--- server/services/PushTokenService.ts
+++ server/services/PushTokenService.ts
@@ -1,6 +1,10 @@
 import { messaging } from 'firebase-admin'
 import { Session } from '../models/Session'
+import { isEnabled } from 'unleash-client'
 import Case from 'case'
+import {
+  FEATURE_FLAGS,
+} from '../constants'
 
 async function sendToUser(
   title: string,
@@ -8,43 +12,60 @@ async function sendToUser(
   data: { path: string },
   tokens: string[]
 ) {
-  return await messaging().sendMulticast({
-    tokens, // can also send to a topic (group of people)
-    // ios and android process data a little differently, so setup separate objects for each
-    apns: {
-      payload: Object.assign(
-        {
-          data,
-        },
-        {
-          aps: {
-            alert: {
-              title: title,
-              body: text,
-              'content-available': 1,
-            },
-          },
-        }
-      ),
-    },
-    android: {
-      // TS says this needs to be a string,
-      // of 'high' | 'normal'
-      // Guessing that 1 is equivalent with 'high'
-      priority: 'high',
+
+  let message
+  if (isEnabled(FEATURE_FLAGS.NEW_MOBILE_APP)) {
+    message = {
+      tokens,
       data: {
-        title: title,
-        body: text,
-        message: text,
-        // image: imageUrl,
-        payload: JSON.stringify(data),
-        'content-available': '1',
-        // type: message.type,
-        icon: 'notification_icon',
-        color: '#16d2aa',
+        'data': 'chat',
+        sessionId: data.path
       },
-    },
-  })
+      notification: {
+        title,
+        body: text,
+        sound: 'Tri-tone'
+      }
+    }
+  } else {
+    message = {
+      tokens,
+      apns: {
+        payload: Object.assign(
+          {
+            data,
+          },
+          {
+            aps: {
+              alert: {
+                title: title,
+                body: text,
+                'content-available': 1,
+              },
+            },
+          }
+        ),
+      },
+      android: {
+        // TS says this needs to be a string,
+        // of 'high' | 'normal'
+        // Guessing that 1 is equivalent with 'high'
+        priority: 'high',
+        data: {
+          title: title,
+          body: text,
+          message: text,
+          // image: imageUrl,
+          payload: JSON.stringify(data),
+          'content-available': '1',
+          // type: message.type,
+          icon: 'notification_icon',
+          color: '#16d2aa',
+        },
+      },
+    }
+  }
+  return await messaging().sendMulticast(message)
 }
 
 export async function sendVolunteerJoined(
@@ -54,8 +75,15 @@ export async function sendVolunteerJoined(
   const { type, subTopic, _id } = session
   const title = 'We found a volunteer!'
   const text = 'Start chatting with your coach now.'
-  const data = {
-    path: `/session/${Case.kebab(type)}/${Case.kebab(subTopic)}/${_id}`,
+  let data
+  if (isEnabled(FEATURE_FLAGS.NEW_MOBILE_APP)) {
+    data = {
+      path: `/session/${Case.kebab(type)}/${Case.kebab(subTopic)}/${_id}`,
+    }
+  } else {
+    data = {
+      path: _id
+    }
   }
 
   await sendToUser(title, text, data, tokens)
