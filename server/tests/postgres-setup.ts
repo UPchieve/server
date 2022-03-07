6ee8f3359e42e5258fbe7aa0462a8af8241aff45
Fern Sanchez <fernando.sanchez@upchieve.org>
2022-03-07 09:49:41 -0500

    More logging, trying to not use port mapping

--- server/tests/postgres-setup.ts
+++ server/tests/postgres-setup.ts
@@ -14,22 +14,26 @@ const healthCheck = {
 let __PG_CONTAINER__: StartedTestContainer | undefined = undefined
 
 export async function setup() {
+  const isCI = Boolean(process.env.CI_CONTAINER)
   console.log('Starting setup')
-  const container = new GenericContainer('subway-postgres')
+  const base = new GenericContainer('subway-postgres')
     .withHealthCheck(healthCheck)
     .withExposedPorts(PORT)
     .withWaitStrategy(Wait.forHealthCheck())
-  // const container = Boolean(process.env.CI_CONTAINER)
-  //  ? base.withNetworkMode('host')
-  //  : base
+  const container = isCI
+    ? base.withNetworkMode('host')
+    : base
 
   __PG_CONTAINER__ = await container.start()
   console.log('Launched container')
 
-  const host = Boolean(process.env.CI_CONTAINER) ? 'docker' : __PG_CONTAINER__.getHost()
+  const host = isCI ? 'docker' : __PG_CONTAINER__.getHost()
+  const port = isCI ? 5432 : __PG_CONTAINER__.getMappedPort(PORT)
+
+  console.log(`host:port at ${host}:${port}`)
   const globalEnv = {
     __PG_HOST__: host,
-    __PG_PORT__: __PG_CONTAINER__.getMappedPort(PORT),
+    __PG_PORT__: port,
   }
 
   setGlobalsFromEnv(global, globalEnv)
