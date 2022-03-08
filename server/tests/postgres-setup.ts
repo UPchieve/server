bede63efa35046dab11df3dc571e8f4b4db5f4d5
Fern Sanchez <fernando.sanchez@upchieve.org>
2022-03-07 20:50:42 -0500

    Back to copy. More logging

--- server/tests/postgres-setup.ts
+++ server/tests/postgres-setup.ts
@@ -39,7 +39,8 @@ export async function setup() {
     //.withBindMount(`${root}/database/db_init`, '/docker-entrypoint-initdb.d')
 
   __PG_CONTAINER__ = await container.start()
-  console.log((await __PG_CONTAINER__.exec(['ls', '-al', '/docker-entrypoint-initdb.d'])).output)
+  console.log('Local folder:', `${root}/database/db_init/schema.sql`)
+  console.log('Postgres folder:', (await __PG_CONTAINER__.exec(['ls', '-al', '/docker-entrypoint-initdb.d'])).output)
   // In CI the container running docker will live at host docker via docker links
   const host = isCI ? 'docker' : __PG_CONTAINER__.getHost()
 
