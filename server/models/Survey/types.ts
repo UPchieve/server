df018d5fde9eafa21ef38d6ccd63da1a1873faa6
Trey Stevens <treykstevens.tks@gmail.com>
2022-03-17 01:48:51 -0400

    moved code from index to types.ts

---
+++ server/models/Survey/types.ts
@@ -0,0 +1,57 @@
+import { values } from 'lodash'
+import { Document, model, Schema, Types } from 'mongoose'
+import { SURVEY_TYPES } from '../../constants'
+import { Ulid } from '../pgUtils'
+import { User } from '../User'
+
+export interface PgSurvey {
+  id: Ulid
+  userId: Ulid
+  sessionId: Ulid
+  responseData: { [key: string]: any }
+  createdAt: Date
+  updatedAt: Date
+  mongoId?: Types.ObjectId
+}
+
+export interface Survey {
+  _id: Types.ObjectId
+  session: Types.ObjectId
+  responseData: { [key: string]: any }
+  user: Types.ObjectId | User
+  surveyType: SURVEY_TYPES
+  createdAt: Date
+}
+
+type SurveyDocument = Survey & Document
+
+const surveySchema = new Schema({
+  session: {
+    type: Types.ObjectId,
+    ref: 'Session',
+  },
+
+  user: {
+    type: Types.ObjectId,
+    ref: 'User',
+  },
+
+  surveyType: {
+    type: String,
+    enum: values(SURVEY_TYPES),
+  },
+
+  responseData: {
+    type: Object,
+    default: '',
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+const SurveyModel = model<SurveyDocument>('Survey', surveySchema)
+
+export default SurveyModel
