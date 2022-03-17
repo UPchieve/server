14a1a77f72dd076d09928d1357b18809e39f24f5
Saumyaa <saumyaa.mehra@gmail.com>
2022-03-17 14:50:04 -0400

    comma fixes

--- server/models/School/pg.queries.ts
+++ server/models/School/pg.queries.ts
@@ -7,23 +7,23 @@ export type IFindSchoolByIdResult = never;
 /** Query 'FindSchoolById' is invalid, so its parameters are assigned type 'never' */
 export type IFindSchoolByIdParams = never;
 
-const findSchoolByIdIR: any = {"name":"findSchoolById","params":[{"name":"schoolId","required":true,"transform":{"type":"scalar"},"codeRefs":{"used":[{"a":310,"b":318,"line":15,"col":10}]}}],"usedParamSet":{"schoolId":true},"statement":{"body":"SELECT\n    id,\n    name AS name_stored,\n    approved AS is_approved,\n    partner AS is_partner,\n    us_state_code AS state_stored,\n    created_at,\n    updated_at\n    cities.name AS city_name_stored\nFROM\n    schools\n    LEFT JOIN cities ON cities.id = schools.city_id\nWHERE\n    id = :schoolId!","loc":{"a":27,"b":318,"line":2,"col":0}}};
+const findSchoolByIdIR: any = {"name":"findSchoolById","params":[{"name":"schoolId","required":true,"transform":{"type":"scalar"},"codeRefs":{"used":[{"a":327,"b":335,"line":15,"col":10}]}}],"usedParamSet":{"schoolId":true},"statement":{"body":"SELECT\n    schools.id,\n    schools.name AS name_stored,\n    approved AS is_approved,\n    partner AS is_partner,\n    us_state_code AS state_stored,\n    created_at,\n    updated_at,\n    cities.name AS city_name_stored\nFROM\n    schools\n    LEFT JOIN cities ON schools.city_id = cities.id\nWHERE\n    id = :schoolId!","loc":{"a":27,"b":335,"line":2,"col":0}}};
 
 /**
  * Query generated from SQL:
  * ```
  * SELECT
- *     id,
- *     name AS name_stored,
+ *     schools.id,
+ *     schools.name AS name_stored,
  *     approved AS is_approved,
  *     partner AS is_partner,
  *     us_state_code AS state_stored,
  *     created_at,
- *     updated_at
+ *     updated_at,
  *     cities.name AS city_name_stored
  * FROM
  *     schools
- *     LEFT JOIN cities ON cities.id = schools.city_id
+ *     LEFT JOIN cities ON schools.city_id = cities.id
  * WHERE
  *     id = :schoolId!
  * ```
--- server/models/School/school.sql
+++ server/models/School/school.sql
@@ -1,15 +1,16 @@
 /* @name findSchoolById */
 SELECT
-    id,
-    name AS name_stored,
+    schools.id,
+    schools.name AS name_stored,
     approved AS is_approved,
     partner AS is_partner,
     us_state_code AS state_stored,
     created_at,
-    updated_at cities.name AS city_name_stored
+    updated_at,
+    cities.name AS city_name_stored
 FROM
     schools
-    LEFT JOIN cities ON cities.id = schools.city_id
+    LEFT JOIN cities ON schools.city_id = cities.id
 WHERE
     id = :schoolId!;
 
