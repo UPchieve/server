d25f0c03f060fe0e34a7a6fe24fbe17fc328a469
Trey Stevens <treykstevens.tks@gmail.com>
2022-03-17 13:41:41 -0400

    parseQueryResult by parsing the json of responseData

--- server/models/Survey/pgqueries.ts
+++ server/models/Survey/pgqueries.ts
@@ -4,6 +4,27 @@ import { getDbUlid, makeRequired, Ulid } from '../pgUtils'
 import * as pgQueries from './pg.queries'
 import { PgSurvey } from './types'
 
+export type Json =
+  | null
+  | boolean
+  | number
+  | string
+  | Json[]
+  | { [key: string]: Json }
+
+export type PgSurveyQueryResult = Omit<PgSurvey, 'responseData'> & {
+  responseData: Json
+}
+
+export function parseQueryResult(result: PgSurveyQueryResult): PgSurvey {
+  const responseData =
+    typeof result.responseData === 'string'
+      ? JSON.parse(result.responseData)
+      : {}
+
+  return { ...result, responseData }
+}
+
 export async function savePresessionSurvey(
   userId: Ulid,
   sessionId: Ulid,
@@ -19,14 +40,17 @@ export async function savePresessionSurvey(
       },
       getClient()
     )
-    if (result.length) return makeRequired(result[0]) as PgSurvey
+    if (result.length) {
+      const parsedResult = parseQueryResult(result[0])
+      return makeRequired(parsedResult)
+    }
     throw new RepoCreateError('Error upserting presession survey')
   } catch (err) {
     throw new RepoCreateError(err)
   }
 }
 
-// NOTE: this particular query can be replaced by a JOIN that happens when we fetch
+// NOTE: this query can be replaced by a JOIN that happens when we fetch
 // the session on the feedback page
 export async function getPresessionSurvey(
   userId: Ulid,
@@ -40,7 +64,10 @@ export async function getPresessionSurvey(
       },
       getClient()
     )
-    if (result.length) return makeRequired(result[0]) as PgSurvey
+    if (result.length) {
+      const parsedResult = parseQueryResult(result[0])
+      return makeRequired(parsedResult)
+    }
   } catch (err) {
     throw new RepoReadError(err)
   }
