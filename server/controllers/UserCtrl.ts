e3ddd40c3b6d25e571b8a390bca767557840bf5b
Trey Stevens <treykstevens.tks@gmail.com>
2022-03-25 03:56:11 -0400

    comment in UserCtrl

--- server/controllers/UserCtrl.ts
+++ server/controllers/UserCtrl.ts
@@ -37,6 +37,7 @@ export async function createStudent(
 ): Promise<Student> {
   studentData.password = await hashPassword(studentData.password)
   // TODO: repo pattern
+  // replace with createStudent
   const student = new StudentModel(studentData)
   student.referralCode = generateReferralCode(student._id)
 
