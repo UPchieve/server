f22e2f88d8753ebc66d0363ed2b692f649595e45
Dave Sudia <dave.sudia@upchieve.org>
2022-03-31 11:53:45 -0600

    finished script and ran it

--- server/dbutils/normalize-school-location-data.ts
+++ server/dbutils/normalize-school-location-data.ts
@@ -1,21 +1,41 @@
 import mongoose from 'mongoose'
 import * as db from '../db'
 import SchoolModel from '../models/School'
+import async from 'async'
 
 // Run:
-// npx ts-node server/dbutils/backfill-ispartner.ts
+// npx ts-node server/dbutils/normalize-school-location-data.ts
 async function upgrade(): Promise<void> {
   let exitCode = 0
   try {
     await db.connect()
 
-    const result = await SchoolModel.updateMany(
-      { isPartner: {
-          $exists: false
-        }},
-      { $set: { isPartner: false } }
-    )
-    console.log(result)
+    const schools = await SchoolModel.find({
+      '$or': [
+        {
+          'cityNameStored': {
+            '$exists': false
+          }
+        }, {
+          'stateStored': {
+            '$exists': false
+          }
+        }
+      ]
+    }, {
+        _id: 1,
+        ST: 1,
+        LCITY: 1
+      }).lean().exec()
+    console.log(schools.length)
+    console.log(schools[0])
+    console.log(schools[1])
+    await async.forEach(schools, async (school) => {
+      await SchoolModel.updateOne({_id: school._id}, {
+        stateStored: school.ST,
+        cityNameStored: school.LCITY
+      })
+    })
   } catch (error) {
     console.error(error)
     exitCode = 1
