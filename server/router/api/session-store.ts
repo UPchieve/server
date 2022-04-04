876280c07e78dd926ad1b114ad710fc71e836537
Fern Sanchez <fernando.sanchez@upchieve.org>
2022-04-04 13:14:11 -0400

    Use a new client for session store

--- server/router/api/session-store.ts
+++ server/router/api/session-store.ts
@@ -2,14 +2,14 @@ import session from 'express-session'
 import CreateRedisStore from 'connect-redis'
 import config from '../../config'
 import { Express } from 'express'
-import { redisClient } from '../../services/RedisService'
+import Redis from 'ioredis'
 
 const RedisStore = CreateRedisStore(session)
 
 export const sessionStoreCollectionName = 'auth-sessions'
 
 export default function(app: Express) {
-  const store = new RedisStore({ client: redisClient })
+  const store = new RedisStore({ client: new Redis(config.redisConnectionString) })
   app.use(
     session({
       resave: false,
