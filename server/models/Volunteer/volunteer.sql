e9f3b379faf364bf07cbc38e361b7cba0f96b703
Trey Stevens <treykstevens.tks@gmail.com>
2022-04-13 00:49:40 +0200

    lint

--- server/models/Volunteer/volunteer.sql
+++ server/models/Volunteer/volunteer.sql
@@ -345,11 +345,10 @@ SELECT
     volunteer_references.email AS reference_email
 FROM
     volunteer_references
-LEFT JOIN volunteer_reference_statuses ON volunteer_reference_statuses.id = volunteer_references.status_id
+    LEFT JOIN volunteer_reference_statuses ON volunteer_reference_statuses.id = volunteer_references.status_id
 WHERE
     volunteer_references.id = :referenceId!
-AND
-    volunteer_reference_statuses.name <> 'removed'
+    AND volunteer_reference_statuses.name <> 'removed'
 LIMIT 1;
 
 
