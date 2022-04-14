15764831d30e2835e5b334ae4b139d5dcf624338
Trey Stevens <treykstevens.tks@gmail.com>
2022-04-14 18:53:32 +0200

    change userId to studentId

--- server/models/Student/queries.ts
+++ server/models/Student/queries.ts
@@ -203,13 +203,13 @@ export type UpdateFavoriteVolunteer = {
 }
 
 export async function getFavoriteVolunteers(
-  userId: Ulid,
+  studentId: Ulid,
   limit: number,
   offset: number
 ): Promise<FavoriteVolunteersResponse> {
   try {
     const result = await pgQueries.getFavoriteVolunteers.run(
-      { userId, limit, offset },
+      { studentId , limit, offset },
       getClient()
     )
     return {
