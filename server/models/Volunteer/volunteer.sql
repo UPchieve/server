673706ee305d4fc79e3e224f57fbb0c825aabe3b
Trey Stevens <treykstevens.tks@gmail.com>
2022-04-19 21:43:07 +0200

    add last_activity_at when creating student and volunteer accounts

--- server/models/Volunteer/volunteer.sql
+++ server/models/Volunteer/volunteer.sql
@@ -1118,8 +1118,8 @@ RETURNING
 
 
 /* @name createVolunteerUser */
-INSERT INTO users (id, email, phone, first_name, last_name, PASSWORD, verified, referred_by, referral_code, created_at, updated_at)
-    VALUES (:userId!, :email!, :phone!, :firstName!, :lastName!, :password!, FALSE, :referredBy, :referralCode!, NOW(), NOW())
+INSERT INTO users (id, email, phone, first_name, last_name, PASSWORD, verified, referred_by, referral_code, last_activity_at, created_at, updated_at)
+    VALUES (:userId!, :email!, :phone!, :firstName!, :lastName!, :password!, FALSE, :referredBy, :referralCode!, NOW(), NOW(), NOW())
 ON CONFLICT (email)
     DO NOTHING
 RETURNING
