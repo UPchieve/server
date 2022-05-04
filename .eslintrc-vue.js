f43e6c2d3b94860c90e44216d0d2f6a7e909fb7d
Fern Sanchez <fernando.sanchez@upchieve.org>
2022-05-04 14:47:40 -0400

    Remove mongo stuff from server config. Add some logs to see what's going on with image upload in staging

--- .eslintrc-vue.js
+++ .eslintrc-vue.js
@@ -8,7 +8,7 @@ module.exports = {
     "@vue/prettier",
   ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+    //"no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn"
   },
   parserOptions: {
--- src/utils/photo-upload.js
+++ src/utils/photo-upload.js
@@ -1,5 +1,6 @@
 function checkIfPhotoIsAllowedType(file) {
   const allowedFormats = ['image/png', 'image/jpeg', 'image/jpg', 'image/gif']
+  console.log(`FILE TYPE: ${file.type}`)
   if (!allowedFormats.includes(file.type)) {
     throw new Error('We only allow .png, .jpeg, or .gif photos')
   }
@@ -7,6 +8,7 @@ function checkIfPhotoIsAllowedType(file) {
 
 function checkIfPhotoIsAllowedSize(file) {
   const tenMegaBytes = 10 * 1000000
+  console.log(`FILE SIZE: ${file.size}`)
   if (file.size > tenMegaBytes) {
     throw new Error(
       `This photo is too large. Please upload a photo less than 10mb.`
