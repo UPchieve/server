304761c6ee866fb430ae1ec73bc57014dff5b1ff
Fern Sanchez <fernando.sanchez@upchieve.org>
2022-05-12 14:01:06 -0400

    Change associated partner seed update to isnert new row instead of updating in place. We will backfill later

--- database/seed-updates/20220512174157_update_assocated_partner_priority_groups.sql
+++ database/seed-updates/20220512174157_update_assocated_partner_priority_groups.sql
@@ -1,36 +1,13 @@
 -- migrate:up
-UPDATE
-    upchieve.notification_priority_groups
-SET
-    name = 'Associated partner volunteers - not notified in the last 24 hours AND they don''t have "high level subjects"',
-    updated_at = NOW()
-WHERE
-    name = 'Verizon volunteers - not notified in the last 24 hours AND they don''t have "high level subjects"';
+INSERT INTO upchieve.notification_priority_groups (name, priority, created_at, updated_at)
+    VALUES ('Associated partner volunteers - not notified in the last 24 hours AND they don''t have "high level subjects"', 85, NOW(), NOW());
 
-UPDATE
-    upchieve.notification_priority_groups
-SET
-    name = 'Associated partner volunteers - not notified in the last 3 days AND they don''t have "high level subjects"',
-    updated_at = NOW()
-WHERE
-    name = 'Verizon volunteers - not notified in the last 3 days AND they don''t have "high level subjects"';
-
+INSERT INTO upchieve.notification_priority_groups (name, priority, created_at, updated_at)
+    VALUES ('Associated partner volunteers - not notified in the last 3 days AND they don''t have "high level subjects"', 95, NOW(), NOW());
 
 -- migrate:down
+DELETE FROM upchieve.notification_priority_groups
+WHERE name = 'Associated partner volunteers - not notified in the last 24 hours AND they don''t have "high level subjects"';
 
-UPDATE
-    upchieve.notification_priority_groups
-SET
-    name = 'Verizon volunteers - not notified in the last 24 hours AND they don''t have "high level subjects"',
-    updated_at = NOW()
-WHERE
-    name = 'Associated partner volunteers - not notified in the last 24 hours AND they don''t have "high level subjects"';
-
-UPDATE
-    upchieve.notification_priority_groups
-SET
-    name = 'Verizon volunteers - not notified in the last 3 days AND they don''t have "high level subjects"',
-    updated_at = NOW()
-WHERE
-    name = 'Associated partner volunteers - not notified in the last 3 days AND they don''t have "high level subjects"';
-
+DELETE FROM upchieve.notification_priority_groups
+WHERE name = 'Associated partner volunteers - not notified in the last 3 days AND they don''t have "high level subjects"';
