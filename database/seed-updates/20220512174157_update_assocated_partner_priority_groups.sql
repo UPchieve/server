8c7be5d811ddae6842701adb6b82831f3c079db4
Fern Sanchez <fernando.sanchez@upchieve.org>
2022-05-12 13:53:45 -0400

    Added migrate down to seed migration

--- database/seed-updates/20220512174157_update_assocated_partner_priority_groups.sql
+++ database/seed-updates/20220512174157_update_assocated_partner_priority_groups.sql
@@ -18,3 +18,19 @@ WHERE
 
 -- migrate:down
 
+UPDATE
+    upchieve.notification_priority_groups
+SET
+    name = 'Verizon volunteers - not notified in the last 24 hours AND they don''t have "high level subjects"',
+    updated_at = NOW()
+WHERE
+    name = 'Associated partner volunteers - not notified in the last 24 hours AND they don''t have "high level subjects"';
+
+UPDATE
+    upchieve.notification_priority_groups
+SET
+    name = 'Verizon volunteers - not notified in the last 3 days AND they don''t have "high level subjects"',
+    updated_at = NOW()
+WHERE
+    name = 'Associated partner volunteers - not notified in the last 3 days AND they don''t have "high level subjects"';
+
