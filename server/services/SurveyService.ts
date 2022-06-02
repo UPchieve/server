a33dfa0184a4c02f6a9e6a8b431249f2ad60e39d
Saumyaa <saumyaa.mehra@gmail.com>
2022-06-02 16:53:53 -0400

    minor naming change fix

--- server/services/SurveyService.ts
+++ server/services/SurveyService.ts
@@ -1,8 +1,8 @@
 import * as SurveyRepo from '../models/Survey'
 
-export async function getPresessionSurvey(
+export async function getPresessionSurveyNew(
   subjectName: string
 ): Promise<SurveyRepo.PresessionSurvey[]> {
-  const surveyQuestions = await SurveyRepo.getPresessionSurvey(subjectName)
+  const surveyQuestions = await SurveyRepo.getPresessionSurveyNew(subjectName)
   return surveyQuestions
 }
