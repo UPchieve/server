55d8ee02719f77460c4ab42a14e3a8427280293b
Dave Sudia <dave.sudia@upchieve.org>
2022-06-24 11:07:22 -0600

    add explanatory comments in mat view query

--- database/migrations/20220617192340_create_users_subjects_mview.sql
+++ database/migrations/20220617192340_create_users_subjects_mview.sql
@@ -1,4 +1,8 @@
 -- migrate:up
+/*
+This CTE retries the total number of certifications needed to unlock every subject (maps `subject_id->number`)
+*/
+
 CREATE MATERIALIZED VIEW IF NOT EXISTS upchieve.users_subjects_mview AS
 WITH subject_totals AS (
     SELECT
@@ -25,14 +29,18 @@ FROM
                 user_id,
                 upchieve.subjects.id AS subject
             FROM
+              -- The end result of these joins is `certifications` attached to which subject they help unlock (will duplicate some certs which can be used to unlock multiple subjects)
                 upchieve.users_certifications
                 JOIN upchieve.certification_subject_unlocks USING (certification_id)
                 JOIN upchieve.subjects ON upchieve.certification_subject_unlocks.subject_id = upchieve.subjects.id
                 JOIN subject_totals ON subject_totals.id = upchieve.subjects.id
             GROUP BY
+              -- bucket the result for each user by subject id (in this case the subject the cert is contributing to unlocking)
                 user_id, upchieve.subjects.id, subject_totals.total
             HAVING
+              -- in each subject bucket check that the user has earned the same number of certs that contribute to unlocking the subject as the total we calculated initially in the CTE
                 COUNT(*)::int >= subject_totals.total) AS sub_unlocked
+             -- the output of the inner most query is one row per user per subject with columns for the user id and subject id so this outer query is kind of unnecessary but in the original example query given we array_agg subject names per user for a human readable output
         GROUP BY
             user_id,
             subject) AS subjects_unlocked ON subjects_unlocked.user_id = upchieve.users.id
