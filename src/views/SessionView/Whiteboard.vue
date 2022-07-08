04817a648529a952fc67edfe0d57e271addece23
Saumyaa <saumyaa.mehra@gmail.com>
2022-07-08 13:40:05 -0400

    testing for only relevant ios platforms

--- src/views/SessionView/Whiteboard.vue
+++ src/views/SessionView/Whiteboard.vue
@@ -370,7 +370,7 @@ export default {
   },
   methods: {
     isMobile() {
-      if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+      if(/iPhone|iPad|iPod/i.test(navigator.userAgent))
         return true
       else
         return false
