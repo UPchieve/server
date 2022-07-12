b58afde86fee25d1c96ea502d06ea450aeecad9d
Trey Stevens <treykstevens.tks@gmail.com>
2022-07-12 18:52:29 +0300

    remove unused import

--- server/models/Survey/queries.ts
+++ server/models/Survey/queries.ts
@@ -2,7 +2,7 @@ import { getClient } from '../../db'
 import { RepoCreateError, RepoReadError } from '../Errors'
 import { getDbUlid, makeRequired, makeSomeRequired, Ulid } from '../pgUtils'
 import * as pgQueries from './pg.queries'
-import { PresessionSurveyResponseData, Survey } from './types'
+import { Survey } from './types'
 import { fixNumberInt } from '../../utils/fix-number-int'
 import _ from 'lodash'
 
