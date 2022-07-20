e5c6de7ce5cea3648b3d927c60c1eae8b6794b79
Trey Stevens <treykstevens.tks@gmail.com>
2022-07-20 18:38:20 +0300

    added a loader for the when retrieving the session context

--- src/views/SessionView/index.vue
+++ src/views/SessionView/index.vue
@@ -38,10 +38,13 @@
         }"
       >
         <div
-          v-if="
-            user.isVolunteer &&
-            studentPresessionResponses.length > 0
-          "
+          v-if="user.isVolunteer && isLoadingPresessionResponse"
+          class="about-session-container"
+        >
+          <loading-message message="Loading" class="about-session-loader" />
+        </div>
+        <div
+          v-else-if="user.isVolunteer && studentPresessionResponses.length > 0"
           class="about-session-container"
         >
           <div 
@@ -53,9 +56,9 @@
           </div>
         </div>
         <div
-          v-if="
+          v-else-if="
             user.isVolunteer &&
-            showNoPressionSurveyResponse
+            (showNoPressionSurveyResponse || studentPresessionResponses.length === 0)
           "
           class="about-session-container"
         >
@@ -123,6 +126,7 @@ import getNotificationPermission from '@/utils/get-notification-permission'
 import { EVENTS } from '@/consts'
 import Gleap from 'gleap'
 import { backOff } from 'exponential-backoff'
+import LoadingMessage from '@/components/LoadingMessage'
 
 const activeHeaderData = {
   component: 'SessionHeader'
@@ -138,7 +142,8 @@ export default {
     DocumentEditor,
     WebNotificationsModal,
     CaretIcon,
-    AboutSessionModal
+    AboutSessionModal,
+    LoadingMessage
   },
   created() {
     if (this.mobileMode) {
@@ -168,7 +173,8 @@ export default {
       showAboutSessionModal: false,
       studentPresessionResponses: [],
       totalStudentSessions: 0,
-      showNoPressionSurveyResponse: false
+      showNoPressionSurveyResponse: false,
+      isLoadingPresessionResponse: false,
     }
   },
   beforeRouteEnter(to, from, next) {
@@ -308,7 +314,7 @@ export default {
         this.$store.dispatch('user/sessionConnected')
 
         if (this.user.isVolunteer && this.isContextSharingWithVolunteerActive) {
-          await this.getSharingContext(sessionId)
+          await this.getSessionContext(sessionId)
         }
 
         if (
@@ -436,8 +442,9 @@ export default {
     toggleAboutSessionModal() {
       this.showAboutSessionModal = !this.showAboutSessionModal
     },
-    async getSharingContext(sessionId) {
+    async getSessionContext(sessionId) {
       try {
+        this.isLoadingPresessionResponse = true
         const pressionSurveyResponse =
           await NetworkService.getPresessionSurveyResponse(sessionId)
         this.totalStudentSessions =
@@ -445,6 +452,8 @@ export default {
         this.studentPresessionResponses = pressionSurveyResponse.data.responses
       } catch(err) {
         this.showNoPressionSurveyResponse = true
+      } finally {
+        this.isLoadingPresessionResponse = false
       }
     },
   },
@@ -541,6 +550,11 @@ export default {
   }
 
   &-no-responses {
+    @include font-category('subheading');
+    padding: 0.4rem 0.5rem;
+  }
+
+  &-loader {
     @include font-category('subheading');
     padding: 0.4rem 0.5rem;
   }
