96ef80b3110439710ee1b5b860607ec9add8ce10
Trey Stevens <treykstevens.tks@gmail.com>
2022-07-21 16:47:12 +0300

    updated seeds per programs changes

--- database/seed-updates/20220713180933_add_new_response_choices_with_place_holders.sql
+++ database/seed-updates/20220713180933_add_new_response_choices_with_place_holders.sql
@@ -160,6 +160,145 @@ WHERE
   AND sq.question_text = 'How well do you understand the topic you want to talk about today?'
   AND rc.choice_text = 'Almost perfectly';
 
+-- add missing essays presession survey
+INSERT INTO upchieve.surveys_context (survey_id, subject_id, survey_type_id, created_at, updated_at)
+SELECT
+  upchieve.surveys.id,
+  upchieve.subjects.id,
+  upchieve.survey_types.id,
+  NOW(),
+  NOW()
+FROM upchieve.surveys
+JOIN upchieve.subjects ON TRUE
+JOIN upchieve.survey_types ON TRUE
+WHERE
+  upchieve.surveys.name = 'College Essays Pre-Session Survey' 
+  AND upchieve.subjects.name = 'essays' 
+  AND upchieve.survey_types.name = 'presession';
+
+
+-- fix response choices with spaces at the end
+UPDATE
+    upchieve.survey_response_choices
+SET
+    choice_text = 'I''m finishing this up',
+    updated_at = NOW()
+WHERE
+    upchieve.survey_response_choices.choice_text = 'I''m finishing this up ';
+
+UPDATE
+    upchieve.survey_response_choices
+SET
+    choice_text = 'I''ve completed this',
+    updated_at = NOW()
+WHERE
+    upchieve.survey_response_choices.choice_text = 'I''ve completed this ';
+
+
+-- add missing response choices to the college related presession surveys
+INSERT INTO
+  upchieve.survey_questions_response_choices (
+    surveys_survey_question_id,
+    response_choice_id,
+    display_priority,
+    created_at,
+    updated_at
+  )
+SELECT
+  ssq.id,
+  rc.id,
+  20,
+  NOW(),
+  NOW()
+FROM
+  upchieve.surveys_survey_questions ssq
+  JOIN upchieve.survey_response_choices rc ON TRUE
+  JOIN upchieve.surveys ON upchieve.surveys.id = ssq.survey_id
+  JOIN upchieve.survey_questions sq ON sq.id = ssq.survey_question_id
+WHERE
+  (
+    upchieve.surveys.name = 'College Planning Pre-Session Survey'
+    OR upchieve.surveys.name = 'College Essays Pre-Session Survey'
+    OR upchieve.surveys.name = 'College Applications Pre-Session Survey'
+  )
+  AND sq.question_text = 'Where are you in the process of reaching your goal?'
+  AND rc.choice_text = 'I''ve done some work on this';
+
+INSERT INTO
+  upchieve.survey_questions_response_choices (
+    surveys_survey_question_id,
+    response_choice_id,
+    display_priority,
+    created_at,
+    updated_at
+  )
+SELECT
+  ssq.id,
+  rc.id,
+  40,
+  NOW(),
+  NOW()
+FROM
+  upchieve.surveys_survey_questions ssq
+  JOIN upchieve.survey_response_choices rc ON TRUE
+  JOIN upchieve.surveys ON upchieve.surveys.id = ssq.survey_id
+  JOIN upchieve.survey_questions sq ON sq.id = ssq.survey_question_id
+WHERE
+  (
+    upchieve.surveys.name = 'College Planning Pre-Session Survey'
+    OR upchieve.surveys.name = 'College Essays Pre-Session Survey'
+    OR upchieve.surveys.name = 'College Applications Pre-Session Survey'
+  )
+  AND sq.question_text = 'Where are you in the process of reaching your goal?'
+  AND rc.choice_text = 'I''ve completed this';
+
+
+-- update response display text copy
+UPDATE
+    upchieve.survey_questions
+SET
+    response_display_text = 'Their goal for the session:',
+    updated_at = NOW()
+WHERE
+    upchieve.survey_questions.question_text = 'What is your primary goal for today''s session?';
+
+
+UPDATE
+    upchieve.survey_questions
+SET
+    response_display_text = 'How much progress they''ve made towards their goal:',
+    updated_at = NOW()
+WHERE
+    upchieve.survey_questions.question_text = 'Where are you in the process of reaching your goal?';
+
+
+UPDATE
+    upchieve.survey_questions
+SET
+    response_display_text = 'How they feel about applying to college:',
+    updated_at = NOW()
+WHERE
+    upchieve.survey_questions.question_text = 'Overall, how do you feel about applying to college?';
+
+
+UPDATE
+    upchieve.survey_questions
+SET
+    response_display_text = 'How well they understand the topic:',
+    updated_at = NOW()
+WHERE
+    upchieve.survey_questions.question_text = 'How well do you understand the topic you want to talk about today?';
+
+
+UPDATE
+    upchieve.survey_questions
+SET
+    response_display_text = 'How they feel about %s:',
+    updated_at = NOW()
+WHERE
+    upchieve.survey_questions.question_text = 'Overall, how do you feel about %s?' 
+    OR upchieve.survey_questions.question_text = 'Overall, how do you feel about the %s section?';
+
 
 -- migrate:down
 DELETE FROM
@@ -269,6 +408,76 @@ FROM
 WHERE
   upchieve.surveys_survey_questions.survey_id = subquery.survey_id
   AND upchieve.surveys_survey_questions.survey_question_id = subquery.survey_question_id;
+
+
+DELETE FROM
+  upchieve.surveys_context using upchieve.surveys
+WHERE
+  upchieve.surveys_context.survey_id = upchieve.surveys.id
+  AND upchieve.surveys.name = 'College Essays Pre-Session Survey';
+
+
+DELETE FROM
+  upchieve.survey_questions_response_choices USING upchieve.survey_response_choices
+WHERE
+  upchieve.survey_questions_response_choices.response_choice_id = upchieve.survey_response_choices.id
+  AND upchieve.survey_response_choices.choice_text = 'I''ve done some work on this';
+
+
+DELETE FROM
+  upchieve.survey_questions_response_choices USING upchieve.survey_response_choices
+WHERE
+  upchieve.survey_questions_response_choices.response_choice_id = upchieve.survey_response_choices.id
+  AND upchieve.survey_response_choices.choice_text = 'I''ve completed this';
+
+
+UPDATE
+    upchieve.survey_response_choices
+SET
+    choice_text = 'I''m finishing this up ',
+    updated_at = NOW()
+WHERE
+    upchieve.survey_response_choices.choice_text = 'I''m finishing this up';
+
+
+UPDATE
+    upchieve.survey_response_choices
+SET
+    choice_text = 'I''ve completed this ',
+    updated_at = NOW()
+WHERE
+    upchieve.survey_response_choices.choice_text = 'I''ve completed this';
+
+
+-- reverting response display labels
+UPDATE
+    upchieve.survey_questions
+SET
+    response_display_text = 'Their goal:',
+    updated_at = NOW()
+WHERE
+    upchieve.survey_questions.question_text = 'What is your primary goal for today''s session?';
+
+
+UPDATE
+    upchieve.survey_questions
+SET
+    response_display_text = 'Their progress:',
+    updated_at = NOW()
+WHERE
+    upchieve.survey_questions.question_text = 'How well do you understand the topic you want to talk about today?' 
+    OR upchieve.survey_questions.question_text = 'Where are you in the process of reaching your goal?';
+
+
+UPDATE
+    upchieve.survey_questions
+SET
+    response_display_text = 'Their confidence:',
+    updated_at = NOW()
+WHERE
+    upchieve.survey_questions.question_text = 'Overall, how do you feel about %s?' 
+    OR upchieve.survey_questions.question_text = 'Overall, how do you feel about the %s section?'
+    OR upchieve.survey_questions.question_text = 'Overall, how do you feel about applying to college?';
 
 
 DELETE FROM
