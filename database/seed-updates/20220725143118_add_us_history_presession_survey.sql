0bd73ec13e7491b48f048db7bafcda569466dc89
Trey Stevens <treykstevens.tks@gmail.com>
2022-08-10 10:33:41 -0400

    udate us history presession migration

--- database/seed-updates/20220725143118_add_us_history_presession_survey.sql
+++ database/seed-updates/20220725143118_add_us_history_presession_survey.sql
@@ -1,13 +1,71 @@
 -- migrate:up
 
--- delete the duplicate response choice options
-DELETE FROM
-  upchieve.survey_response_choices src1 USING upchieve.survey_response_choices src2
+-- Insert the new response choice
+INSERT INTO upchieve.survey_response_choices (score, choice_text, created_at, updated_at)
+VALUES (0, 'Other', NOW(), NOW()),
+       (1, 'Not at all', NOW(), NOW());
+
+-- Migrate existing submissions to the newly insert response choices
+UPDATE
+    upchieve.users_surveys_submissions
+SET
+    survey_response_choice_id = subquery.ids[1],
+    updated_at = NOW()
+FROM (
+    SELECT
+      array_agg(id ORDER BY created_at DESC) as ids
+    FROM
+        upchieve.survey_response_choices
+    WHERE
+        choice_text = 'Other'
+    ) AS subquery
 WHERE
-  src1.id > src2.id
-  AND src1.choice_text = src2.choice_text;
+    upchieve.users_surveys_submissions.survey_response_choice_id = ANY(subquery.ids);
 
 
+UPDATE
+    upchieve.users_surveys_submissions
+SET
+    survey_response_choice_id = subquery.ids[1],
+    updated_at = NOW()
+FROM (
+    SELECT
+      array_agg(id ORDER BY created_at DESC) as ids
+    FROM
+        upchieve.survey_response_choices
+    WHERE
+        choice_text = 'Not at all'
+    ) AS subquery
+WHERE
+    upchieve.users_surveys_submissions.survey_response_choice_id = ANY(subquery.ids);
+
+
+-- Delete older response choices of 'Other' and 'Not at all'
+WITH other_ids AS (
+    SELECT
+      (array_agg(id ORDER BY created_at DESC))[2:] as ids
+    FROM
+        upchieve.survey_response_choices
+    WHERE
+        choice_text = 'Other'
+)
+DELETE FROM upchieve.survey_response_choices
+WHERE (SELECT id = ANY (ids) FROM other_ids);
+
+
+WITH not_at_all_ids AS (
+    SELECT
+      (array_agg(id ORDER BY created_at DESC))[2:] as ids
+    FROM
+        upchieve.survey_response_choices
+    WHERE
+        choice_text = 'Not at all'
+)
+DELETE FROM upchieve.survey_response_choices
+WHERE (SELECT id = ANY (ids) FROM not_at_all_ids);
+
+
+-- Add U.S History Pre-Session Survey
 INSERT INTO upchieve.surveys (name, created_at, updated_at)
 VALUES ('U.S. History Pre-Session Survey', NOW(), NOW())
 ON CONFLICT ON CONSTRAINT surveys_name_key
@@ -82,7 +140,36 @@ WHERE
   (upchieve.surveys.name = 'U.S. History Pre-Session Survey' AND sq.question_text = 'Overall, how do you feel about %s?' AND rc.choice_text = 'Nervous' AND sub.text::int = 20) OR 
   (upchieve.surveys.name = 'U.S. History Pre-Session Survey' AND sq.question_text = 'Overall, how do you feel about %s?' AND rc.choice_text = 'Neutral' AND sub.text::int = 30) OR 
   (upchieve.surveys.name = 'U.S. History Pre-Session Survey' AND sq.question_text = 'Overall, how do you feel about %s?' AND rc.choice_text = 'Optimistic' AND sub.text::int = 40) OR 
-  (upchieve.surveys.name = 'U.S. History Pre-Session Survey' AND sq.question_text = 'Overall, how do you feel about %s?' AND rc.choice_text = 'Confident' AND sub.text::int = 50);
+  (upchieve.surveys.name = 'U.S. History Pre-Session Survey' AND sq.question_text = 'Overall, how do you feel about %s?' AND rc.choice_text = 'Confident' AND sub.text::int = 50) OR
+
+  -- migrate over old questions to new 'Other' and 'Not at all' response choices
+  (upchieve.surveys.name = 'STEM Pre-Session Survey' AND sq.question_text = 'What is your primary goal for today''s session?' AND rc.choice_text = 'Other' AND sub.text::int = 70) OR
+  (upchieve.surveys.name = 'Humanities Essays Pre-Session Survey' AND sq.question_text = 'What is your primary goal for today''s session?' AND rc.choice_text = 'Other' AND sub.text::int = 60) OR
+  (upchieve.surveys.name = 'Reading Pre-Session Survey' AND sq.question_text = 'What is your primary goal for today''s session?' AND rc.choice_text = 'Other' AND sub.text::int = 40) OR
+  (upchieve.surveys.name = 'College Planning Pre-Session Survey' AND sq.question_text = 'What is your primary goal for today''s session?' AND rc.choice_text = 'Other' AND sub.text::int = 60) OR
+  (upchieve.surveys.name = 'College Essays Pre-Session Survey' AND sq.question_text = 'What is your primary goal for today''s session?' AND rc.choice_text = 'Other' AND sub.text::int = 50) OR
+  (upchieve.surveys.name = 'College Applications Pre-Session Survey' AND sq.question_text = 'What is your primary goal for today''s session?' AND rc.choice_text = 'Other' AND sub.text::int = 60) OR
+  (upchieve.surveys.name = 'SAT Prep Pre-Session Survey' AND sq.question_text = 'What is your primary goal for today''s session?' AND rc.choice_text = 'Other' AND sub.text::int = 60) OR
+
+  (upchieve.surveys.name = 'Student Post-Session Survey' AND sq.question_text = 'Sorry to hear that, what happened?' AND rc.choice_text = 'Other' AND sub.text::int = 60) OR
+  (upchieve.surveys.name = 'General Volunteer Post-Session Survey' AND sq.question_text = 'Please select all that apply' AND rc.choice_text = 'Other' AND sub.text::int = 60) OR
+  (upchieve.surveys.name = 'SAT Prep Volunteer Post-Session Survey' AND sq.question_text = 'Please select all that apply' AND rc.choice_text = 'Other' AND sub.text::int = 60) OR
+  (upchieve.surveys.name = 'College Counseling Volunteer Post-Session Survey' AND sq.question_text = 'Please select all that apply' AND rc.choice_text = 'Other' AND sub.text::int = 60) OR
+
+
+  (upchieve.surveys.name = 'STEM Pre-Session Survey' AND sq.question_text = 'How well do you understand the topic you want to talk about today?' AND rc.choice_text = 'Not at all' AND sub.text::int = 10) OR
+  (upchieve.surveys.name = 'Humanities Essays Pre-Session Survey' AND sq.question_text = 'How well do you understand the topic you want to talk about today?' AND rc.choice_text = 'Not at all' AND sub.text::int = 10) OR
+  (upchieve.surveys.name = 'Reading Pre-Session Survey' AND sq.question_text = 'How well do you understand the topic you want to talk about today?' AND rc.choice_text = 'Not at all' AND sub.text::int = 10) OR
+  (upchieve.surveys.name = 'SAT Prep Pre-Session Survey' AND sq.question_text = 'How well do you understand the topic you want to talk about today?' AND rc.choice_text = 'Not at all' AND sub.text::int = 10) OR
+
+  (upchieve.surveys.name = 'Student Post-Session Survey' AND sq.question_text = 'Your goal for this session was to %s. Did UPchieve help you achieve your goal?' AND rc.choice_text = 'Not at all' AND sub.text::int = 10) OR
+  (upchieve.surveys.name = 'Student Post-Session Survey' AND sq.question_text = 'Overall, how supportive was your coach today?' AND rc.choice_text = 'Not at all' AND sub.text::int = 10) OR
+  (upchieve.surveys.name = 'Student Post-Session Survey' AND sq.question_text = 'Overall, how much did your coach push you to do your best work today?' AND rc.choice_text = 'Not at all' AND sub.text::int = 10) OR
+
+  (upchieve.surveys.name = 'General Volunteer Post-Session Survey' AND sq.question_text = '%s''s goal for this session was to %s. Were you able to help them achieve their goal?' AND rc.choice_text = 'Not at all' AND sub.text::int = 10) OR
+  (upchieve.surveys.name = 'SAT Prep Volunteer Post-Session Survey' AND sq.question_text = '%s''s goal for this session was to %s. Were you able to help them achieve their goal?' AND rc.choice_text = 'Not at all' AND sub.text::int = 10) OR
+  (upchieve.surveys.name = 'SAT Prep Volunteer Post-Session Survey' AND sq.question_text = 'Were there any student safety, academic integrity, or community guideline issues during this session?' AND rc.choice_text = 'Not at all' AND sub.text::int = 10) OR
+  (upchieve.surveys.name = 'College Counseling Volunteer Post-Session Survey' AND sq.question_text = '%s''s goal for this session was to %s. Were you able to help them achieve their goal?' AND rc.choice_text = 'Not at all' AND sub.text::int = 10);
 
 
 -- migrate:down
