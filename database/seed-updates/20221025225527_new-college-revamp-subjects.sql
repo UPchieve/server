c1a767d600aeec0ef9c509d782b0a2af1ee9f203
Trey Stevens <treykstevens.tks@gmail.com>
2022-10-31 17:11:26 -0600

    refactored college revamp migration

--- database/seed-updates/20221025225527_new-college-revamp-subjects.sql
+++ database/seed-updates/20221025225527_new-college-revamp-subjects.sql
@@ -9,9 +9,9 @@ WHERE
 
 INSERT INTO upchieve.subjects (name, display_name, display_order, topic_id, tool_type_id, created_at, updated_at)
 SELECT
-    'collegePrep',
-    'College Prep',
-    1,
+    new_subjects.name,
+    new_subjects.display_name,
+    new_subjects.display_priority::int,
     subquery.topic_id,
     subquery.tool_type_id,
     NOW(),
@@ -25,92 +25,9 @@ FROM (
         JOIN upchieve.tool_types ON upchieve.tool_types.name = 'documenteditor'
     WHERE
         upchieve.topics.name = 'college') AS subquery
-RETURNING
-    id AS ok;
-
-INSERT INTO upchieve.subjects (name, display_name, display_order, topic_id, tool_type_id, created_at, updated_at)
-SELECT
-    'collegeList',
-    'College List',
-    2,
-    subquery.topic_id,
-    subquery.tool_type_id,
-    NOW(),
-    NOW()
-FROM (
-    SELECT
-        upchieve.topics.id AS topic_id,
-        upchieve.tool_types.id AS tool_type_id
-    FROM
-        upchieve.topics
-        JOIN upchieve.tool_types ON upchieve.tool_types.name = 'documenteditor'
-    WHERE
-        upchieve.topics.name = 'college') AS subquery
-RETURNING
-    id AS ok;
-
-INSERT INTO upchieve.subjects (name, display_name, display_order, topic_id, tool_type_id, created_at, updated_at)
-SELECT
-    'collegeApps',
-    'Applications',
-    3,
-    subquery.topic_id,
-    subquery.tool_type_id,
-    NOW(),
-    NOW()
-FROM (
-    SELECT
-        upchieve.topics.id AS topic_id,
-        upchieve.tool_types.id AS tool_type_id
-    FROM
-        upchieve.topics
-        JOIN upchieve.tool_types ON upchieve.tool_types.name = 'documenteditor'
-    WHERE
-        upchieve.topics.name = 'college') AS subquery
-RETURNING
-    id AS ok;
-
-INSERT INTO upchieve.subjects (name, display_name, display_order, topic_id, tool_type_id, created_at, updated_at)
-SELECT
-    'applicationEssays',
-    'Application Essays',
-    4,
-    subquery.topic_id,
-    subquery.tool_type_id,
-    NOW(),
-    NOW()
-FROM (
-    SELECT
-        upchieve.topics.id AS topic_id,
-        upchieve.tool_types.id AS tool_type_id
-    FROM
-        upchieve.topics
-        JOIN upchieve.tool_types ON upchieve.tool_types.name = 'documenteditor'
-    WHERE
-        upchieve.topics.name = 'college') AS subquery
-RETURNING
-    id AS ok;
-
-INSERT INTO upchieve.subjects (name, display_name, display_order, topic_id, tool_type_id, created_at, updated_at)
-SELECT
-    'financialAid',
-    'Financial Aid',
-    5,
-    subquery.topic_id,
-    subquery.tool_type_id,
-    NOW(),
-    NOW()
-FROM (
-    SELECT
-        upchieve.topics.id AS topic_id,
-        upchieve.tool_types.id AS tool_type_id
-    FROM
-        upchieve.topics
-        JOIN upchieve.tool_types ON upchieve.tool_types.name = 'documenteditor'
-    WHERE
-        upchieve.topics.name = 'college') AS subquery
-RETURNING
-    id AS ok;
+    JOIN UNNEST(ARRAY[1, 2, 3, 4, 5], ARRAY['collegePrep', 'collegeList', 'collegeApps', 'applicationEssays', 'financialAid'], ARRAY['College Prep', 'College List', 'Applications', 'Application Essays', 'Financial Aid']) AS new_subjects (display_priority,
+        name,
+        display_name) ON TRUE;
 
 INSERT INTO upchieve.certifications (name, created_at, updated_at)
     VALUES ('collegePrep', NOW(), NOW()), ('collegeList', NOW(), NOW()), ('collegeApps', NOW(), NOW()), ('applicationEssays', NOW(), NOW()), ('financialAid', NOW(), NOW())
@@ -338,31 +255,15 @@ WHERE
 
 DELETE FROM upchieve.quiz_certification_grants USING upchieve.quizzes
 WHERE upchieve.quizzes.id = upchieve.quiz_certification_grants.quiz_id
-    AND upchieve.quizzes.name = 'collegePrep'
-    OR upchieve.quizzes.name = 'collegeList'
-    OR upchieve.quizzes.name = 'collegeApps'
-    OR upchieve.quizzes.name = 'applicationEssays'
-    OR upchieve.quizzes.name = 'financialAid';
+    AND upchieve.quizzes.name IN ('collegePrep', 'collegeList', 'collegeApps', 'applicationEssays', 'financialAid');
 
 DELETE FROM upchieve.certification_subject_unlocks USING upchieve.subjects
 WHERE upchieve.subjects.id = upchieve.certification_subject_unlocks.subject_id
-    AND upchieve.subjects.name = 'collegePrep'
-    OR upchieve.subjects.name = 'collegeList'
-    OR upchieve.subjects.name = 'collegeApps'
-    OR upchieve.subjects.name = 'applicationEssays'
-    OR upchieve.subjects.name = 'financialAid';
+    AND upchieve.subjects.name IN ('collegePrep', 'collegeList', 'collegeApps', 'applicationEssays', 'financialAid');
 
 DELETE FROM upchieve.certifications
-WHERE upchieve.certifications.name = 'collegePrep'
-    OR upchieve.certifications.name = 'collegeList'
-    OR upchieve.certifications.name = 'collegeApps'
-    OR upchieve.certifications.name = 'applicationEssays'
-    OR upchieve.certifications.name = 'financialAid';
+WHERE upchieve.certifications.name IN ('collegePrep', 'collegeList', 'collegeApps', 'applicationEssays', 'financialAid');
 
 DELETE FROM upchieve.subjects
-WHERE upchieve.subjects.name = 'collegePrep'
-    OR upchieve.subjects.name = 'collegeList'
-    OR upchieve.subjects.name = 'collegeApps'
-    OR upchieve.subjects.name = 'applicationEssays'
-    OR upchieve.subjects.name = 'financialAid';
+WHERE upchieve.subjects.name IN ('collegePrep', 'collegeList', 'collegeApps', 'applicationEssays', 'financialAid');
 
