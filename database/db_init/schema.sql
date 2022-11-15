34a55634330fd6fb52706e596fce7571c298b21e
Haley B-E <haley.brandterichsen@upchieve.org>
2022-11-15 16:03:08 -0500

    258: update db_init/schema

--- database/db_init/schema.sql
+++ database/db_init/schema.sql
@@ -1792,7 +1792,8 @@ CREATE TABLE upchieve.users (
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     updated_at timestamp with time zone DEFAULT now() NOT NULL,
     phone text,
-    mongo_id character varying(24)
+    mongo_id character varying(24),
+    other_signup_source text
 );
 
 
@@ -4570,4 +4571,5 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20220901190221'),
     ('20220916002003'),
     ('20221026004526'),
-    ('20221027064644');
+    ('20221027064644'),
+    ('20221114200757');
