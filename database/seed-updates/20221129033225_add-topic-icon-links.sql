9b9f1b673deb8b84568efc57334f2f64bcf0ac28
Trey Stevens <treykstevens.tks@gmail.com>
2022-11-29 12:19:56 -0600

    set icon_link to null when downgrading and add updated_at

--- database/seed-updates/20221129033225_add-topic-icon-links.sql
+++ database/seed-updates/20221129033225_add-topic-icon-links.sql
@@ -2,42 +2,48 @@
 UPDATE
     upchieve.topics
 SET
-    icon_link = 'https://cdn.upchieve.org/site-images/topic-icons/math.svg'
+    icon_link = 'https://cdn.upchieve.org/site-images/topic-icons/math.svg',
+    updated_at = NOW()
 WHERE
     name = 'math';
 
 UPDATE
     upchieve.topics
 SET
-    icon_link = 'https://cdn.upchieve.org/site-images/topic-icons/science.svg'
+    icon_link = 'https://cdn.upchieve.org/site-images/topic-icons/science.svg',
+    updated_at = NOW()
 WHERE
     name = 'science';
 
 UPDATE
     upchieve.topics
 SET
-    icon_link = 'https://cdn.upchieve.org/site-images/topic-icons/college.svg'
+    icon_link = 'https://cdn.upchieve.org/site-images/topic-icons/college.svg',
+    updated_at = NOW()
 WHERE
     name = 'college';
 
 UPDATE
     upchieve.topics
 SET
-    icon_link = 'https://cdn.upchieve.org/site-images/topic-icons/sat.svg'
+    icon_link = 'https://cdn.upchieve.org/site-images/topic-icons/sat.svg',
+    updated_at = NOW()
 WHERE
     name = 'sat';
 
 UPDATE
     upchieve.topics
 SET
-    icon_link = 'https://cdn.upchieve.org/site-images/topic-icons/english.svg'
+    icon_link = 'https://cdn.upchieve.org/site-images/topic-icons/english.svg',
+    updated_at = NOW()
 WHERE
     name = 'readingWriting';
 
 UPDATE
     upchieve.topics
 SET
-    icon_link = 'https://cdn.upchieve.org/site-images/topic-icons/social-studies.svg'
+    icon_link = 'https://cdn.upchieve.org/site-images/topic-icons/social-studies.svg',
+    updated_at = NOW()
 WHERE
     name = 'socialStudies';
 
@@ -45,7 +51,8 @@ WHERE
 UPDATE
     upchieve.topics
 SET
-    icon_link = ''
+    icon_link = NULL,
+    updated_at = NOW()
 WHERE
     name = 'math'
     OR name = 'science'
