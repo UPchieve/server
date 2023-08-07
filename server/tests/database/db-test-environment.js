0578b26f6c85f3381e502ba7f397b0583ab99a78
alexlindsay <alex.lindsay@upchieve.org>
2023-08-07 11:08:58 -0700

    Lint.

--- server/tests/database/db-test-environment.js
+++ server/tests/database/db-test-environment.js
@@ -5,7 +5,7 @@ class DbTestEnvironment extends NodeEnvironment {
 
   async setup() {
     super.setup()
-    const client =this.createTestClient(process.env.__TEST_DB_CONNECTION_STRING__)
+    const client = this.createTestClient(process.env.__TEST_DB_CONNECTION_STRING__)
     await client.query('SET search_path TO upchieve')
     this.global.__TEST_DB_CLIENT__ = client
   }
