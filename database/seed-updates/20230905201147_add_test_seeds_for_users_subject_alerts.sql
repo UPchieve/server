0b3191921f05b1e05cb02b8efde372bd52431bd0
Ravi Makhija <ravi.makhija.pi@gmail.com>
2023-09-06 09:47:52 -0400

    Add test seed migrations
    - Fix test seed data for table users_certifications so that it is
      consistent with table users_quizzes for usHistory.
    - Add test seed data for table users_subject_alerts.

---
+++ database/seed-updates/20230905201147_add_test_seeds_for_users_subject_alerts.sql
@@ -0,0 +1,137 @@
+-- migrate:up
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-9c76-e3ad-ce3108a3236f', 2, FALSE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-9c76-e3ad-ce3108a3236f', 3, FALSE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-9c76-e3ad-ce3108a3236f', 19, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-9c76-e3ad-ce3108a3236f', 10, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-9c76-e3ad-ce3108a3236f', 7, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-9c76-e3ad-ce3108a3236f', 11, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-9c76-e3ad-ce3108a3236f', 17, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-9c76-e3ad-ce3108a3236f', 4, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-9c76-e3ad-ce3108a3236f', 12, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-9c76-e3ad-ce3108a3236f', 18, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-9c76-e3ad-ce3108a3236f', 1, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-9c76-e3ad-ce3108a3236f', 6, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-9c76-e3ad-ce3108a3236f', 5, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-9c76-e3ad-ce3108a3236f', 24, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 2, FALSE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 3, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 19, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 10, FALSE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 7, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 11, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 17, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 4, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 12, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 18, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 1, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 6, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 15, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 5, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 26, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-f256-6fe1-596e028d34b1', 24, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-af9e-8f1d-815bf6891cf5', 2, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-af9e-8f1d-815bf6891cf5', 3, FALSE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-af9e-8f1d-815bf6891cf5', 10, FALSE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-af9e-8f1d-815bf6891cf5', 7, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-af9e-8f1d-815bf6891cf5', 11, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-af9e-8f1d-815bf6891cf5', 17, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-af9e-8f1d-815bf6891cf5', 4, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-af9e-8f1d-815bf6891cf5', 20, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-af9e-8f1d-815bf6891cf5', 12, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-af9e-8f1d-815bf6891cf5', 18, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-af9e-8f1d-815bf6891cf5', 1, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-af9e-8f1d-815bf6891cf5', 6, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-af9e-8f1d-815bf6891cf5', 5, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+INSERT INTO upchieve.users_subject_alerts (user_id, subject_id, alerts_on, created_at, updated_at)
+    VALUES ('01859800-bca8-af9e-8f1d-815bf6891cf5', 24, TRUE, '2023-09-06 10:00:00.0 -04:00'::timestamptz, '2023-09-06 10:00:00.0 -04:00'::timestamptz);
+
+-- migrate:down
+DELETE FROM upchieve.users_subject_alerts
+WHERE user_id IN ('01859800-bca8-9c76-e3ad-ce3108a3236f', '01859800-bca8-f256-6fe1-596e028d34b1', '01859800-bca8-af9e-8f1d-815bf6891cf5');
+
