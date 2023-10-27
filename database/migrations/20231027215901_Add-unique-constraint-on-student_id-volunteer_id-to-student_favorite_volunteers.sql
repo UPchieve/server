aecda3e42b8c00c1c8db6cd2d73bb0907a63430a
Jamie Kasulis <jamie.kasulis@upchieve.org>
2023-10-27 18:48:29 -0400

    Handle case where ALTER TABLE (a DDL statement which causes an implicit COMMIT right before it is executed) fails: Make sure we can still recover in the DOWN script by utilizing a temporary backup table that stores the original contents of student_favorite_volunteers

---
+++ database/migrations/20231027215901_Add-unique-constraint-on-student_id-volunteer_id-to-student_favorite_volunteers.sql
@@ -0,0 +1,61 @@
+-- migrate:up
+CREATE TEMPORARY TABLE student_favorite_volunteers_backup AS
+SELECT
+    *
+FROM
+    upchieve.student_favorite_volunteers;
+
+BEGIN TRANSACTION;
+-- Select the row with max updated_at for each unique (student_id, volunteer_id) combination from student_favorite_coaches.
+-- Then store these in a new table, unique_favorites.
+WITH uf AS (
+    SELECT
+        student_id,
+        volunteer_id,
+        updated_at,
+        created_at,
+        ROW_NUMBER() OVER (PARTITION BY student_id,
+            volunteer_id ORDER BY updated_at DESC) AS rn
+    FROM
+        upchieve.student_favorite_volunteers
+)
+SELECT
+    student_id,
+    volunteer_id,
+    updated_at,
+    created_at INTO TEMPORARY UniqueFavoritesTemp -- The # denotes a local temporary table
+FROM
+    uf
+WHERE
+    rn = 1;
+-- Now dump the contents of the old table and repopulate it with the contents of the temp table.
+DELETE FROM upchieve.student_favorite_volunteers;
+INSERT INTO upchieve.student_favorite_volunteers
+SELECT
+    *
+FROM
+    UniqueFavoritesTemp;
+-- Now add the unique constraint
+ALTER TABLE IF EXISTS upchieve.student_favorite_volunteers
+    ADD CONSTRAINT unique_student_id_volunteer_id UNIQUE (student_id, volunteer_id);
+DROP TABLE IF EXISTS UniqueFavoritesTemp;
+-- Only delete once everything else has run successfully.
+DROP TABLE IF EXISTS student_favorite_volunteers_backup;
+COMMIT;
+
+-- migrate:down
+ALTER TABLE IF EXISTS upchieve.student_favorite_volunteers
+    DROP CONSTRAINT IF EXISTS unique_student_id_volunteer_id;
+
+INSERT INTO upchieve.student_favorite_volunteers
+SELECT
+    *
+FROM
+    student_favorite_volunteers_backup
+WHERE (student_id, volunteer_id, created_at, updated_at)
+NOT IN (
+    SELECT
+        *
+    FROM
+        upchieve.student_favorite_volunteers);
+
