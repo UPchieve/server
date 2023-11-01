190a763f13aa725998c224354492577c7e9aed3e
Jamie Kasulis <jamie.kasulis@upchieve.org>
2023-10-31 21:16:45 -0400

    Create job to delete duplicates from student_favorite_volunteers

---
+++ server/scripts/delete-duplicate-student-favorite-volunteers.ts
@@ -0,0 +1,22 @@
+import * as StudentRepo from '../models/Student'
+import { log } from '../worker/logger'
+import { runInTransaction, TransactionClient } from '../db'
+export const deleteDuplicateStudentFavoriteVolunteers = async (): Promise<void> => {
+  const numDuplicates = await StudentRepo.countDuplicateStudentVolunteerFavorites()
+  if (numDuplicates === 0) {
+    log('Found 0 duplicates in student_favorite_volunteers. Returning')
+    return
+  }
+
+  await runInTransaction(async (tc: TransactionClient) => {
+    const numDeleted = await StudentRepo.deleteDuplicateStudentVolunteerFavorites(
+      tc
+    )
+    log(`Deleted ${numDeleted} duplicates from student_favorite_volunteers`)
+    if (numDeleted !== numDuplicates) {
+      log(
+        `Expected to delete ${numDuplicates} duplicates from student_favorite_volunteers, but actually deleted ${numDeleted}`
+      )
+    }
+  })
+}
