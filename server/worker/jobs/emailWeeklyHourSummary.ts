ec73e0fe19629da339657bab37ab5eb0b0d2a7cb
Trey Stevens <treykstevens.tks@gmail.com>
2023-11-03 14:09:44 +0000

    add flagging for allowing 0 hours

--- server/worker/jobs/emailWeeklyHourSummary.ts
+++ server/worker/jobs/emailWeeklyHourSummary.ts
@@ -11,6 +11,7 @@ import {
   updateVolunteerHourSummaryIntroById,
 } from '../../models/Volunteer/queries'
 import newrelic from 'newrelic'
+import { getWeeklySummaryAllHoursFlag } from '../../services/FeatureFlagService'
 
 // Runs weekly at 6am EST on Monday
 export default async (): Promise<void> => {
@@ -54,15 +55,17 @@ export default async (): Promise<void> => {
           lastMonday.toDate(),
           lastSunday.toDate()
         )
-      /*
-      The smallest this number can be is .01 hours =36 seconds (as per the rounding
-      in VolunteerService.ts:68-70) So users with 36-54 seconds of time will have
-      .01 hours coaching which gets rounded down to 0 hours/minutes at formatting
-      in MailService/index.js:87-99. So we need to check .01 hours in addition
-      to 0 prevent an email from getting sent that displays 0 hours of volutneering
-      TODO: clean up formatting rounding logic to round 30+ seconds up a minute
-      */
-      if (!summaryStats || summaryStats.totalVolunteerHours <= 0.01) continue
+
+      const isWeeklySummaryAllHoursActive = await getWeeklySummaryAllHoursFlag(
+        id
+      )
+      const allowZeroHours =
+        isWeeklySummaryAllHoursActive && !volunteerPartnerOrg
+      if (
+        !summaryStats ||
+        (!allowZeroHours && summaryStats.totalVolunteerHours <= 0.01)
+      )
+        continue
 
       await MailService.sendHourSummaryEmail(
         firstName,
