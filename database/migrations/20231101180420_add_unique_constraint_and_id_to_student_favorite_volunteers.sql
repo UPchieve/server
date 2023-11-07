b526ffdb1505082518c29b62b2cdcf7ad71de628
Jamie Kasulis <jamie.kasulis@upchieve.org>
2023-11-07 15:57:51 -0500

    use primary key instead of just a unique constraint; remove id column

--- database/migrations/20231101180420_add_unique_constraint_and_id_to_student_favorite_volunteers.sql
+++ database/migrations/20231101180420_add_unique_constraint_and_id_to_student_favorite_volunteers.sql
@@ -1,14 +1,8 @@
 -- migrate:up
 ALTER TABLE IF EXISTS upchieve.student_favorite_volunteers
-    ADD CONSTRAINT unique_student_id_volunteer_id UNIQUE (student_id, volunteer_id);
-
-ALTER TABLE IF EXISTS upchieve.student_favorite_volunteers
-    ADD COLUMN IF NOT EXISTS id SERIAL;
+    ADD PRIMARY KEY (student_id, volunteer_id);
 
 -- migrate:down
 ALTER TABLE IF EXISTS upchieve.student_favorite_volunteers
-    DROP CONSTRAINT IF EXISTS unique_student_id_volunteer_id;
-
-ALTER TABLE IF EXISTS upchieve.student_favorite_volunteers
-    DROP COLUMN IF EXISTS id;
+    DROP CONSTRAINT IF EXISTS student_favorite_volunteers_pkey;
 
