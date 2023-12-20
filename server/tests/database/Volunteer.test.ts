96f1c80a0611f6a168b4be4b00d52d66ba5d7ae0
Jamie Kasulis <jamie.kasulis@upchieve.org>
2023-12-20 10:45:41 -0500

    Remove test

--- server/tests/database/Volunteer.test.ts
+++ server/tests/database/Volunteer.test.ts
@@ -1,9 +1,4 @@
-import {
-  AdminUpdateVolunteer,
-  getNextVolunteerToNotify,
-  updateVolunteerForAdmin,
-} from '../../models/Volunteer'
-import { getUserContactInfoById } from '../../models/User'
+import { getNextVolunteerToNotify } from '../../models/Volunteer'
 
 test('Make a connection', async () => {
   const result = await getNextVolunteerToNotify({
@@ -16,26 +11,4 @@ test('Make a connection', async () => {
     favoriteVolunteers: undefined,
   })
   expect(result).toBeUndefined()
-})
-
-test('updateVolunteerForAdmin', async () => {
-  const userId = '01859800-bca8-af9e-8f1d-815bf6891cf5'
-  const update: AdminUpdateVolunteer = {
-    firstName: 'Partner',
-    lastName: 'UPchieve',
-    volunteerPartnerOrg: 'health-co',
-    isVerified: true,
-    isBanned: true, // changed
-    isDeactivated: false,
-    isApproved: true,
-    email: 'volunteer1@upchieve.org',
-  }
-  await updateVolunteerForAdmin(userId, update)
-
-  const resultantUser = await getUserContactInfoById(userId)
-  expect(resultantUser).toMatchObject({
-    id: userId,
-    email: update.email,
-    banned: update.isBanned,
-  })
 })
