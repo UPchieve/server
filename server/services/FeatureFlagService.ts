8152a0e0405b8f0593d4355abb3717fef721ba1d
alexlindsay <alex.lindsay@upchieve.org>
2024-01-02 13:26:56 -0800

    Remove references to unleash feature flags.

--- server/services/FeatureFlagService.ts
+++ server/services/FeatureFlagService.ts
@@ -1,49 +1,8 @@
-const { createApp } = require('@unleash/proxy')
-import { initialize } from 'unleash-client'
-import config from '../config'
 import { FEATURE_FLAGS } from '../constants'
 import { client as phClient } from '../posthog'
 import { Ulid } from '../models/pgUtils'
 
-/**
- * This creates a proxy server that the frontend can hit.
- * We were hitting rate limit issues from all of our users' browsers
- * hitting our Gitlab Unleash service, so we now cache flags on our server
- * and serve up the requests from there. The token is only for the paid
- * Unleash service, which we don't use, but it's a required parameter.
- */
-export const unleashProxy = createApp({
-  unleashUrl: config.unleashUrl,
-  unleashInstanceId: config.unleashId,
-  environment: config.unleashName,
-  unleashAppName: config.unleashName,
-  logLevel: 'debug',
-  refreshInterval: 5000,
-  unleashApiToken: 'notused',
-  clientKeys: [config.featureFlagClientKey],
-  port: config.featureFlagPort,
-})
-
-/**
- * This is the server itself (and the worker) reaching out to
- * the Gitlab Unleash instance. It only reaches out 1/s, so we're only
- * hitting Gitlab 1/s for as many web/worker instances we have.
- * We could theoretically hit the proxy above using the proxy client
- * in the node server (essentially reaching out to itself as a middle step)
- * but that felt like overengineering.
- */
-export const initializeUnleash = (): void => {
-  if (config.unleashId)
-    initialize({
-      url: config.unleashUrl,
-      appName: config.unleashName,
-      environment: config.unleashName,
-      instanceId: config.unleashId,
-      refreshInterval: 5000,
-    })
-}
-
-export async function isFeatureEnabled(
+async function isFeatureEnabled(
   featureFlagName: FEATURE_FLAGS,
   userId: Ulid
 ) {
@@ -55,6 +14,12 @@ export async function getFeatureFlagPayload(
   userId: Ulid
 ) {
   return await phClient.isFeatureEnabled(featureFlagName, userId)
+}
+
+export function isChatBotEnabled() {
+  // TODO: Either put this feature flag into PH, or remove
+  // references from code.
+  return false
 }
 
 export async function getStandardizedCertsFlag(userId: Ulid) {
