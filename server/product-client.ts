e8149812bfc498e5d3f0ba215a67ced14c2d5f4d
alexlindsay <alex.lindsay@upchieve.org>
2024-01-08 12:53:58 -0800

    Don't create PostHog instance if no valid token.

--- server/product-client.ts
+++ server/product-client.ts
@@ -1,6 +1,22 @@
 import config from './config'
 import { PostHog } from 'posthog-node'
 
-export const client = new PostHog(config.posthogToken, {
-  host: 'https://app.posthog.com',
-})
+// TODO: Handle local dev instance better.
+export const client =
+  config.posthogToken !== 'bogus'
+    ? new PostHog(config.posthogToken, {
+        host: 'https://app.posthog.com',
+      })
+    : {
+        isFeatureEnabled: () => false,
+        getFeatureFlagPayload: () => '',
+        getAllFlagsAndPayloads: () => {
+          return { featureFlags: {}, featureFlagPayloads: {} }
+        },
+        captureEvent: () => {
+          /* no-op */
+        },
+        shutdownAsync: () => {
+          /* no-op */
+        },
+      }
