dbcc8037ae7f43a50fca193f0a3f9f384df42b03
Ravi Makhija <ravi@palmtreecode.com>
2024-01-08 13:37:49 -0500

    Add seed migration for volunteer1 usHistory cert

---
+++ database/seed-updates/20240108182859_fix_volunteer1_certification_seed_data.sql
@@ -0,0 +1,12 @@
+-- migrate:up
+-- volunteer1@upchieve.org passed the usHistory quiz (quiz_id 32), but was missing the corresponding
+-- certification for usHistory (certification_id 23), so we add it here. 
+-- Also, note that the certification was also added in the below test seed generation code, and may 
+-- need to be synched with this type of migration in the future to e.g. avoid double insertion:
+-- database/seeds/testData/volunteers.ts
+insert into upchieve.users_certifications
+values ('01859800-bca8-af9e-8f1d-815bf6891cf5', 23, '2023-01-09 19:27:44.0+00'::timestamptz, '2023-01-09 19:27:44.0+00'::timestamptz);
+
+-- migrate:down
+delete from upchieve.users_certifications
+where user_id = '01859800-bca8-af9e-8f1d-815bf6891cf5' and certification_id = 23;
