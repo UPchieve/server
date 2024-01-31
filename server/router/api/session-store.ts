07cfeca161c2700f62b73dbfb5d2166d3e7a0c23
Jamie Kasulis <jamie.kasulis@upchieve.org>
2024-01-31 16:21:20 -0500

    Set up API to retrieve token after session middleware. Register csrf protection middleware.

--- server/router/api/session-store.ts
+++ server/router/api/session-store.ts
@@ -3,6 +3,8 @@ import CreatePgStore from 'connect-pg-simple'
 import config from '../../config'
 import { Express } from 'express'
 import { getClient } from '../../db'
+import { csrfSync } from 'csrf-sync'
+import logger from '../../logger'
 
 const PgStore = CreatePgStore(session)
 
@@ -24,6 +26,22 @@ export default function(app: Express) {
       },
     })
   )
+
+  // CSRF middleware - must be registered after session middleware
+  const { generateToken, csrfSynchronisedProtection } = csrfSync()
+
+  app.get('/api/csrftoken', (req, res) => {
+    const csrfToken = generateToken(req)
+    return res.json({ csrfToken })
+  })
+
+  app.use((req, res, next) => {
+    if (req.url === '/auth/login') {
+      next()
+    } else {
+      csrfSynchronisedProtection(req, res, next)
+    }
+  })
 
   return store
 }
