1344ffc6819de3ad8fb22f27b196a73d190a72e2
Trey Stevens <treykstevens.tks@gmail.com>
2024-02-05 23:31:03 -0600

    several ux updates

--- server/services/ProgressReportsService/index.ts
+++ server/services/ProgressReportsService/index.ts
@@ -31,6 +31,7 @@ import {
   getMessagesForFrontend,
   MessageForFrontend,
   UserSessionsFilter,
+  getSessionById,
 } from '../../models/Session'
 import { captureEvent } from '../AnalyticsService'
 import { EVENTS } from '../../constants'
@@ -86,7 +87,18 @@ export async function saveProgressReport(
   let reportId: Ulid = ''
   try {
     // Early exit if there is no report to save
-    if (!Object.keys(data.summary).length || !data.concepts.length) return
+    if (!Object.keys(data.summary).length)
+      throw new Error(
+        `No progress report summary created for user ${userId} on session ${sessionIds.join(
+          ','
+        )}`
+      )
+    if (!data.concepts.length)
+      throw new Error(
+        `No progress report concepts created for user ${userId} on session ${sessionIds.join(
+          ','
+        )}`
+      )
 
     reportId = await insertProgressReport(userId, 'pending')
 
@@ -207,7 +219,8 @@ export async function generateProgressReport(
           type ProgressInfoTypes = 'recommendation' | 'reason'
 
           type ProgressReportDetail = {
-            // Content elaborating on the focusArea and infoType for a concept, specific to the student's performance or needs
+            // Content elaborating on the focusArea and infoType for a concept, specific to the student's performance or needs. The response should be
+            // as if you're talking directly to the student
             content: string
             // Determines if the associated concept is categorized as a 'strength' or 'practiceArea', with flexibility for future assessment types
             focusArea: ProgressFocusAreas
@@ -217,7 +230,8 @@ export async function generateProgressReport(
           }
 
           type ProgressReportSummary = {
-            // Consolidated summary reflecting the overarching findings or conclusions from the assessment of all concepts
+            // Consolidated summary reflecting the overarching findings or conclusions from the assessment of all concepts. The response should be
+            // as if you're talking directly to the student
             summary: string
             // Aggregated grade representing the overall performance level in the subject, on a scale of 65-100
             overallGrade: number
@@ -261,6 +275,8 @@ export async function generateProgressReport(
 export async function queueGenerateProgressReportForUser(
   sessionId: Ulid
 ): Promise<void> {
+  const session = await getSessionById(sessionId)
+  if (session.subject !== 'reading') return
   await QueueService.add(
     Jobs.GenerateProgressReport,
     { sessionId },
