c406189e63bb8e34b221a571570d9756831e122e
Jamie Kasulis <jamie.kasulis@upchieve.org>
2024-05-17 17:04:27 -0400

    mr feedback

--- database/migrations/20240517164134_add-censored-messages-table.sql
+++ database/migrations/20240517164134_add-censored-messages-table.sql
@@ -1,10 +1,14 @@
 -- migrate:up
+CREATE TYPE moderation_system AS ENUM (
+    'regex'
+);
+
 CREATE TABLE IF NOT EXISTS upchieve.censored_session_messages (
     id uuid PRIMARY KEY,
-    sender_id uuid REFERENCES upchieve.users (id),
-    contents text,
+    sender_id uuid REFERENCES upchieve.users (id) NOT NULL,
+    message text,
     session_id uuid NOT NULL REFERENCES upchieve.sessions (id),
-    censored_by text NOT NULL,
+    censored_by moderation_system NOT NULL,
     created_at timestamp NOT NULL
 );
 
@@ -14,4 +18,6 @@ CREATE INDEX IF NOT EXISTS censored_messages_created_at ON upchieve.censored_ses
 
 -- migrate:down
 DROP TABLE IF EXISTS upchieve.censored_session_messages;
+
+DROP TYPE IF EXISTS moderation_system;
 
