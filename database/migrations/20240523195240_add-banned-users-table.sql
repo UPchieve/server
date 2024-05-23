f3b1708ab6cbd4c00220a9e4c58d2227be93cf69
Liseidy Bueno <liseidybueno@gmail.com>
2024-05-23 18:15:38 -0400

    lint fixes

--- database/migrations/20240523195240_add-banned-users-table.sql
+++ database/migrations/20240523195240_add-banned-users-table.sql
@@ -5,15 +5,14 @@ CREATE TYPE ban_types AS ENUM (
 );
 
 CREATE TABLE IF NOT EXISTS upchieve.banned_users (
-  id uuid PRIMARY KEY,
-  user_id uuid NOT NULL REFERENCES upchieve.users (id),
-  ban_type ban_types NOT NULL,
-  created_at timestamp NOT NULL,
-  updated_at timestamp NOT NULL,
-  ban_reason_id integer REFERENCES upchieve.ban_reasons (id)
-)
-
+    id uuid PRIMARY KEY,
+    user_id uuid NOT NULL REFERENCES upchieve.users (id),
+    ban_type ban_types NOT NULL,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL,
+    ban_reason_id integer REFERENCES upchieve.ban_reasons (id))
 -- migrate:down
 DROP TABLE IF EXISTS upchieve.banned_users;
+
 DROP TYPE IF EXISTS ban_types;
 
