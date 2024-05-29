78643de4be74c2596eed4ba72a8c1e0b2a4e5a6d
Liseidy Bueno <liseidybueno@gmail.com>
2024-05-29 17:04:36 -0300

    update getVolunteerContactInfoByIds to get banned user idf rom the DB

--- server/models/Volunteer/volunteer.sql
+++ server/models/Volunteer/volunteer.sql
@@ -13,7 +13,7 @@ FROM
     LEFT JOIN banned_users ON banned_users.user_id = users.id
 WHERE
     users.id = :userId!
-    AND banned_users.user_ID IS NULL
+    AND banned_users.user_id IS NULL
     AND users.deactivated IS FALSE
     AND users.test_user IS FALSE;
 
@@ -30,9 +30,10 @@ FROM
     users
     LEFT JOIN volunteer_profiles ON volunteer_profiles.user_id = users.id
     LEFT JOIN volunteer_partner_orgs ON volunteer_partner_orgs.id = volunteer_profiles.volunteer_partner_org_id
+    LEFT JOIN banned_users ON banned_users.user_id = users.id
 WHERE
     users.id = ANY (:userIds!)
-    AND users.banned IS FALSE
+    AND banned_users.user_id IS NULL
     AND users.deactivated IS FALSE
     AND users.test_user IS FALSE;
 
