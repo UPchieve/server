f2bb50fc1536036d53ac7c04ef9268a0f25464b0
Liseidy Bueno <liseidybueno@gmail.com>
2024-05-29 17:38:43 -0300

    update getVolunteersForNiceToMeetYou to get banned users from banned_users table

--- server/models/Volunteer/volunteer.sql
+++ server/models/Volunteer/volunteer.sql
@@ -783,8 +783,9 @@ FROM
     users
     JOIN volunteer_profiles ON volunteer_profiles.user_id = users.id
     LEFT JOIN volunteer_partner_orgs ON volunteer_partner_orgs.id = volunteer_profiles.volunteer_partner_org_id
+    LEFT JOIN banned_users ON banned_users.user_id = users.id
 WHERE
-    users.banned IS FALSE
+    banned_users.user_id IS NULL
     AND users.deactivated IS FALSE
     AND users.test_user IS FALSE
     AND users.created_at >= :start!
