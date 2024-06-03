fe7619d230a5bc97eb738c7920278c8836dbf221
Liseidy Bueno <liseidybueno@gmail.com>
2024-06-03 16:04:01 -0300

    lint fix

--- database/migrations/20240603184154_update-sessions-table-tin-include-ban-type.sql
+++ database/migrations/20240603184154_update-sessions-table-tin-include-ban-type.sql
@@ -2,12 +2,14 @@
 ALTER TABLE upchieve.sessions
     ADD COLUMN IF NOT EXISTS student_ban_type upchieve.ban_types;
 
-UPDATE upchieve.sessions
-SET student_ban_type = CASE
-    WHEN student_banned THEN 'complete'::upchieve.ban_types
-    ELSE NULL
-END;
-
+UPDATE
+    upchieve.sessions
+SET
+    student_ban_type = CASE WHEN student_banned THEN
+        'complete'::upchieve.ban_types
+    ELSE
+        NULL
+    END;
 
 -- migrate:down
 ALTER TABLE upchieve.sessions
