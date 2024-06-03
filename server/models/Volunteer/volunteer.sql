538d2de655193356f9e117f94e0148d83572c1c0
Liseidy Bueno <liseidybueno@gmail.com>
2024-06-03 18:13:06 -0300

    update getVolunteersForNiceToMeetYou query

--- server/models/Volunteer/volunteer.sql
+++ server/models/Volunteer/volunteer.sql
@@ -784,6 +784,7 @@ FROM
     LEFT JOIN volunteer_partner_orgs ON volunteer_partner_orgs.id = volunteer_profiles.volunteer_partner_org_id
 WHERE
     users.banned IS FALSE
+    AND users.ban_type IS NULL
     AND users.deactivated IS FALSE
     AND users.test_user IS FALSE
     AND users.created_at >= :start!
