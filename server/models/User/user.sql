ba3faf22506349d03e4735a19012871020823515
Liseidy Bueno <liseidybueno@gmail.com>
2024-06-03 17:16:06 -0300

    update getLegacyUser and legacyUser model

--- server/models/User/user.sql
+++ server/models/User/user.sql
@@ -495,6 +495,7 @@ SELECT
             FALSE
         END) AS is_admin,
     users.banned AS is_banned,
+    users.ban_type AS ban_type,
     ban_reasons.name AS ban_reason,
     users.test_user AS is_test_user,
     FALSE AS is_fake_user,
