ca5d41ba227dcc2b9b48d18dda4723b14712df1b
Liseidy Bueno <liseidybueno@gmail.com>
2024-06-07 14:26:56 -0300

    lint fix

--- server/services/UserService.ts
+++ server/services/UserService.ts
@@ -309,10 +309,17 @@ export async function adminUpdateUser(data: unknown) {
   }
 
   // if unbanning student, also unban their IP addresses
-  if (!isVolunteer && userBeforeUpdate.banType === USER_BAN_TYPES.COMPLETE && banType !== USER_BAN_TYPES.COMPLETE)
+  if (
+    !isVolunteer &&
+    userBeforeUpdate.banType === USER_BAN_TYPES.COMPLETE &&
+    banType !== USER_BAN_TYPES.COMPLETE
+  )
     await updateIpStatusByUserId(userBeforeUpdate.id, IP_ADDRESS_STATUS.OK)
 
-  if (userBeforeUpdate.banType !== USER_BAN_TYPES.COMPLETE && banType === USER_BAN_TYPES.COMPLETE)
+  if (
+    userBeforeUpdate.banType !== USER_BAN_TYPES.COMPLETE &&
+    banType === USER_BAN_TYPES.COMPLETE
+  )
     // TODO: queue email
     await MailService.sendBannedUserAlert(userId, 'admin')
 
