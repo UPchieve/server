5beba7716566660fddec9c8ad6ee7e09995ae41d
Liseidy Bueno <liseidybueno@gmail.com>
2024-06-21 18:01:44 -0300

    code review changes

--- database/migrations/20240612180331_add-shadowbanned-column-to-sessions.sql
+++ database/migrations/20240612180331_add-shadowbanned-column-to-sessions.sql
@@ -2,13 +2,6 @@
 ALTER TABLE upchieve.sessions
     ADD COLUMN shadowbanned boolean;
 
-UPDATE
-    upchieve.sessions
-SET
-    shadowbanned = TRUE
-WHERE
-    student_banned = TRUE;
-
 -- migrate:down
 ALTER TABLE upchieve.sessions
     DROP COLUMN shadowbanned;
