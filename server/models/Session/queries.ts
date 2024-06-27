caa28ddbb30e10dd2d27eb87ad1e274340350e46
Liseidy Bueno <liseidybueno@gmail.com>
2024-06-26 20:24:55 -0400

    remove banned from getSessionsForAdminFilter

--- server/models/Session/queries.ts
+++ server/models/Session/queries.ts
@@ -1077,7 +1077,7 @@ export async function getSessionsForAdminFilter(
       makeSomeOptional(v, [
         'volunteerEmail',
         'volunteerFirstName',
-        'volunteerIsBanned',
+        'volunteerBanType',
         'volunteerTestUser',
         'volunteerTotalPastSessions',
         'reviewReasons',
@@ -1098,13 +1098,13 @@ export async function getSessionsForAdminFilter(
       if (
         session.volunteerFirstName &&
         session.volunteerEmail &&
-        !!session.volunteerIsBanned &&
+        !!session.volunteerBanType &&
         !!session.volunteerTestUser &&
         !!session.volunteerTotalPastSessions
       ) {
         volunteer = {
           firstname: session.volunteerFirstName,
-          isBanned: session.volunteerIsBanned,
+          isBanned: session.volunteerBanType === USER_BAN_TYPES.COMPLETE,
           isTestUser: session.volunteerTestUser,
           totalPastSessions: session.volunteerTotalPastSessions,
         }
@@ -1113,7 +1113,8 @@ export async function getSessionsForAdminFilter(
 
       const student = {
         firstname: session.studentFirstName,
-        isBanned: session.studentIsBanned,
+        isBanned: session.studentBanType === USER_BAN_TYPES.COMPLETE,
+        isShadowBanned: session.studentBanType === USER_BAN_TYPES.SHADOW,
         isTestUser: session.studentTestUser,
         totalPastSessions: session.studentTotalPastSessions,
       }
