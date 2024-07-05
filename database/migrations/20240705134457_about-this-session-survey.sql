81aa6ae0216b397b2d36700acaa34e0260482cd5
Jamie Kasulis <jamie.kasulis@upchieve.org>
2024-07-05 14:37:27 -0400

    WIP - Insert into surveys_survey_questions, survey_questions_response_choices

--- database/migrations/20240705134457_about-this-session-survey.sql
+++ database/migrations/20240705134457_about-this-session-survey.sql
@@ -16,8 +16,6 @@ WHERE
     upchieve.surveys.name = 'About This Session Survey'
     AND upchieve.survey_types.name = 'about-this-session';
 
--- Question: Is this information helpful?
--- Answers: (0, Not helpful) or (1, Helpful)
 INSERT INTO upchieve.survey_response_choices (score, choice_text, display_image)
     VALUES (0, 'Not helpful', 'https://cdn.upchieve.org/site-images/thumbs-down.svg'), (1, 'Helpful', 'https://cdn.upchieve.org/site-images/thumbs-up.svg');
 
@@ -39,12 +37,77 @@ FROM
 WHERE
     qt.name = 'free response';
 
--- migrate:down
-DELETE FROM upchieve.surveys
-WHERE name = 'About This Session Survey';
+INSERT INTO upchieve.surveys_survey_questions (survey_id, survey_question_id, display_priority)
+SELECT
+    s.id,
+    sq.id,
+    10
+FROM
+    upchieve.surveys s
+    JOIN upchieve.survey_questions sq ON TRUE
+WHERE
+    s.name = 'About This Session Survey'
+    AND sq.question_text = 'Is this information helpful?';
 
-DELETE FROM upchieve.survey_types
-WHERE name = 'about-this-session';
+INSERT INTO upchieve.surveys_survey_questions (survey_id, survey_question_id, display_priority)
+SELECT
+    s.id,
+    sq.id,
+    20
+FROM
+    upchieve.surveys s
+    JOIN upchieve.survey_questions sq ON TRUE
+WHERE
+    s.name = 'About This Session Survey'
+    AND sq.question_text = 'What information would you like to see here?';
+
+INSERT INTO upchieve.survey_questions_response_choices (surveys_survey_question_id, response_choice_id, display_priority)
+SELECT
+    ssq.id,
+    rc.id,
+    10
+FROM
+    upchieve.surveys s
+    JOIN upchieve.surveys_survey_questions ssq ON ssq.survey_id = s.id
+    JOIN upchieve.survey_response_choices rc ON TRUE
+    JOIN upchieve.survey_questions sq ON sq.id = ssq.survey_question_id
+WHERE
+    s.name = 'About This Session Survey'
+    AND rc.choice_text = 'Not helpful'
+    AND rc.score = 0
+    AND sq.question_text = 'Is this information helpful?';
+
+INSERT INTO upchieve.survey_questions_response_choices (surveys_survey_question_id, response_choice_id, display_priority)
+SELECT
+    ssq.id,
+    rc.id,
+    20
+FROM
+    upchieve.surveys s
+    JOIN upchieve.surveys_survey_questions ssq ON ssq.survey_id = s.id
+    JOIN upchieve.survey_response_choices rc ON TRUE
+    JOIN upchieve.survey_questions sq ON sq.id = ssq.survey_question_id
+WHERE
+    s.name = 'About This Session Survey'
+    AND rc.choice_text = 'Helpful'
+    AND rc.score = 1
+    AND sq.question_text = 'Is this information helpful?';
+
+-- migrate:down
+WITH survey AS (
+    SELECT
+        *
+    FROM
+        upchieve.surveys
+    WHERE
+        name = 'About This Session Survey')
+DELETE FROM upchieve.surveys_survey_questions
+WHERE survey_id IN (
+        SELECT
+            id
+        FROM
+            survey
+        LIMIT 1);
 
 DELETE FROM upchieve.surveys_context
 WHERE survey_id = (
@@ -69,3 +132,13 @@ WHERE score = 1
 DELETE FROM upchieve.survey_questions
 WHERE question_text IN ('Is this information helpful?', 'What information would you like to see here?');
 
+DELETE FROM upchieve.surveys
+WHERE name = 'About This Session Survey';
+
+DELETE FROM upchieve.survey_types
+WHERE name = 'about-this-session';
+
+-- @TODO
+-- DELETE FROM upchieve.surveys_survey_questions
+-- @TODO
+-- DELETE FROM upchieve.survey_questions_response_choices
