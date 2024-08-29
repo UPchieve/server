1e7b45c75305dcaefc597178bef1ff8c6d85b1a0
Liseidy Bueno <liseidybueno@gmail.com>
2024-08-29 14:59:04 -0600

    add create assignment query

---
+++ server/models/Assignments/assignments.sql
@@ -0,0 +1,4 @@
+/* @name createAssignment */
+INSERT INTO assignments(id, class_id, description, title, number_of_sessions, min_duration_in_minutes, due_date, start_at, subject_id, created_at, updated_at)
+    VALUES (:id!, :classId!, :description, :title, :numberOfSessions, :minDurationInMinutes, :dueDate, :startAt, :subjectId, NOW(), NOW())
+RETURNING id, class_id, description, title, number_of_sessions, min_duration_in_minutes, due_date, start_at, subject_id, created_at, updated_at;
