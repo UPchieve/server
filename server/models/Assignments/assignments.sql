ef4b21c07f31cd07fba96cef1d28ae7f87c5d722
Liseidy Bueno <liseidybueno@gmail.com>
2024-08-29 16:08:11 -0600

    add getAssignmentsByClassId sql query

--- server/models/Assignments/assignments.sql
+++ server/models/Assignments/assignments.sql
@@ -24,3 +24,20 @@ FROM
 WHERE
     class_id = :classId!;
 
+/* @name getAssignmentsByClassId */
+SELECT 
+    id,
+    class_id,
+    description,
+    title,
+    number_of_sessions,
+    min_duration_in_minutes,
+    due_date,
+    start_at,
+    subject_id,
+    created_at,
+    updated_at
+FROM 
+    assignments
+WHERE 
+    class_id = :classId!;
