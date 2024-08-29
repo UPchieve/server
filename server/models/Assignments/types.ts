be2a95601891c6621de32dec8a8d18437617744c
Liseidy Bueno <liseidybueno@gmail.com>
2024-08-29 14:39:50 -0600

    create assignment type

---
+++ server/models/Assignments/types.ts
@@ -0,0 +1,17 @@
+import { Ulid } from '../pgUtils'
+
+export type Assignment = {
+  id: Ulid
+  class_id: Ulid
+  description?: string
+  title?: string
+  number_of_sessions?: Number
+  min_duration_in_minutes?: Number
+  due_date?: Date
+  start_at?: Date
+  subject_id: Number
+  created_at: Date
+  updatedAt: Date
+}
+
+export type CreateAssignmentPayload = Pick<Assignment, 'id' | 'class_id' | 'subject_id'>
