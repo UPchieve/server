54496b5d039e5a3cdd5adaeb25608a728471c7bd
Liseidy Bueno <liseidybueno@gmail.com>
2024-08-29 15:03:39 -0600

    change to camel case

--- server/models/Assignments/types.ts
+++ server/models/Assignments/types.ts
@@ -2,16 +2,16 @@ import { Ulid } from '../pgUtils'
 
 export type Assignment = {
   id: Ulid
-  class_id: Ulid
+  classId: Ulid
   description?: string
   title?: string
-  number_of_sessions?: Number
-  min_duration_in_minutes?: Number
-  due_date?: Date
-  start_at?: Date
-  subject_id?: Number
-  created_at: Date
+  numberOfSessions?: Number
+  minDurationInMinutes?: Number
+  dueDate?: Date
+  startAt?: Date
+  subjectId?: Number
+  createdAt: Date
   updatedAt: Date
 }
 
-export type CreateAssignmentPayload = Pick<Assignment, 'id' | 'class_id'>
+export type CreateAssignmentPayload = Pick<Assignment, 'id' | 'classId'>
