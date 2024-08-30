c8d60f6a7439469c83b4394b56ec0293e11dad21
Liseidy Bueno <liseidybueno@gmail.com>
2024-08-30 12:19:44 -0600

    lint fix

--- server/router/api/assignments.ts
+++ server/router/api/assignments.ts
@@ -3,13 +3,14 @@ import * as AssignmentsService from '../../services/AssignmentsService'
 import { resError } from '../res-error'
 
 export function routeAssignments(router: Router): void {
-  
   router.route('/assignment/:assignmentId').get(async function(req, res) {
     try {
       const assignmentId = req.params.assignmentId as string
-      const assignment = await AssignmentsService.getAssignmentById(assignmentId)
+      const assignment = await AssignmentsService.getAssignmentById(
+        assignmentId
+      )
       res.json({ assignment })
-    } catch(err) {
+    } catch (err) {
       resError(res, err)
     }
   })
