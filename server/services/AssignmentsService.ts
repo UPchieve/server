3a66fb97eb8390c35087f865bd8762c4579fa972
Liseidy Bueno <liseidybueno@gmail.com>
2024-08-30 13:44:51 -0600

    add create student assignment

--- server/services/AssignmentsService.ts
+++ server/services/AssignmentsService.ts
@@ -84,3 +84,17 @@ export async function getAssignmentsByStudentId(
 ): Promise<AssignmentsRepo.StudentAssignment[]> {
   return AssignmentsRepo.getAssignmentsByStudentId(userId)
 }
+
+export async function createStudentAssignment(
+  userId: Ulid,
+  assignmentId: Ulid
+) {
+  return runInTransaction(async (tc: TransactionClient) => {
+    const assignment = await AssignmentsRepo.createStudentAssignment(
+      userId,
+      assignmentId, 
+      tc
+    )
+    return assignment
+  })
+}
