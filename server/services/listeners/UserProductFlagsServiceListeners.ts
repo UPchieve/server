05e00c96ed7b9e926ac8106cb1d37e615e4fca43
Trey Stevens <treykstevens.tks@gmail.com>
2024-09-04 16:44:59 -0600

    add listeners for job on session metrics calculated

---
+++ server/services/listeners/UserProductFlagsServiceListeners.ts
@@ -0,0 +1,11 @@
+import { SESSION_EVENTS } from '../../constants/events'
+import * as UserProductFlagsService from '../UserProductFlagsService'
+import register from './register'
+
+export function listeners() {
+  register(
+    SESSION_EVENTS.SESSION_METRICS_CALCULATED,
+    UserProductFlagsService.queueFallIncentiveLeavingMoneyOnTableJob,
+    'queueFallIncentiveLeavingMoneyOnTableJob'
+  )
+}
