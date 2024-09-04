5f927a6d0c3f0d500f202fb586da23d6df709ac2
Trey Stevens <treykstevens.tks@gmail.com>
2024-09-03 19:13:02 -0600

    add service level functions for user product flags

---
+++ server/services/UserProductFlagsService.ts
@@ -0,0 +1,66 @@
+import { Ulid } from '../models/pgUtils'
+import { getSessionById } from '../models/Session'
+import { getUserVerificationInfoById } from '../models/User'
+import {
+  enrollStudentToFallIncentiveProgram,
+  getUPFByUserId,
+} from '../models/UserProductFlags'
+import QueueService from '../services/QueueService'
+import { Jobs } from '../worker/jobs'
+
+export async function checkIfInIncentiveProgram(userId: Ulid) {
+  const flags = await getUPFByUserId(userId)
+  if (flags?.fallIncentiveEnrollmentAt)
+    throw new Error(`You're already enrolled in the fall incentive program`)
+}
+
+export async function queueIncentiveProgramEnrollmentWelcomeJob(userId: Ulid) {
+  await QueueService.add(
+    Jobs.EmailFallIncentiveEnrollmentWelcome,
+    { userId },
+    { removeOnComplete: true, removeOnFail: true }
+  )
+}
+
+export async function queueInvitedToFallIncentiveReminderJob(userId: Ulid) {
+  await QueueService.add(
+    Jobs.EmailInvitedToFallIncentiveReminder,
+    { userId },
+    { removeOnComplete: true, removeOnFail: true }
+  )
+}
+
+export async function queueFallIncentiveMoneyCanBeMadeReminderJob(
+  sessionId: Ulid
+) {
+  const session = await getSessionById(sessionId)
+  await QueueService.add(
+    Jobs.EmailFallIncentiveMoneyOnTable,
+    { userId: session.studentId },
+    { removeOnComplete: true, removeOnFail: true }
+  )
+}
+
+export async function queueFallIncentiveSessionQualificationJob(
+  sessionId: Ulid
+) {
+  const session = await getSessionById(sessionId)
+  await QueueService.add(
+    Jobs.EmailFallIncentiveSessionQualification,
+    { userId: session.studentId },
+    { removeOnComplete: true, removeOnFail: true }
+  )
+}
+
+export async function incentiveProgramEnrollmentEnroll(userId: Ulid) {
+  await checkIfInIncentiveProgram(userId)
+  const userVerificationInfo = await getUserVerificationInfoById(userId)
+  if (!userVerificationInfo?.phoneVerified)
+    throw new Error(
+      'Your phone number must be verified before joining the program.'
+    )
+
+  const enrollmentDate = await enrollStudentToFallIncentiveProgram(userId)
+  await queueIncentiveProgramEnrollmentWelcomeJob(userId)
+  return enrollmentDate
+}
