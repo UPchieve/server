4f3578dc2df535fad9bc26fc5164aa5ed095a9c1
Liseidy Bueno <liseidybueno@gmail.com>
2024-09-04 15:49:01 -0400

    add is_required column, make class_id not null, add semicolon

--- database/migrations/20240828232026_create_assignments_table.sql
+++ database/migrations/20240828232026_create_assignments_table.sql
@@ -1,13 +1,14 @@
 -- migrate:up
 CREATE TABLE upchieve.assignments (
     id uuid PRIMARY KEY,
-    class_id uuid REFERENCES upchieve.teacher_classes (id),
+    class_id uuid NOT NULL REFERENCES upchieve.teacher_classes (id),
     description text,
     title text,
     number_of_sessions int,
     min_duration_in_minutes int,
     due_date timestamptz,
     start_at timestamptz,
+    is_required boolean NOT NULL DEFAULT FALSE,
     subject_id int REFERENCES upchieve.subjects (id),
     created_at timestamptz NOT NULL DEFAULT NOW(),
     updated_at timestamptz NOT NULL DEFAULT NOW()
