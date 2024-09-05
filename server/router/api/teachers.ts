e57d7d3c4da1a83c593115712e8cf6452978a648
Liseidy Bueno <liseidybueno@gmail.com>
2024-09-05 17:11:43 -0400

    add assignment data to route

--- server/router/api/teachers.ts
+++ server/router/api/teachers.ts
@@ -67,25 +67,18 @@ export function routeTeachers(app: Express, router: Router): void {
   /* Assignments */
   router.route('/assignment').post(async function(req, res) {
     try {
-      const classId = req.body.classId
-      const description = (req.body.description as string) ?? null
-      const title = (req.body.title as string) ?? null
-      const numberOfSessions = (req.body.numberOfSessions as number) ?? null
-      const minDurationInMinutes =
-        (req.body.minDurationInMinutes as number) ?? null
-      const dueDate = (req.body.dueDate as Date) ?? null
-      const startAt = (req.body.startDate as Date) ?? null
-      const subjectId = (req.body.subjectId as number) ?? null
-      const assignment = await AssignmentsService.createAssignment(
-        classId,
-        description,
-        title,
-        numberOfSessions,
-        minDurationInMinutes,
-        dueDate,
-        startAt,
-        subjectId
-      )
+      const assignmentData = {
+        classId: req.body.classId as string,
+        description: (req.body.description as string) ?? null,
+        title: (req.body.title as string) ?? null,
+        numberOfSessions: (req.body.numberOfSessions as number) ?? null,
+        minDurationInMinutes: (req.body.minDurationInMinutes as number) ?? null,
+        isRequired: req.body.isRequired as boolean,
+        dueDate: (req.body.dueDate as Date) ?? null,
+        startDate: (req.body.startDate as Date) ?? null,
+        subjectId: (req.body.subjectId as number) ?? null,
+      }
+      const assignment = await AssignmentsService.createAssignment(assignmentData)
       res.json({ assignment })
     } catch (err) {
       resError(res, err)
