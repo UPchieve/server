0bf907a6bfdc4f87a653ad454254e563a7c5cada
Liseidy Bueno <liseidybueno@gmail.com>
2024-09-05 16:41:01 -0400

    add checks and data param

--- server/services/AssignmentsService.ts
+++ server/services/AssignmentsService.ts
@@ -1,31 +1,42 @@
 import { runInTransaction, TransactionClient } from '../db'
 import { Ulid } from '../models/pgUtils'
 import * as AssignmentsRepo from '../models/Assignments'
+import * as SubjectsRepo from '../models/Subjects'
+import { NotAllowedError, InputError } from '../models/Errors'
 
-export async function createAssignment(
-  classId: Ulid,
-  description?: string,
-  title?: string,
-  numberOfSessions?: number,
-  minDurationInMinutes?: number,
-  dueDate?: Date,
-  startAt?: Date,
+type AssignmentInputdata = {
+  classId: Ulid
+  isRequired: boolean
+  description?: string
+  title?: string
+  numberOfSessions?: number
+  minDurationInMinutes?: number
+  dueDate?: Date
+  startDate?: Date
   subjectId?: number
-) {
+}
+
+export async function createAssignment(data: AssignmentInputdata) {
   return runInTransaction(async (tc: TransactionClient) => {
-    const assignment = await AssignmentsRepo.createAssignment(
-      {
-        classId,
-        description,
-        title,
-        numberOfSessions,
-        minDurationInMinutes,
-        dueDate,
-        startAt,
-        subjectId,
-      },
-      tc
-    )
+    const assignment = await AssignmentsRepo.createAssignment(data, tc)
+
+    const numSessions = data.numberOfSessions
+    const startDate = data.startDate
+    const dueDate = data.dueDate
+    const subjectId = data.subjectId
+
+    if (numSessions && numSessions <= 0)
+      throw new InputError('Number of sessions must be greater than 0.')
+    if (startDate && dueDate && startDate > dueDate)
+      throw new NotAllowedError('Start date cannot be after the due date.')
+    if (subjectId) {
+      const allSubjects = await SubjectsRepo.getSubjectsWithTopic()
+      const idExists = Object.values(allSubjects).some(
+        subject => subject.id === subjectId
+      )
+
+      if (!idExists) throw new InputError('Subject id is not valid')
+    }
     return assignment
   })
 }
