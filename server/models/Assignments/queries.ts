3244841420521c7eb64e5ec341f8bd2fb34fedf8
Liseidy Bueno <liseidybueno@gmail.com>
2024-09-09 14:39:47 -0400

    remove return statement

--- server/models/Assignments/queries.ts
+++ server/models/Assignments/queries.ts
@@ -86,8 +86,6 @@ export async function getAssignmentById(
         'startDate',
         'subjectId',
       ])
-    } else {
-      return undefined
     }
   } catch (err) {
     throw new RepoReadError(err)
