ac017483b66aaa8b39472c00ffd6909aadea2082
Jamie Kasulis <jamie.kasulis@upchieve.org>
2024-09-13 00:13:52 +0900

    Tweak migration and do db dump

--- database/migrations/20240912141821_create-tutor-bot-conversations-tables.sql
+++ database/migrations/20240912141821_create-tutor-bot-conversations-tables.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS upchieve.tutor_bot_conversations (
     id uuid PRIMARY KEY,
     user_id uuid NOT NULL REFERENCES upchieve.users (id),
-    session_id uuid DEFAULT NULL,
+    session_id uuid REFERENCES upchieve.sessions (id),
     created_at timestamptz DEFAULT now() NOT NULL,
     updated_at timestamptz DEFAULT now() NOT NULL
 );
@@ -17,15 +17,15 @@ CREATE TABLE IF NOT EXISTS upchieve.tutor_bot_conversation_messages (
     id uuid PRIMARY KEY,
     tutor_bot_conversation_id uuid NOT NULL REFERENCES upchieve.tutor_bot_conversations (id),
     sender_id uuid NOT NULL REFERENCES upchieve.users (id),
-    sender_user_type tutor_bot_conversation_user_type NOT NULL,
+    sender_user_type upchieve.tutor_bot_conversation_user_type NOT NULL,
     message text NOT NULL,
-    created_at timestampz DEFAULT now() NOT NULL
+    created_at timestamptz DEFAULT now() NOT NULL
 );
 
 -- migrate:down
+DROP TABLE IF EXISTS upchieve.tutor_bot_conversation_messages;
+
 DROP TABLE IF EXISTS upchieve.tutor_bot_conversations;
 
 DROP TYPE IF EXISTS upchieve.tutor_bot_conversation_user_type;
-
-DROP TABLE IF EXISTS upchieve.tutor_bot_conversation_messages;
 
