16298927f5b436c5e5fc1eb7eac312accf305384
ravi.makhija.pi <ravi.makhija.pi@gmail.com>
2024-10-04 03:57:05 +0000

    IDX Setup

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,12 +1,11 @@
 { pkgs, ... }: {
-  channel = "stable-24.05"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+  channel = "stable-24.05";
   packages = [
     pkgs.docker
     pkgs.docker-compose
     pkgs.postgresql_14
   ];
-  env = { SUBWAY_REDIS_HOST = "localhost"; SUBWAY_DB_HOST = "localhost"; NVM_DIR="~/.nvm"; PATH = ["~/.nvm/versions/node/v20.10.0/bin"]; };
+  env = { SUBWAY_REDIS_HOST = "localhost"; SUBWAY_DB_HOST = "localhost"; NVM_DIR="$HOME/.nvm"; PATH = ["$HOME/.nvm/versions/node/v20.10.0/bin"]; NPM_CONFIG_PREFIX=""; };
   services.docker.enable = true;
   idx = {
     extensions = [
@@ -16,8 +15,9 @@
     };
     workspace = {
       onCreate = {
-        nvm-node-npm-install = "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash && . ~/nvm.sh && . .idx/dev_setup.sh && unset NPM_CONFIG_PREFIX && npm install";
-        default.openFiles = [ "README.md" ];
+        install-node = "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash && \
+                        . $HOME/.nvm/nvm.sh && \
+                        nvm install v20.10.0";
       };
       onStart = {
         default.openFiles = [ "README.md" ];
