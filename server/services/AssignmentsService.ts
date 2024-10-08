55d677c126bce5388e3d1e0eda49d1778fc53d45
Liseidy Bueno <liseidybueno@gmail.com>
2024-10-08 12:50:27 -0400

    add changes to add assignment to select students

--- server/services/AssignmentsService.ts
+++ server/services/AssignmentsService.ts
@@ -41,7 +41,10 @@ export const asAssignment = asFactory<CreateAssignmentPayload>({
   title: asOptional(asString),
 })
 
-export async function createAssignment(data: CreateAssignmentPayload) {
+export async function createAssignment(
+  data: CreateAssignmentPayload,
+  studentIds: string[]
+) {
   return runInTransaction(async (tc: TransactionClient) => {
     const numSessions = data.numberOfSessions
     if (numSessions && numSessions <= 0)
@@ -71,7 +74,11 @@ export async function createAssignment(data: CreateAssignmentPayload) {
       tc
     )
 
-    await addAssignmentForClass(data.classId, assignment.id, tc)
+    if (studentIds.length > 0) {
+      await addAssignmentForStudents(studentIds, assignment.id, tc)
+    } else {
+      await addAssignmentForClass(data.classId, assignment.id, tc)
+    }
 
     return assignment
   })
