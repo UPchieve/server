424e54faaae51e4d702447a64758f67fc52ac2ca
Liseidy Bueno <liseidybueno@gmail.com>
2024-11-07 12:22:13 -0500

    add prioirty column to teachers classes

--- database/migrations/20241106175852_add-priority-column-to-teachers-classes.sql
+++ database/migrations/20241106175852_add-priority-column-to-teachers-classes.sql
@@ -1,2 +1,8 @@
 -- migrate:up
+ALTER TABLE upchieve.teacher_classes
+    ADD COLUMN is_priority BOOLEAN DEFAULT NULL;
+
 -- migrate:down
+ALTER TABLE upchieve.teacher_classes
+    DROP COLUMN IF EXISTS is_priority;
+
