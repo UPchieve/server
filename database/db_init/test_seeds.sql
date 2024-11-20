0992f05b2ec11a38ef40de4324cf100873915443
Jamie Kasulis <jamie.kasulis@upchieve.org>
2024-11-20 14:34:11 -0500

    Rename moderation_infractions.said_at -> created_at and session_audio_transcript_messages.created_at -> said_at

--- database/db_init/test_seeds.sql
+++ database/db_init/test_seeds.sql
@@ -359,6 +359,22 @@ COPY upchieve.availability_histories (id, user_id, weekday_id, available_start, 
 
 
 --
+-- Data for Name: session_audio_transcript_messages; Type: TABLE DATA; Schema: upchieve; Owner: admin
+--
+
+COPY upchieve.session_audio_transcript_messages (id, user_id, session_id, message, created_at, updated_at) FROM stdin;
+\.
+
+
+--
+-- Data for Name: censored_session_audio_transcript_messages; Type: TABLE DATA; Schema: upchieve; Owner: admin
+--
+
+COPY upchieve.censored_session_audio_transcript_messages (session_audio_transcript_message_id, message, created_at, updated_at) FROM stdin;
+\.
+
+
+--
 -- Data for Name: censored_session_messages; Type: TABLE DATA; Schema: upchieve; Owner: admin
 --
 
