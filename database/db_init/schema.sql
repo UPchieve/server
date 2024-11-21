601ccda2b77962cbb43f111518de2101e058d0cf
Jamie Kasulis <jamie.kasulis@upchieve.org>
2024-11-21 13:45:34 -0500

    Remove updated_at

--- database/db_init/schema.sql
+++ database/db_init/schema.sql
@@ -372,8 +372,7 @@ ALTER SEQUENCE upchieve.ban_reasons_id_seq OWNED BY upchieve.ban_reasons.id;
 CREATE TABLE upchieve.censored_session_audio_transcript_messages (
     session_audio_transcript_message_id uuid NOT NULL,
     message text NOT NULL,
-    said_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL
+    said_at timestamp with time zone DEFAULT now() NOT NULL
 );
 
 
@@ -1575,8 +1574,7 @@ CREATE TABLE upchieve.session_audio_transcript_messages (
     user_id uuid NOT NULL,
     session_id uuid NOT NULL,
     message text NOT NULL,
-    said_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL
+    said_at timestamp with time zone DEFAULT now() NOT NULL
 );
 
 
--- database/db_init/test_seeds.sql
+++ database/db_init/test_seeds.sql
@@ -362,7 +362,7 @@ COPY upchieve.availability_histories (id, user_id, weekday_id, available_start, 
 -- Data for Name: session_audio_transcript_messages; Type: TABLE DATA; Schema: upchieve; Owner: admin
 --
 
-COPY upchieve.session_audio_transcript_messages (id, user_id, session_id, message, said_at, updated_at) FROM stdin;
+COPY upchieve.session_audio_transcript_messages (id, user_id, session_id, message, said_at) FROM stdin;
 \.
 
 
@@ -370,7 +370,7 @@ COPY upchieve.session_audio_transcript_messages (id, user_id, session_id, messag
 -- Data for Name: censored_session_audio_transcript_messages; Type: TABLE DATA; Schema: upchieve; Owner: admin
 --
 
-COPY upchieve.censored_session_audio_transcript_messages (session_audio_transcript_message_id, message, said_at, updated_at) FROM stdin;
+COPY upchieve.censored_session_audio_transcript_messages (session_audio_transcript_message_id, message, said_at) FROM stdin;
 \.
 
 
