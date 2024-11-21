fcf12d3f8262770cdb1f784a5dcb1b3097d02c63
Jamie Kasulis <jamie.kasulis@upchieve.org>
2024-11-21 13:45:34 -0500

    Remove updated_at

--- database/migrations/20241120184235_censored-session-audio-transcript-messages.sql
+++ database/migrations/20241120184235_censored-session-audio-transcript-messages.sql
@@ -2,8 +2,7 @@
 CREATE TABLE IF NOT EXISTS upchieve.censored_session_audio_transcript_messages (
     session_audio_transcript_message_id uuid NOT NULL PRIMARY KEY REFERENCES upchieve.session_audio_transcript_messages (id),
     message text NOT NULL,
-    said_at timestamptz NOT NULL DEFAULT now(),
-    updated_at timestamptz NOT NULL DEFAULT now()
+    said_at timestamptz NOT NULL DEFAULT now()
 );
 
 -- migrate:down
