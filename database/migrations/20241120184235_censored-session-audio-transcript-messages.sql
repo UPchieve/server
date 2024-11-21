ca830af363f9a5b65902915e1e0a1d224241005b
Jamie Kasulis <jamie.kasulis@upchieve.org>
2024-11-21 14:24:14 -0500

    Remove said_at (since it is redundant) from censored transcripts table

--- database/migrations/20241120184235_censored-session-audio-transcript-messages.sql
+++ database/migrations/20241120184235_censored-session-audio-transcript-messages.sql
@@ -1,8 +1,7 @@
 -- migrate:up
 CREATE TABLE IF NOT EXISTS upchieve.censored_session_audio_transcript_messages (
     session_audio_transcript_message_id uuid NOT NULL PRIMARY KEY REFERENCES upchieve.session_audio_transcript_messages (id),
-    message text NOT NULL,
-    said_at timestamptz NOT NULL DEFAULT now()
+    message text NOT NULL
 );
 
 -- migrate:down
