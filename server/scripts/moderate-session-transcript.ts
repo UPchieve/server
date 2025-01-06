1dd794a06b2f441d3219ebed47d6c2a5d3fe7b77
Jamie Kasulis <jamie.kasulis@upchieve.org>
2025-01-06 17:43:34 -0500

    Flag sessions

--- server/scripts/moderate-session-transcript.ts
+++ server/scripts/moderate-session-transcript.ts
@@ -3,20 +3,41 @@ import { Job } from 'bull'
 import * as SessionService from '../services/SessionService'
 import * as ModerationService from '../services/ModerationService'
 import config from '../config'
+import { runInTransaction, TransactionClient } from '../db'
+import logger from '../logger'
+import {
+  updateSessionFlagsById,
+  updateSessionReviewReasonsById,
+} from '../models/Session'
+import { USER_SESSION_METRICS } from '../constants'
 
 export interface ModerateSessionTranscriptJobData {
   sessionId: Ulid
 }
 
-export default async function(jobData: Job<ModerateSessionTranscriptJobData>) {
+export default async function(job: Job<ModerateSessionTranscriptJobData>) {
   const transcript = await SessionService.getSessionTranscript(
-    jobData.data.sessionId
+    job.data.sessionId
   )
   const result = await ModerationService.moderateTranscript(transcript)
   console.log('Transcript moderation result', JSON.stringify(result, null, 2))
   const confidenceThreshold = config.contextualModerationConfidenceThreshold
   if (result.confidence >= confidenceThreshold) {
     console.log('Flagging session for review')
-    // @TODO: Flag the session and mark for review
+    await runInTransaction(async (tc: TransactionClient) => {
+      await updateSessionFlagsById(job.data.sessionId, [
+        USER_SESSION_METRICS.flaggedByModerationJob,
+      ])
+      // @TODO Can get more specific reasons from the model if we want.
+      await updateSessionReviewReasonsById(
+        job.data.sessionId,
+        [USER_SESSION_METRICS.flaggedByModerationJob],
+        false
+      ) // this also sets sessions.to_review = true
+    })
+    logger.info(
+      { sessionId: job.data.sessionId },
+      'Contextual moderation job flagged session for review'
+    )
   }
 }
