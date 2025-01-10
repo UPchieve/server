3bb62c0167cf4f9a132285ebe3bd105d9ebb4599
Liseidy Bueno <liseidybueno@gmail.com>
2025-01-10 13:42:29 -0300

    do not set up files

--- jest.config.database_ci.js
+++ jest.config.database_ci.js
@@ -3,7 +3,7 @@ const { defaults: tsjPreset } = require('ts-jest/presets')
 const FIVE_MINUTES_IN_MS = 5 * 60 * 1000
 
 module.exports = {
-  setupFiles: ['<rootDir>/server/tests/setup.ts'],
+  // setupFiles: ['<rootDir>/server/tests/setup.ts'],
   setupFilesAfterEnv: [
     '<rootDir>/server/tests/force-gc.ts',
     '<rootDir>/server/tests/database/db-mocks-setup.ts',
