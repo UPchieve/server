2e2fbe74412fc8fa8329a0f2fc9c3fcbafe4fb95
Liseidy Bueno <liseidybueno@gmail.com>
2025-01-21 14:59:08 -0300

    make changes to docker and ci

--- jest.config.database_ci.js
+++ jest.config.database_ci.js
@@ -2,9 +2,12 @@ const { defaults: tsjPreset } = require('ts-jest/presets')
 
 module.exports = {
   testEnvironment: '<rootDir>/server/tests/new_db/db-test-environment.js',
-  testMatch: ['<rootDir>/server/tests/new_db/*.test.(js|ts)'],
+  testMatch: ['<rootDir>/server/tests/database/*.test.(js|ts)'],
   testPathIgnorePatterns: ['/node_modules/'],
-  setupFilesAfterEnv: ['<rootDir>/server/tests/new_db/db-mocks-setup.ts'],
+  setupFilesAfterEnv: [
+    '<rootDir>/server/tests/force-gc.ts',
+    '<rootDir>/server/tests/new_db/db-mocks-setup.ts',
+  ],
   transform: tsjPreset.transform,
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 }
