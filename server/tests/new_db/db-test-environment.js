b77c38a4557fb869e48702b995abafa403b4ecaa
Liseidy Bueno <liseidybueno@gmail.com>
2025-01-22 15:20:12 -0300

    update init db func

--- server/tests/new_db/db-test-environment.js
+++ server/tests/new_db/db-test-environment.js
@@ -3,14 +3,13 @@ const Pool = require('pg').Pool
 const fs = require('fs').promises
 const path = require('path')
 
-const POSTGRES_USER = 'admin'
-const POSTGRES_PASSWORD = 'Password123'
+const POSTGRES_USER = process.env.POSTGRES_USER || 'admin'
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || 'Password123'
 const POSTGRES_HOST = process.env.CI ? 'postgres' : 'localhost'
-const POSTGRES_PORT = process.env.CI ? 5432 : 5500
-const DEFAULT_DB = 'upchieve'
+const POSTGRES_PORT = process.env.DB_PORT || (process.env.CI ? 5432 : 5500)
+const POSTGRES_DB = process.env.POSTGRES_DB || 'upchieve'
 
 let isInitialized = false
-let initializationPromise = null
 class DbTestEnvironment extends NodeEnvironment {
   constructor(config) {
     super(config)
@@ -20,6 +19,10 @@ class DbTestEnvironment extends NodeEnvironment {
   async setup() {
     await super.setup()
 
+    if (process.env.CI && !isInitialized) {
+      await this.initializeDatabase()
+    }
+
     try {
       this.testPool = new Pool({
         database: DEFAULT_DB,
@@ -28,17 +31,6 @@ class DbTestEnvironment extends NodeEnvironment {
         port: POSTGRES_PORT,
         host: POSTGRES_HOST,
       })
-
-      if (process.env.CI && !isInitialized) {
-        if (!initializationPromise) {
-          initializationPromise = this.initializeDatabase(this.testPool).then(
-            () => {
-              isInitialized = true
-            }
-          )
-        }
-        await initializationPromise
-      }
 
       this.testPool.on('connect', async client => {
         await client.query('SET search_path TO upchieve;')
@@ -51,15 +43,18 @@ class DbTestEnvironment extends NodeEnvironment {
     }
   }
 
-  async initializeDatabase(pool) {
-    const client = await pool.connect()
+  async initializeDatabase() {
+    if (isInitialized) return
+
+    const pool = new Pool({
+      database: POSTGRES_DB,
+      user: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      port: POSTGRES_PORT,
+      host: POSTGRES_HOST,
+    })
+
     try {
-      // const dropSchemas = ['upchieve', 'auth', 'basic_access', 'public']
-
-      // for (const schema of dropSchemas) {
-      //   await client.query(`DROP SCHEMA IF EXISTS ${schema} CASCADE;`)
-      // }
-
       for (const file of [
         'schema',
         'auth',
@@ -69,18 +64,28 @@ class DbTestEnvironment extends NodeEnvironment {
         'refresh_materialized_views',
       ]) {
         const filePath = path.join('database', 'db_init', `${file}.sql`)
-        console.log(`Executing ${filePath}...`)
+        console.log(`Reading ${filePath}...`)
         const sqlContent = await fs.readFile(filePath, 'utf-8')
-        await client.query(sqlContent)
+
+        console.log(`Executing ${filePath}...`)
+        const statements = sqlContent
+          .replace(/\r\n/g, '\n')
+          .split(';')
+          .map(s => s.trim())
+          .filter(s => s.length > 0)
+
+        for (const statement of statements) {
+          await pool.query(statement)
+        }
       }
+      isInitialized = true
     } catch (error) {
       console.error('Error initializing database:', error)
       throw error
     } finally {
-      client.release()
+      await pool.end()
     }
   }
-
   async teardown() {
     try {
       if (this.testPool) {
