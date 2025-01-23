85e86eb77eccd4d3bcc40cab5a436b16f4e41779
Liseidy Bueno <liseidybueno@gmail.com>
2025-01-23 12:46:43 -0300

    restructure code and do full ci

--- jest.global-teardown.ts
+++
@@ -1,6 +0,0 @@
-const { dropTestDatabase, closeConnections } = require('./jest.setup')
-
-module.exports = async () => {
-  await dropTestDatabase()
-  await closeConnections()
-}
--- server/tests/database/db-test-environment.js
+++ server/tests/database/db-test-environment.js
@@ -1,28 +1,49 @@
 const NodeEnvironment = require('jest-environment-node').TestEnvironment
 const Pool = require('pg').Pool
 
+const POSTGRES_USER = process.env.POSTGRES_USER || 'admin'
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || 'Password123'
+const POSTGRES_HOST = process.env.CI ? 'postgres' : 'localhost'
+const POSTGRES_PORT = process.env.DB_PORT || (process.env.CI ? 5432 : 5500)
+const DEFAULT_DB = process.env.POSTGRES_DB || 'upchieve'
+
 class DbTestEnvironment extends NodeEnvironment {
+  constructor(config) {
+    super(config)
+    this.testPool
+  }
 
   async setup() {
-    super.setup()
-    const client = this.createTestClient(process.env.__TEST_DB_CONNECTION_STRING__)
-    await client.query('SET search_path TO upchieve')
-    this.global.__TEST_DB_CLIENT__ = client
+    await super.setup()
+    try {
+      this.testPool = new Pool({
+        database: DEFAULT_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+        port: POSTGRES_PORT,
+        host: POSTGRES_HOST,
+      })
+
+      this.testPool.on('connect', async client => {
+        await client.query('SET search_path TO upchieve;')
+      })
+      this.global.__TEST_DB_CLIENT__ = this.testPool
+      this.global.__TEST_DB_NAME__ = this.testDbName
+    } catch (error) {
+      console.error('Error setting up test database:', error)
+      throw error
+    }
   }
 
   async teardown() {
-    this.global.__TEST_DB_CLIENT__?.end()
-    super.teardown()
-  }
-
-  createTestClient(connectionString) {
-    return new Pool({
-      connectionString,
-      allowExitOnIdle: true,
-      connectionTimeoutMillis: 0,
-      idleTimeoutMillis: 0,
-      ssl: false,
-    })
+    try {
+      if (this.testPool) {
+        await this.testPool.end()
+      }
+    } catch (error) {
+      console.error('Error tearing down test database:', error)
+    }
+    await super.teardown()
   }
 }
 
