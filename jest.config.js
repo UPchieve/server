48e5e5dab1dd63cf511e0ce57c5f5758a08eb06c
Liseidy Bueno <liseidybueno@gmail.com>
2025-01-23 14:30:45 -0300

    remove jest.config.js

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testMatch: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['<rootDir>/server'],
-  testEnvironment: 'node',
-}
