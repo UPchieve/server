4d1d3eaf7b7a6732a0a07834af4386bcb880e3f6
Liseidy Bueno <liseidybueno@gmail.com>
2025-01-23 11:59:57 -0300

    use before script for postgresql db

--- jest.config.database_ci.js
+++ jest.config.database_ci.js
@@ -8,7 +8,5 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/server/tests/database/db-mocks-setup.ts'],
   transform: tsjPreset.transform,
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  globalSetup: '<rootDir>/server/tests/new_db/global-db-setup.js',
-  globalTeardown: '<rootDir>/server/tests/new_db/global-db-teardown.js',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 }
