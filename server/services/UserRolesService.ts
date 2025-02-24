2760e810dd3795ebceb9a099062162b587de1225
Jamie Kasulis <jamie.kasulis@upchieve.org>
2025-02-24 10:36:54 -0500

    Create RoleContext class to drive user role determination

--- server/services/UserRolesService.ts
+++ server/services/UserRolesService.ts
@@ -2,7 +2,13 @@ import { getClient, TransactionClient } from '../db'
 import { Ulid } from '../models/pgUtils'
 import * as UserRepo from '../models/User'
 import { UserRole } from '../models/User'
+import * as CacheService from '../cache'
+import { KeyNotFoundError } from '../cache'
+import config from '../config'
 
+/**
+ * @deprecated Use {@link RoleContext} instead
+ */
 export async function getUserRolesById(
   userId: Ulid,
   tc: TransactionClient = getClient()
@@ -16,6 +22,9 @@ export async function getUserRolesById(
   }
 }
 
+/**
+ * @deprecated Use {@link RoleContext} instead
+ */
 export function getUserTypeFromRoles(roles: UserRole[] = [], userId: Ulid) {
   const userTypes = roles.filter(r => r !== 'admin')
   // For now, we assume all users have one role, not including admin.
@@ -28,14 +37,81 @@ export function getUserTypeFromRoles(roles: UserRole[] = [], userId: Ulid) {
   return userTypes[0]
 }
 
+/**
+ * @deprecated Use {@link RoleContext} instead
+ */
 export function isVolunteerUserType(userType: UserRole) {
   return userType === 'volunteer'
 }
 
+/**
+ * @deprecated Use {@link RoleContext} instead
+ */
 export function isStudentUserType(userType: UserRole) {
   return userType === 'student'
 }
 
+/**
+ * @deprecated Use {@link RoleContext} instead
+ */
 export function isTeacherUserType(userType: UserRole) {
   return userType === 'teacher'
+}
+
+export class RoleContext {
+  readonly roles: UserRole[]
+  readonly activeRole: UserRole
+
+  constructor(roles: UserRole[], activeRole: UserRole) {
+    this.roles = roles
+    this.activeRole = activeRole
+  }
+
+  isActiveRole(role: UserRole) {
+    return this.activeRole === role
+  }
+
+  hasRole(role: UserRole) {
+    return this.roles.includes(role)
+  }
+
+  isAdmin() {
+    // @TODO Can just use hasRole.
+    return this.roles.includes('admin')
+  }
+}
+
+export async function getRoleContext(
+  userId: string,
+  tc?: TransactionClient
+): Promise<RoleContext | undefined> {
+  try {
+    const key = `${config.cacheKeys.userRoleContextPrefix}${userId}`
+    const roleContextStr = await CacheService.get(key)
+    const data: { activeRole: UserRole; roles: UserRole[] } = JSON.parse(
+      roleContextStr
+    )
+    return new RoleContext(data.roles, data.activeRole)
+  } catch (err) {
+    if (!(err instanceof KeyNotFoundError)) {
+      throw err
+    }
+    // On cache miss: Create RoleContext from DB and save to cache
+    const roles = await UserRepo.getUserRolesById(userId, tc ?? getClient())
+    const activeRole = roles.filter(
+      r => r === 'volunteer' || r === 'student' || r === 'teacher'
+    )[0] // @TODO Handle failure.
+    const roleContext = new RoleContext(roles, activeRole)
+    await updateRoleContext(userId, roles, activeRole)
+    return roleContext
+  }
+}
+
+export async function updateRoleContext(
+  userId: string,
+  roles: UserRole[],
+  activeRole: UserRole
+): Promise<void> {
+  const key = `${config.cacheKeys.userRoleContextPrefix}${userId}`
+  await CacheService.save(key, JSON.stringify({ roles, activeRole }))
 }
