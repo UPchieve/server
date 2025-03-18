7adcbf2aebb0d452eac2730dd69a2e92478f55e7
Sammy Nave <sammy.nave@gmail.com>
2025-03-18 15:48:30 -0400

    schema

--- database/migrations/20250318175742_add_shareable_domains_table.sql
+++ database/migrations/20250318175742_add_shareable_domains_table.sql
@@ -1,9 +1,5 @@
 -- migrate:up
-<<<<<<< HEAD
 CREATE TABLE upchieve.shareable_domains (
-=======
-CREATE TABLE shareable_domains (
->>>>>>> 86944335 (Adds shareable_domains table)
     id serial PRIMARY KEY,
     domain VARCHAR(255) NOT NULL UNIQUE,
     created_at timestamp with time zone DEFAULT NOW(),
@@ -11,9 +7,4 @@ CREATE TABLE shareable_domains (
 );
 
 -- migrate:down
-<<<<<<< HEAD
 DROP TABLE upchieve.shareable_domains;
-=======
-DROP TABLE shareable_domains;
->>>>>>> 86944335 (Adds shareable_domains table)
-
