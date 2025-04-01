ee97a6731f9b410f95c672778da5332e4fb8d746
Trey Stevens <treykstevens.tks@gmail.com>
2025-04-01 11:47:15 -0600

    add new feature flag for getting started

--- server/services/FeatureFlagService.ts
+++ server/services/FeatureFlagService.ts
@@ -1,6 +1,6 @@
 import { FEATURE_FLAGS } from '../constants'
 import { client as productClient } from '../product-client'
-import { Ulid } from '../models/pgUtils'
+import { Ulid, Uuid } from '../models/pgUtils'
 import { timeLimit } from '../utils/time-limit'
 import * as AnalyticsService from './AnalyticsService'
 import { TUTOR_BOT_MODELS } from './TutorBotService'
@@ -161,4 +161,8 @@ export async function getTutorBotSubjectModelsPayload(
 
 export async function isTremendousEmbeddedRewardsEnabled(userId: Ulid) {
   return await isFeatureEnabled(FEATURE_FLAGS.TREMENDOUS_EMBDED_REWARDS, userId)
+}
+
+export async function isTeacherGettingStartedEnabled(userId: Uuid) {
+  return await isFeatureEnabled(FEATURE_FLAGS.TEACHER_GETTING_STARTED, userId)
 }
