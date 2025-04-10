8d5cc0f3a549699d6f2bf8e555d4d0269cf5834d
Liseidy Bueno <liseidybueno@gmail.com>
2025-04-10 13:42:50 -0300

    add get session summary function to service and queries

--- server/services/SessionSummariesService.ts
+++ server/services/SessionSummariesService.ts
@@ -1,17 +1,16 @@
 import { Ulid } from '../models/pgUtils'
 import * as SessionRepo from '../models/Session/queries'
 import * as ProgressReportsService from './ProgressReportsService'
-import { SubjectAndTopic, getSubjectAndTopic } from '../models/Subjects'
-import { TOOL_TYPES } from '../constants'
+import { getSubjectAndTopic } from '../models/Subjects'
+import { TOOL_TYPES, USER_ROLES } from '../constants'
 import * as LangfuseService from './LangfuseService'
 import { openai } from './BotsService'
-import logger, { logError } from '../logger'
+import logger from '../logger'
+import * as SessionSummariesRepo from '../models/SessionSummaries/queries'
 
-export async function generateTeacherSessionSummary(
-  sessionId: Ulid
-){
+export async function generateTeacherSessionSummary(sessionId: Ulid) {
   const session = await SessionRepo.getSessionById(sessionId)
-  if(!session.volunteerId) return
+  if (!session.volunteerId) return
   const subjectData = await getSubjectAndTopic(session.subject)
   if (!subjectData)
     throw new Error(
@@ -20,21 +19,38 @@ export async function generateTeacherSessionSummary(
 
   const messages = await SessionRepo.getMessagesForFrontend(session.id)
 
-  const sessionWithMessages = { ...session, subjectName: subjectData.subjectName, topicName: subjectData.topicName, messages}
+  const sessionWithMessages = {
+    ...session,
+    subjectName: subjectData.subjectName,
+    topicName: subjectData.topicName,
+    messages,
+  }
 
   const botPrompt = await ProgressReportsService.formatSessionsForBotPrompt(
-      [sessionWithMessages],
-      subjectData.toolType as TOOL_TYPES
-    )
+    [sessionWithMessages],
+    subjectData.toolType as TOOL_TYPES
+  )
 
   const systemPrompt = `You are generating a one sentence summary of an online tutoring session between a student and a tutor for the student's teacher to view.`
 
-  const summary = await generateSessionSummary(session.studentId, systemPrompt, botPrompt)
+  const summary = await generateSessionSummary(
+    session.studentId,
+    systemPrompt,
+    botPrompt
+  )
 
-  //save summary
-  //get summary
-  //return summary
+  const savedSummary = await SessionSummariesRepo.addSessionSummary(
+    session.id,
+    summary,
+    USER_ROLES.TEACHER
+  )
+  return savedSummary
+}
 
+export async function getLatestSessionSummary(sessionId: Ulid) {
+  const sessionSummary =
+    await SessionSummariesRepo.getLatestSessionSummary(sessionId)
+  return sessionSummary
 }
 
 const LF_TRACE_NAME = 'teacherSessionSummary'
@@ -44,7 +60,7 @@ export async function generateSessionSummary(
   userId: Ulid,
   systemPrompt: string,
   botPrompt: string
-): Promise<string | null> {
+): Promise<string> {
   const t = LangfuseService.getClient().trace({
     name: LF_TRACE_NAME,
     userId,
@@ -75,5 +91,5 @@ export async function generateSessionSummary(
   logger.info(
     `User: ${userId} received session summary completion ${completion} with response ${response}`
   )
-  return response ? JSON.parse(response) : null
+  return response ? JSON.parse(response) : ''
 }
