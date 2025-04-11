c91fb52da9fae60e791edcf64c6d2be078c31dc7
Liseidy Bueno <liseidybueno@gmail.com>
2025-04-11 16:39:07 -0300

    fix error name

--- server/services/SessionSummariesService.ts
+++ server/services/SessionSummariesService.ts
@@ -18,7 +18,7 @@ export async function generateSessionSummaryByUserType(
   const subjectData = await getSubjectAndTopic(session.subject)
   if (!subjectData)
     throw new Error(
-      `generateProgressReportForUser: No subject named ${session.subject} found`
+      `generateSessionSummaryByUserType: No subject named ${session.subject} found`
     )
 
   const messages = await SessionRepo.getMessagesForFrontend(session.id)
