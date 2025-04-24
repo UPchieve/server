9d738ca25429b64f7881a1697b9cd4b4a0b91556
Jamie Kasulis <jamie.kasulis@upchieve.org>
2025-04-24 17:32:47 -0400

    Add ignore ability. Update output

--- bin/pool_client_check.sh
+++ bin/pool_client_check.sh
@@ -5,9 +5,9 @@ EXIT_CODE=0
 echo "Checking for instances of PoolClient.connect()"
 echo ""
 
-MATCHES=$(grep -rn --include="*.js" --include="*.ts" --exclude="./server/db.ts" -E '.*[a-zA-Z0-9_]*client\.connect\(\).*|.*[a-zA-Z0-9_]*Client\.connect\(\).*|.*getClient\(\)\.connect\(\).*' ./server)
+MATCHES=$(grep -rn --include="*.js" --include="*.ts" --exclude="./server/db.ts" -E '.*[a-zA-Z0-9_]*client\.connect\(\).*|.*[a-zA-Z0-9_]*Client\.connect\(\).*|.*getClient\(\)\.connect\(\).*' ./server | grep -v "// ignore-pool-client-connect-check")
 if [ ! -z "$MATCHES" ]; then
-  echo "Found client.connect() in files:"
+  echo "🚫 Found potential use of PoolClient.connect() in files:"
   echo ""
   echo "$MATCHES"
   echo ""
@@ -17,7 +17,7 @@ fi
 if [ "$EXIT_CODE" -eq 0 ]; then
   echo "✅ No uses of PoolClient.connect() detected"
 else
-  echo "🚫 Detected potential use of PoolClient.connect()! Please fix before committing."
+  echo "⚠️ Please fix before committing, or append this to the end of the line: // ignore-pool-client-connect-check"
   echo ""
   echo "Why do this? See retrospective: https://gitlab.com/groups/upchieve/-/wikis/0-On-Call-Rotation/z-Retrospectives/2025-04-14?redirected_from=0-On-Call-Rotation/z-Retrospectives/2025.04.14"
 fi
