a904d0a80b86fd2354fb7dceb486e043576e8c7e
Jamie Kasulis <jamie.kasulis@upchieve.org>
2025-05-14 17:51:18 -0400

    Push most of the logic into SQL instead

--- server/worker/jobs/backfill-onboarded-status-for-combined-quiz-users.ts
+++ server/worker/jobs/backfill-onboarded-status-for-combined-quiz-users.ts
@@ -5,7 +5,7 @@
 import { User } from '../../models/User'
 import { getClient, runInTransaction, TransactionClient } from '../../db'
 import { camelCaseKeys } from '../../tests/db-utils'
-import { UserQuiz, VolunteerContactInfo } from '../../models/Volunteer'
+import { VolunteerContactInfo } from '../../models/Volunteer'
 import {
   ACCOUNT_USER_ACTIONS,
   EVENTS,
@@ -18,14 +18,35 @@ import logger from '../../logger'
 
 const client = getClient()
 const logPrefix = 'backfillOnboardedStatus: '
-async function getUsersWhoAreApprovedButNotOnboarded(): Promise<User[]> {
+async function getUsers(): Promise<User[]> {
   try {
     const result = await client.query(`
-    SELECT u.*
-    FROM users u
-    JOIN volunteer_profiles vp on vp.user_id = u.id
-    WHERE vp.approved IS TRUE AND vp.onboarded IS FALSE
-    AND u.ban_type IS NULL AND u.banned IS FALSE AND u.deactivated IS FALSE
+    with onboarded_not_approved_users as (select u.created_at, u.last_activity_at, u.*, uq.updated_at as upchieve_101_passed_at
+from users u
+join volunteer_profiles vp on vp.user_id = u.id
+left join users_quizzes uq on uq.user_id = u.id
+where vp.approved is true and vp.onboarded is false
+and exists (select * from user_actions where vp.user_id = u.id and action = 'COMPLETED BACKGROUND INFORMATION')
+and exists (select * from user_actions where vp.user_id = u.id and action = 'UNLOCKED SUBJECT')
+and exists (select * from user_actions where vp.user_id = u.id and action = 'ADDED PHOTO ID')
+and exists (select * from user_actions where vp.user_id = u.id and action = 'UPDATED AVAILABILITY')
+and exists (select * from users_quizzes where quiz_id = 22 and user_id = u.id and passed is true)
+and exists (select * from users_quizzes where quiz_id <> 22 and user_id = u.id and passed is true)
+and u.ban_type is null and u.banned is false and u.deactivated is false
+and uq.passed is true and uq.quiz_id = 22
+order by u.created_at)
+SELECT distinct id
+FROM onboarded_not_approved_users onau
+WHERE EXISTS (
+    SELECT 1
+    FROM users_quizzes uq2
+    WHERE
+        uq2.user_id = onau.id
+        AND uq2.passed IS TRUE
+        AND uq2.quiz_id <> 22
+        AND uq2.updated_at >= '2025-04-11 00:00:00 EST'
+        AND ABS(EXTRACT(EPOCH FROM (uq2.updated_at - onau.upchieve_101_passed_at))) <= 10
+);
     `)
     return result.rows.map((row) => camelCaseKeys(row))
   } catch (err) {
@@ -37,101 +58,16 @@ async function getUsersWhoAreApprovedButNotOnboarded(): Promise<User[]> {
   }
 }
 
-async function getUsersQuizzesForUsers(userIds: string[]): Promise<UserQuiz[]> {
-  try {
-    const result = await client.query(
-      `
-      SELECT uq.*
-      FROM users_quizzes uq
-      WHERE uq.user_id = ANY($1)
-      AND uq.passed IS TRUE
-      AND uq.updated_at >= '2025-04-11 00:00:00 EST'
-    `,
-      [userIds]
-    )
-    return result.rows.map((row) => camelCaseKeys(row))
-  } catch (err) {
-    logger.error(
-      err,
-      `${logPrefix}Failed to get users_quizzes for users: ${err}`
-    )
-    throw err
-  }
-}
-
 export default async function backfillCombinedQuizUsersOnboardedStatus() {
   try {
     // Get approved but not onboarded users
-    const users = await getUsersWhoAreApprovedButNotOnboarded()
-    const distinctUserIds = new Set<string>(users.map((u) => u.id))
+    const users = await getUsers()
+    const userIds = users.map((u) => u.id)
     logger.info(
-      `${logPrefix}Total number of users who are approved but not onboarded: ${distinctUserIds.size}`
+      { userIds },
+      `${logPrefix}Found ${userIds.length} users to update`
     )
-
-    // Pull users_quizzes for those users. Map the quiz results by user id.
-    const usersQuizzes = await getUsersQuizzesForUsers(
-      Array.from(distinctUserIds)
-    )
-    const userQuizMap: { [userId: string]: UserQuiz[] } = {}
-    usersQuizzes.forEach((quiz) => {
-      if (userQuizMap.hasOwnProperty(quiz.userId)) {
-        // Existing key
-        const existingValue = userQuizMap[quiz.userId] // { id to UserQuiz[] }
-        const updatedValue = [...existingValue, quiz]
-        userQuizMap[quiz.userId] = updatedValue
-      } else {
-        // New key
-        userQuizMap[quiz.userId] = [quiz]
-      }
-    })
-    logger.info(
-      `${logPrefix}Mapped user ID to their passed quizzes. Total users: ${Object.keys(userQuizMap).length}`
-    )
-
-    // Now pull out the users who have passed UPchieve101 AND some subject quiz
-    const UPCHIEVE_101_QUIZ_ID = 22
-    const userQuizMapForUsersWhoPassedAllRequiredQuizzes: {
-      [userId: string]: UserQuiz[]
-    } = {}
-    Object.entries(userQuizMap).forEach(([userId, quizList]) => {
-      const passedUpchieve101 = quizList.some(
-        (quiz) => quiz.quizId === UPCHIEVE_101_QUIZ_ID
-      )
-      const passedSubject = quizList.some(
-        (quiz) => quiz.quizId !== UPCHIEVE_101_QUIZ_ID
-      )
-      if (passedUpchieve101 && passedSubject)
-        userQuizMapForUsersWhoPassedAllRequiredQuizzes[userId] = quizList
-    })
-    logger.info(
-      `${logPrefix}Total users who passed both UPC101 and a subject quiz: ${Object.keys(userQuizMapForUsersWhoPassedAllRequiredQuizzes).length}`
-    )
-
-    // Now filter those down to people who went through the combined quiz flow (passed UPC101 and some other quiz within 10s of each other)
-    const combinedQuizUsers: string[] = []
-    Object.entries(userQuizMapForUsersWhoPassedAllRequiredQuizzes).forEach(
-      ([userId, quizList]) => {
-        const upchieve101QuizResult = quizList.find(
-          (quiz) => quiz.quizId === UPCHIEVE_101_QUIZ_ID
-        )
-        if (!upchieve101QuizResult)
-          throw new Error('User has not passed UPchieve 101!')
-        const passedUpchieve101At = upchieve101QuizResult.updatedAt
-        const combinedSubjectQuiz = quizList.find(
-          (quiz) =>
-            quiz.quizId !== UPCHIEVE_101_QUIZ_ID &&
-            isWithin10Seconds(quiz.updatedAt, passedUpchieve101At)
-        )
-        if (combinedSubjectQuiz) {
-          combinedQuizUsers.push(userId)
-        }
-      }
-    )
-    logger.info(
-      `${logPrefix}Found ${combinedQuizUsers.length} combined quiz users`
-    )
-
-    await updateOnboardedAndSendEmails(combinedQuizUsers, client)
+    await updateOnboardedAndSendEmails(userIds, client)
     logger.info(`${logPrefix}Finished updating.`)
   } catch (err) {
     logger.error(
@@ -139,10 +75,6 @@ export default async function backfillCombinedQuizUsersOnboardedStatus() {
       `${logPrefix}Error backfilling onboarded status for combined quiz users: ${err}`
     )
   }
-}
-
-function isWithin10Seconds(date1: Date, date2: Date): boolean {
-  return Math.abs(date1.getTime() - date2.getTime()) <= 10 * 1000
 }
 
 async function updateOnboardedAndSendEmails(
