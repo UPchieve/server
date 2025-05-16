79b730ca22161069b00f40eeac15dfd07eb36d92
Trey Stevens <treykstevens.tks@gmail.com>
2025-05-16 14:02:36 -0600

    remove session ended related listeners

--- server/services/listeners/index.ts
+++ server/services/listeners/index.ts
@@ -1,13 +1,7 @@
-import { listeners as SessionServiceListeners } from './SessionServiceListeners'
 import { listeners as StudentServiceListeners } from './StudentServiceListeners'
 import { listeners as UserCreationServiceListeners } from './UserCreationServiceListeners'
-import { listeners as ProgressReportsServiceListeners } from './ProgressReportsServiceListeners'
-import { listeners as IncentiveProgramServiceListeners } from './IncentiveProgramServiceListeners'
 
 export function registerListeners() {
-  SessionServiceListeners()
   StudentServiceListeners()
   UserCreationServiceListeners()
-  ProgressReportsServiceListeners()
-  IncentiveProgramServiceListeners()
 }
