65973b609ef43a23206117d37a11cdcbb09273f6
Trey Stevens <treykstevens.tks@gmail.com>
2025-05-16 13:56:48 -0600

    remove updateUserSessionMetricsByUserId references

--- server/models/UserSessionMetrics/queries.ts
+++ server/models/UserSessionMetrics/queries.ts
@@ -1,53 +1,9 @@
-import { merge } from 'lodash'
 import { getClient, TransactionClient } from '../../db'
-import { RepoReadError, RepoUpdateError } from '../Errors'
+import { RepoReadError } from '../Errors'
 import { makeRequired, Ulid } from '../pgUtils'
 import * as pgQueries from './pg.queries'
 import { UserSessionMetrics } from './types'
 import { UserRole } from '../User'
-
-export type UserSessionMetricsUpdateQuery = { [key: string]: number }
-
-// TODO: To remove. This will be computed via a view
-export async function updateUserSessionMetricsByUserId(
-  userId: Ulid,
-  metrics: UserSessionMetrics
-): Promise<UserSessionMetrics> {
-  try {
-    const result = await pgQueries.updateUserSessionMetricsByUserId.run(
-      {
-        userId,
-        absentStudent: metrics.absentStudent,
-        absentVolunteer: metrics.absentVolunteer,
-        lowSessionRatingFromCoach: metrics.lowSessionRatingFromCoach,
-        lowSessionRatingFromStudent: metrics.lowSessionRatingFromStudent,
-        lowCoachRatingFromStudent: metrics.lowCoachRatingFromStudent,
-        reported: metrics.reported,
-        onlyLookingForAnswers: metrics.onlyLookingForAnswers,
-        rudeOrInappropriate: metrics.rudeOrInappropriate,
-        commentFromStudent: metrics.commentFromStudent,
-        commentFromVolunteer: metrics.commentFromVolunteer,
-        hasBeenUnmatched: metrics.hasBeenUnmatched,
-        hasHadTechnicalIssues: metrics.hasHadTechnicalIssues,
-        personalIdentifyingInfo: metrics.personalIdentifyingInfo,
-        gradedAssignment: metrics.gradedAssignment,
-        coachUncomfortable: metrics.coachUncomfortable,
-        studentCrisis: metrics.studentCrisis,
-      },
-      getClient()
-    )
-    if (result.length) return makeRequired(result[0])
-    throw new RepoUpdateError(
-      'updateUserSessionMetricsByUserId query did not update'
-    )
-  } catch (err) {
-    throw new RepoUpdateError(
-      `Failed to update metrics ${metrics} for user ${userId}: ${
-        (err as Error).message
-      }`
-    )
-  }
-}
 
 export async function getUserSessionMetricsByUserId(
   userId: Ulid,
