34371cc9831be7054d56e0748a5c3711324720c9
Trey Stevens <treykstevens.tks@gmail.com>
2025-05-16 13:47:10 -0600

    remove UserSessionMetricsService and related files

--- server/services/listeners/index.ts
+++ server/services/listeners/index.ts
@@ -1,5 +1,4 @@
 import { listeners as SessionServiceListeners } from './SessionServiceListeners'
-import { listeners as USMServiceListeners } from './USMServiceListeners'
 import { listeners as StudentServiceListeners } from './StudentServiceListeners'
 import { listeners as UserCreationServiceListeners } from './UserCreationServiceListeners'
 import { listeners as ProgressReportsServiceListeners } from './ProgressReportsServiceListeners'
@@ -7,7 +6,6 @@ import { listeners as IncentiveProgramServiceListeners } from './IncentiveProgra
 
 export function registerListeners() {
   SessionServiceListeners()
-  USMServiceListeners()
   StudentServiceListeners()
   UserCreationServiceListeners()
   ProgressReportsServiceListeners()
