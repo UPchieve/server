c881300f3266f46c674fa775f4e841fcccf77324
Trey Stevens <treykstevens.tks@gmail.com>
2025-05-19 10:16:37 -0600

    run lint

--- database/migrations/20250517000547_add_language_code_to_users.sql
+++ database/migrations/20250517000547_add_language_code_to_users.sql
@@ -1,8 +1,8 @@
 -- migrate:up
 ALTER TABLE upchieve.users
-ADD COLUMN preferred_language_code VARCHAR(2);
-
+    ADD COLUMN preferred_language_code VARCHAR(2);
 
 -- migrate:down
 ALTER TABLE upchieve.users
-DROP COLUMN preferred_language_code;
+    DROP COLUMN preferred_language_code;
+
