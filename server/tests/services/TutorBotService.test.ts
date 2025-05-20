b7df4369136e7d56ad908eafe1d63163dcca0d11
Kasozi Wilson <kwilson05@users.noreply.github.com>
2025-05-20 10:09:25 -0400

    todo

---
+++ server/tests/services/TutorBotService.test.ts
@@ -0,0 +1,6 @@
+import { mocked } from 'jest-mock'
+import * as LangfuseService from '../../services/LangfuseService'
+import * as TutorBotService from '../../services/TutorBotService'
+
+//TODO add some tests
+describe('TutorBotService', () => {})
