4eaccf8cb015660b66dcf460d24258a2cc21bdbf
Trey Stevens <treykstevens.tks@gmail.com>
2025-05-21 20:05:29 -0600

    remove migration

--- database/migrations/20250515005311_add_campaigns_to_user_product_flags.sql
+++
@@ -1,8 +0,0 @@
--- migrate:up
-ALTER TABLE upchieve.user_product_flags
-    ADD COLUMN campaigns JSONB DEFAULT '{}'::jsonb;
-
--- migrate:down
-ALTER TABLE upchieve.user_product_flags
-    DROP COLUMN campaigns;
-
