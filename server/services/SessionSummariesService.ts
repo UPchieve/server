a3e88b6f9df4a9ca6f48d06ec8cbc5aa45ec4dd6
Kasozi Wilson <kwilson05@users.noreply.github.com>
2025-06-03 10:13:45 -0400

    use new signature

--- server/services/SessionSummariesService.ts
+++ server/services/SessionSummariesService.ts
@@ -4,7 +4,7 @@ import * as ProgressReportsService from './ProgressReportsService'
 import { getSubjectAndTopic } from '../models/Subjects'
 import { USER_ROLES, USER_ROLES_TYPE } from '../constants'
 import * as LangfuseService from './LangfuseService'
-import { invokeChatApi, MODEL_ID as OPENAI_MODELID } from './OpenAIService'
+import { invokeModel, MODEL_ID as OPENAI_MODELID } from './OpenAIService'
 import logger from '../logger'
 import * as SessionSummariesRepo from '../models/SessionSummaries/queries'
 import QueueService from './QueueService'
@@ -168,7 +168,7 @@ export async function generateSessionSummary(
     model: OPENAI_MODELID,
     input: botPrompt,
   })
-  const result = await invokeChatApi({
+  const result = await invokeModel({
     prompt: systemPrompt,
     userMessage: botPrompt,
   })
