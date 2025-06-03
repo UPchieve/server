d4d53834de5b33131ff0f06a539c6dc020573665
Kasozi Wilson <kwilson05@users.noreply.github.com>
2025-06-03 10:24:13 -0400

    json default response type

--- server/services/SessionSummariesService.ts
+++ server/services/SessionSummariesService.ts
@@ -4,7 +4,11 @@ import * as ProgressReportsService from './ProgressReportsService'
 import { getSubjectAndTopic } from '../models/Subjects'
 import { USER_ROLES, USER_ROLES_TYPE } from '../constants'
 import * as LangfuseService from './LangfuseService'
-import { invokeModel, MODEL_ID as OPENAI_MODELID } from './OpenAIService'
+import {
+  ChatApiResponseType,
+  invokeModel,
+  MODEL_ID as OPENAI_MODELID,
+} from './OpenAIService'
 import logger from '../logger'
 import * as SessionSummariesRepo from '../models/SessionSummaries/queries'
 import QueueService from './QueueService'
@@ -171,6 +175,7 @@ export async function generateSessionSummary(
   const result = await invokeModel({
     prompt: systemPrompt,
     userMessage: botPrompt,
+    responseType: ChatApiResponseType.TEXT,
   })
   gen.end({ output: result })
 
