fbd556cfd82a7b350bc138c00aca996b729eba26
Trey Stevens <treykstevens.tks@gmail.com>
2025-07-31 11:39:26 -0600

    mr comments and remove unintentional merge of extra classlink strategy

--- server/router/auth/passport-auth-middleware.ts
+++ server/router/auth/passport-auth-middleware.ts
@@ -14,6 +14,7 @@ import * as UserCreationService from '../../services/UserCreationService'
 import {
   RegisterStudentPayload,
   SessionWithSsoData,
+  SsoProviderNames,
   verifyPassword,
 } from '../../utils/auth-utils'
 import { isDevEnvironment } from '../../utils/environments'
@@ -105,7 +106,7 @@ type SSOProfile = passport.Profile & {
 }
 
 type SSOHandlerOptions = {
-  providerName: 'Clever' | 'ClassLink'
+  providerName: SsoProviderNames.CLEVER | SsoProviderNames.CLASSLINK
   isStudent: (userType: USER_ROLES_TYPE) => boolean
   isTeacher: (userType: USER_ROLES_TYPE) => boolean
   // TODO: When including ClassLink rostering, normalize to a similar type
@@ -309,7 +310,13 @@ export function addPassportAuthMiddleware() {
           return passportLoginUser(profile.id, issuer, done)
         } else {
           const { userData } = (req.session as SessionWithSsoData).sso ?? {}
-          return passportRegisterUser(profile, issuer, 'Google', userData, done)
+          return passportRegisterUser(
+            profile,
+            issuer,
+            SsoProviderNames.GOOGLE,
+            userData,
+            done
+          )
         }
       }
     )
@@ -325,9 +332,9 @@ export function addPassportAuthMiddleware() {
       done: Function
     ) {
       return handleSSOStrategy(req, profile, done, {
-        providerName: 'Clever',
-        isStudent: CleverAPIService.isStudent,
-        isTeacher: CleverAPIService.isTeacher,
+        providerName: SsoProviderNames.CLEVER,
+        isStudent,
+        isTeacher,
         rosterTeacher: CleverRosterService.rosterTeacherClasses,
       })
     })
@@ -343,112 +350,10 @@ export function addPassportAuthMiddleware() {
       done: Function
     ) {
       return handleSSOStrategy(req, profile, done, {
-        providerName: 'ClassLink',
-        isStudent: (userType) => userType === 'student',
-        isTeacher: (userType) => userType === 'teacher',
+        providerName: SsoProviderNames.CLASSLINK,
+        isStudent,
+        isTeacher,
       })
-    })
-  )
-
-  // TODO: ClassLink and Clever passport strategies have mostly duplicated logic (SSO handling, account linking, account creation),
-  // refactor to handle shared logic
-  passport.use(
-    'classlink',
-    new ClassLinkStrategy({ callbackURL: getRedirectURI() }, async function (
-      req: Request,
-      _accessToken: string,
-      _refreshToken: string,
-      profile: ClassLinkPassportProfile,
-      done: Function
-    ) {
-      try {
-        const { userData } = (req.session as SessionWithSsoData).sso ?? {}
-        // Check if the user has already used ClassLink SSO.
-        const existingFedCred = await FedCredService.getFedCredForUser(
-          profile.id,
-          profile.issuer
-        )
-        if (existingFedCred) {
-          if (userData && profile.userType === 'student') {
-            const data = {
-              schoolId: userData.schoolId,
-              studentPartnerOrgKey: (userData as RegisterStudentPayload)
-                .studentPartnerOrgKey,
-              studentPartnerOrgSiteName: (userData as RegisterStudentPayload)
-                .studentPartnerOrgSiteName,
-              userId: existingFedCred.userId,
-            }
-            await UserCreationService.upsertStudent(data)
-          }
-          return done(null, { id: existingFedCred.userId })
-        }
-
-        const firstName = profile.name?.givenName
-        const lastName = profile.name?.familyName
-        if (!firstName || !lastName) {
-          return done(null, false, {
-            errorMessage: 'Missing required field in passport.Profile',
-          })
-        }
-
-        const email = profile.emails?.[0]?.value ?? userData?.email
-        if (!email)
-          return done(null, false, {
-            profileId: profile.id,
-            issuer: profile.issuer,
-            firstName,
-            lastName,
-          })
-
-        const existingUser = await getUserVerificationByEmails(
-          email,
-          userData?.email
-        )
-        if (existingUser) {
-          if (userData && profile.userType === 'student') {
-            const data = {
-              schoolId: userData.schoolId,
-              studentPartnerOrgKey: (userData as RegisterStudentPayload)
-                .studentPartnerOrgKey,
-              studentPartnerOrgSiteName: (userData as RegisterStudentPayload)
-                .studentPartnerOrgSiteName,
-              userId: existingUser.id,
-            }
-            await UserCreationService.upsertStudent(data)
-          }
-          await FedCredService.linkAccount(
-            profile.id,
-            profile.issuer,
-            existingUser.id
-          )
-          return done(null, { id: existingUser.id })
-        }
-
-        // If the user doesn't exist, register them.
-        const data = {
-          ...userData,
-          email,
-          firstName,
-          issuer: profile.issuer,
-          lastName,
-          profileId: profile.id,
-          schoolId: profile.schoolId,
-        }
-        if (profile.userType === 'student') {
-          const student = await UserCreationService.registerStudent(data)
-          return done(null, student)
-        } else if (profile.userType === 'teacher') {
-          const teacher = await UserCreationService.registerTeacher(data)
-          return done(null, teacher)
-        }
-      } catch (err) {
-        logger.error(err, 'Failed ClassLink SSO.')
-        return done(null, false, {
-          userType: profile.userType,
-          errorMessage:
-            'Failed ClassLink SSO. Please try again or contact support.',
-        })
-      }
     })
   )
 }
@@ -469,4 +374,12 @@ function getRedirectURI() {
     ? `http://localhost:3000`
     : `${config.protocol}://${config.host}`
   return `${host}/auth/oauth2/redirect`
+}
+
+function isStudent(userType: USER_ROLES_TYPE): boolean {
+  return userType === 'student'
+}
+
+function isTeacher(userType: USER_ROLES_TYPE): boolean {
+  return userType === 'teacher'
 }
