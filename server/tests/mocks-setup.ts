8b50a0174675f8e638e06b8f56fd9a0de78bcd97
Sammy Nave <sammy.nave@gmail.com>
2025-08-07 15:57:54 -0400

    finall///

--- server/tests/mocks-setup.ts
+++ server/tests/mocks-setup.ts
@@ -1,4 +1,4 @@
-const redisMockImpl = require('ioredis-mock')
+const redisMockImpl = require('ioredis-mock/jest')
 redisMockImpl.prototype.config = jest.fn()
 jest.mock('ioredis', () => redisMockImpl)
 jest.mock('yjs')
