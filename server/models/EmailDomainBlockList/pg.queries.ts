100a3caa4d2b20819cbebc9915b5f2f8113d74a1
Kasozi Wilson <kwilson05@users.noreply.github.com>
2025-08-18 18:18:52 -0400

    reapply schema updates

--- server/models/EmailDomainBlockList/pg.queries.ts
+++ server/models/EmailDomainBlockList/pg.queries.ts
@@ -1,13 +1,21 @@
 /** Types generated for queries found in "server/models/EmailDomainBlockList/email_domain_blocklist.sql" */
 import { PreparedQuery } from '@pgtyped/runtime';
 
-/** Query 'GetBlockedEmailDomainByDomain' is invalid, so its result is assigned type 'never'.
- *  */
-export type IGetBlockedEmailDomainByDomainResult = never;
+/** 'GetBlockedEmailDomainByDomain' parameters type */
+export interface IGetBlockedEmailDomainByDomainParams {
+  email_domain: string;
+}
 
-/** Query 'GetBlockedEmailDomainByDomain' is invalid, so its parameters are assigned type 'never'.
- *  */
-export type IGetBlockedEmailDomainByDomainParams = never;
+/** 'GetBlockedEmailDomainByDomain' return type */
+export interface IGetBlockedEmailDomainByDomainResult {
+  domain: string;
+}
+
+/** 'GetBlockedEmailDomainByDomain' query type */
+export interface IGetBlockedEmailDomainByDomainQuery {
+  params: IGetBlockedEmailDomainByDomainParams;
+  result: IGetBlockedEmailDomainByDomainResult;
+}
 
 const getBlockedEmailDomainByDomainIR: any = {"usedParamSet":{"email_domain":true},"params":[{"name":"email_domain","required":true,"transform":{"type":"scalar"},"locs":[{"a":69,"b":82}]}],"statement":"SELECT\n    DOMAIN\nFROM\n    email_domain_blocklist\nWHERE\n    DOMAIN = :email_domain!"};
 
