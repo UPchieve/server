a3fbb395bbde307134f355c1ce6f24703d9a4267
Jamie Kasulis <jamie.kasulis@upchieve.org>
2025-08-21 10:29:09 -0400

    Fix rebase

--- database/seed-updates/20250813150449_new-upchieve-training-course.sql
+++
@@ -1,8 +0,0 @@
--- migrate:up
-INSERT INTO upchieve.training_courses (name, display_name)
-    VALUES ('upchieveTraining', 'UPchieve Training');
-
--- migrate:down
-DELETE FROM upchieve.training_courses
-WHERE name = 'upchieveTraining';
-
