955fda8f242ed35c37ed33d4f55cf5b52ed28f4e
Trey Stevens <treykstevens.tks@gmail.com>
2025-09-05 16:23:34 -0600

    update env name for app storage container

--- server/config.ts
+++ server/config.ts
@@ -361,10 +361,9 @@ const config: Static<typeof Config> = {
     process.env.SUBWAY_ASSIGNMENTS_STORAGE_CONTAINER || 'bogus',
   assignmentsFrontdoorHostName:
     process.env.SUBWAY_ASSIGNMENTS_FRONTDOOR_HOSTNAME || 'bogus',
-  appStorageAccountName:
-    process.env.SUBWAY_SESSIONS_STORAGE_ACCOUNT_NAME || 'bogus',
+  appStorageAccountName: process.env.SUBWAY_APP_STORAGE_ACCOUNT_NAME || 'bogus',
   appStorageAccountAccessKey:
-    process.env.SUBWAY_SESSIONS_STORAGE_ACCOUNT_ACCESS_KEY || 'bogus',
+    process.env.SUBWAY_APP_STORAGE_ACCOUNT_ACCESS_KEY || 'bogus',
   sessionsStorageContainer:
     process.env.SUBWAY_SESSIONS_STORAGE_CONTAINER || 'bogus',
   version: process.env.SUBWAY_VERSION || 'development',
