93f2cb1e0b426e0ff4b1904cc48ca7e7cc8b888e
Jamie Kasulis <jamie.kasulis@upchieve.org>
2025-09-22 17:31:33 -0400

    Stub out sending a special email to approved (and now therefore ready-to-coach) coaches

--- server/worker/jobs/backfillOnboardedStatus.ts
+++ server/worker/jobs/backfillOnboardedStatus.ts
@@ -1,6 +1,7 @@
 import {
   getVolunteersForOnboardingBackfill,
   updateVolunteerOnboarded,
+  VolunteerToOnboard,
 } from '../../models/Volunteer'
 import { runInTransaction, TransactionClient } from '../../db'
 import logger from '../../logger'
@@ -15,71 +16,101 @@ import { Jobs } from './index'
  * step is no longer required.
  */
 export default async function backfillOnboardedStatus() {
-  await runInTransaction(async (tc: TransactionClient) => {
-    const volunteersToUpdate = await getVolunteersForOnboardingBackfill(tc)
-    if (!volunteersToUpdate.length) {
+  const onboarded: VolunteerToOnboard[] | undefined = await runInTransaction(
+    async (tc: TransactionClient) => {
+      const targetVolunteers = await getVolunteersForOnboardingBackfill(tc)
+      if (!targetVolunteers.length) {
+        logger.info(
+          'Onboarding backfill: Found no volunteers to update, so returning early.'
+        )
+        return
+      }
       logger.info(
-        'Onboarding backfill: Found no volunteers to update, so returning early.'
-      )
-      return
-    }
-    logger.info(
-      {
-        volunteerIdsToOnboard: volunteersToUpdate.map((vol) => vol.id),
-      },
-      `Onboarding backfill: Found ${volunteersToUpdate.length} volunteers to backfill onboarded status for.`
-    )
-
-    const onboardedVolunteers = []
-    for (const volunteer of volunteersToUpdate) {
-      await updateVolunteerOnboarded(volunteer.id, tc)
-      await createAccountAction(
         {
-          action: ACCOUNT_USER_ACTIONS.ONBOARDED,
-          userId: volunteer.id,
-          ipAddress: undefined,
+          volunteerIdsToOnboard: targetVolunteers.map((vol) => vol.id),
         },
-        tc
+        `Onboarding backfill: Found ${targetVolunteers.length} volunteers to backfill onboarded status for.`
       )
-      AnalyticsService.captureEvent(volunteer.id, EVENTS.ACCOUNT_ONBOARDED, {
-        event: EVENTS.ACCOUNT_ONBOARDED,
-        source: 'remove-availability-reqt-backfill-script',
-      })
-      if (volunteer.approved) {
-        AnalyticsService.captureEvent(
-          volunteer.id,
-          EVENTS.ACCOUNT_VOLUNTEER_READY,
+
+      const onboardedVolunteers: VolunteerToOnboard[] = []
+      for (const volunteer of targetVolunteers) {
+        await updateVolunteerOnboarded(volunteer.id, tc)
+        await createAccountAction(
           {
-            source: 'remove-availability-reqt-backfill-script',
-          }
+            action: ACCOUNT_USER_ACTIONS.ONBOARDED,
+            userId: volunteer.id,
+            ipAddress: undefined,
+          },
+          tc
         )
+        AnalyticsService.captureEvent(volunteer.id, EVENTS.ACCOUNT_ONBOARDED, {
+          event: EVENTS.ACCOUNT_ONBOARDED,
+          source: 'remove-availability-reqt-backfill-script',
+        })
+        if (volunteer.approved) {
+          AnalyticsService.captureEvent(
+            volunteer.id,
+            EVENTS.ACCOUNT_VOLUNTEER_READY,
+            {
+              source: 'remove-availability-reqt-backfill-script',
+            }
+          )
+        }
+        onboardedVolunteers.push(volunteer)
       }
-      onboardedVolunteers.push(volunteer.id)
+      return onboardedVolunteers
     }
+  )
+  if (!onboarded) {
+    return
+  }
+
+  logger.info(
+    {
+      onboardedVolunteerIds: onboarded.map((volunteer) => volunteer.id),
+    },
+    `Onboarding backfill: Onboarded ${onboarded.length ?? 0} volunteers`
+  )
+
+  try {
+    logger.info(
+      `Onboarding backfill: Queueing up volunteer quick tips email for ${onboarded.length} volunteers`
+    )
+    for (const volunteer of onboarded) {
+      await QueueService.add(
+        Jobs.EmailVolunteerQuickTips,
+        { volunteerId: volunteer.id },
+        // Process job 5 days after the volunteer is onboarded.
+        {
+          delay: 1000 * 60 * 60 * 24 * 5,
+          removeOnComplete: true,
+          removeOnFail: true,
+        }
+      )
+    }
+  } catch (err) {
+    logger.error(
+      { err },
+      'Onboarding backfill: Failed to enqueue volunteer quick tips email job'
+    )
+  }
+
+  const readyToCoachVolunteers = onboarded.filter(
+    (volunteer) => volunteer.approved
+  )
+
+  try {
     logger.info(
       {
-        onboardedVolunteerIds: onboardedVolunteers,
+        readyToCoachVolunteerIds: readyToCoachVolunteers.map((vol) => vol.id),
       },
-      `Onboarding backfill: Onboarded ${onboardedVolunteers.length} volunteers`
+      `Onboarding backfill: Sending now-ready-to-coach email to ${readyToCoachVolunteers.length} volunteers`
     )
-
-    try {
-      for (const volunteer of volunteersToUpdate) {
-        await QueueService.add(
-          Jobs.EmailVolunteerQuickTips,
-          { volunteerId: volunteer.id },
-          // Process job 5 days after the volunteer is onboarded.
-          {
-            delay: 1000 * 60 * 60 * 24 * 5,
-            removeOnComplete: true,
-            removeOnFail: true,
-          }
-        )
-
-        // @TODO Special email for these volunteers, especially ones whose lastActivityAt was a while ago?
-      }
-    } catch (err) {
-      logger.error('Onboarding backfill: Failed to send onboarding email')
-    }
-  })
+    // @TODO mail service call
+  } catch (err) {
+    logger.error(
+      { err },
+      'Onboarding backfill: Failed to send now-ready-to-coach email to some volunteers'
+    )
+  }
 }
