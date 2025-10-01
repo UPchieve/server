ffa2dfb8a152c583b6e6443b87b19371a99fe815
Jamie Kasulis <jamie.kasulis@upchieve.org>
2025-10-01 14:07:58 -0400

    Expose queue, add() function from QueueService. Add() uses default job options

--- server/worker/jobs/notifyTutors.ts
+++ server/worker/jobs/notifyTutors.ts
@@ -33,7 +33,7 @@ export default async (job: Job<NotifyTutorsJobData>): Promise<void> => {
         notificationSchedule,
         currentNotificationRound: currentNotificationRound + 1,
       },
-      { delay, removeOnComplete: true, removeOnFail: true }
+      { delay, removeOnFail: true }
     )
 
   try {
@@ -50,7 +50,7 @@ export default async (job: Job<NotifyTutorsJobData>): Promise<void> => {
           sessionId: sessionId.toString(),
           volunteerId: volunteerNotified.toString(),
         },
-        { delay: 1000 * 60 * 5, removeOnComplete: true, removeOnFail: true }
+        { delay: 1000 * 60 * 5, removeOnFail: true }
       )
     } else {
       log(
