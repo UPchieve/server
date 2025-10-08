a0a440406863bdafee923f381ac041660c823683
Jamie Kasulis <jamie.kasulis@upchieve.org>
2025-10-08 11:21:51 -0400

    Fix ban filter

--- server/models/Volunteer/volunteer.sql
+++ server/models/Volunteer/volunteer.sql
@@ -1692,8 +1692,8 @@ FROM
             ap.volunteer_partner_org_id = vp.volunteer_partner_org_id) AS associated_partners ON TRUE
     JOIN users_unlocked_subjects_mview unlocked_subjects ON unlocked_subjects.user_id = u.id
 WHERE (u.ban_type IS NULL
-    OR u.ban_type <> 'complete'::ban_types
-    OR u.ban_type <> 'shadow'::ban_types)
+    OR (u.ban_type <> 'complete'::ban_types
+        AND u.ban_type <> 'shadow'::ban_types))
 AND u.deactivated IS FALSE
 AND u.test_user IS FALSE
 AND vp.onboarded IS TRUE
