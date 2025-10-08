4ed9fb90f840b950063295a9f8e44dc6f76f5c08
Jamie Kasulis <jamie.kasulis@upchieve.org>
2025-10-08 12:45:28 -0400

    Filter out muted subjects

--- server/worker/jobs/updateCachedVolunteersForTextNotifications.ts
+++ server/worker/jobs/updateCachedVolunteersForTextNotifications.ts
@@ -2,6 +2,7 @@ import * as CacheService from '../../cache'
 import * as VolunteerService from '../../services/VolunteerService'
 import { TextableVolunteer } from '../../models/Volunteer'
 import logger from '../../logger'
+import { omit } from 'lodash'
 
 const CACHE_KEY = 'VOLUNTEERS-FOR-TEXT-NOTIFICATIONS'
 const LOG_PREFIX = 'Caching textable volunteers: '
@@ -12,10 +13,34 @@ export default async (): Promise<void> => {
     `${LOG_PREFIX}Found ${textableVolunteers.length} candidate volunteers.`
   )
 
-  await saveToCache(textableVolunteers)
+  const volunteersWithoutMutedSubjects: Omit<
+    TextableVolunteer,
+    'mutedSubjects'
+  >[] = removeMutedSubjects(textableVolunteers)
+
+  await saveToCache(volunteersWithoutMutedSubjects)
   logger.info(`${LOG_PREFIX}Saved volunteers to cache.`)
 }
 
-async function saveToCache(volunteers: TextableVolunteer[]) {
+async function saveToCache(
+  volunteers: Omit<TextableVolunteer, 'mutedSubjects'>[]
+) {
   await CacheService.save(CACHE_KEY, JSON.stringify(volunteers))
+}
+
+function removeMutedSubjects(
+  volunteers: TextableVolunteer[]
+): Omit<TextableVolunteer, 'mutedSubjects'>[] {
+  return volunteers.map((volunteer) => {
+    const mutedSubjects = volunteer.mutedSubjects
+    const updatedVolunteer = omit(volunteer, ['mutedSubjects']) as Omit<
+      TextableVolunteer,
+      'mutedSubjects'
+    >
+    updatedVolunteer.unlockedSubjects =
+      updatedVolunteer.unlockedSubjects.filter(
+        (subject) => !mutedSubjects.includes(subject)
+      )
+    return updatedVolunteer
+  })
 }
