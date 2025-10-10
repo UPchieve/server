31073fb510284ce479ee324a88d425a2ac57b58d
Jamie Kasulis <jamie.kasulis@upchieve.org>
2025-10-10 09:11:30 -0400

    Undo change to fitler out muted subjects

--- server/tests/jobs/updateCachedVolunteersForTextNotifications.test.ts
+++ server/tests/jobs/updateCachedVolunteersForTextNotifications.test.ts
@@ -1,61 +1,63 @@
-import * as CacheService from '../../cache'
-import * as VolunteerService from '../../services/VolunteerService'
-import updateCachedVolunteersForTextNotifications from '../../worker/jobs/updateCachedVolunteersForTextNotifications'
-import { TextableVolunteer } from '../../models/Volunteer'
-import { getDbUlid } from '../../../database/seeds/utils'
-import { faker } from '@faker-js/faker'
-import { omit } from 'lodash'
+// import * as CacheService from '../../cache'
+// import * as VolunteerService from '../../services/VolunteerService'
+// import updateCachedVolunteersForTextNotifications from '../../worker/jobs/updateCachedVolunteersForTextNotifications'
+// import { TextableVolunteer } from '../../models/Volunteer'
+// import { getDbUlid } from '../../../database/seeds/utils'
+// import { faker } from '@faker-js/faker'
+// import { omit } from 'lodash'
+//
+// jest.mock('../../cache')
+// jest.mock('../../services/VolunteerService')
+// jest.mock('../../logger')
+//
+// const mockCacheService = jest.mocked(CacheService)
+// const mockVolunteerService = jest.mocked(VolunteerService)
+// describe('Filtering out muted subjects', () => {
+//   it('Saves to cache the candidate volunteers with muted subjects filtered out', async () => {
+//     const mutedPrealgebra = buildTextableVolunteer({
+//       id: '1',
+//       mutedSubjects: ['prealgebra'],
+//       unlockedSubjects: ['prealgebra', 'algebraOne'],
+//     })
+//     const noneMuted = buildTextableVolunteer({
+//       id: '3',
+//       unlockedSubjects: ['prealgebra', 'algebraOne'],
+//     })
+//     const volunteers: TextableVolunteer[] = [mutedPrealgebra, noneMuted]
+//     const expectedVolunteers = [
+//       {
+//         ...omit(mutedPrealgebra, 'mutedSubjects'),
+//         unlockedSubjects: ['algebraOne'],
+//       },
+//       {
+//         ...omit(noneMuted, 'mutedSubjects'),
+//         unlockedSubjects: ['prealgebra', 'algebraOne'],
+//       },
+//     ]
+//     mockVolunteerService.getVolunteersForTextNotifications.mockResolvedValue(
+//       volunteers
+//     )
+//
+//     await updateCachedVolunteersForTextNotifications()
+//
+//     expect(mockCacheService.save).toHaveBeenCalledTimes(1)
+//     expect(mockCacheService.save).toHaveBeenCalledWith(
+//       'VOLUNTEERS-FOR-TEXT-NOTIFICATIONS',
+//       JSON.stringify(expectedVolunteers)
+//     )
+//   })
+// })
+//
+// function buildTextableVolunteer(
+//   overrides: Partial<TextableVolunteer> = {}
+// ): TextableVolunteer {
+//   return {
+//     id: getDbUlid(),
+//     firstName: faker.string.alpha(),
+//     mutedSubjects: [],
+//     unlockedSubjects: ['prealgebra', 'algebraOne'],
+//     ...overrides,
+//   }
+// }
 
-jest.mock('../../cache')
-jest.mock('../../services/VolunteerService')
-jest.mock('../../logger')
-
-const mockCacheService = jest.mocked(CacheService)
-const mockVolunteerService = jest.mocked(VolunteerService)
-describe('Filtering out muted subjects', () => {
-  it('Saves to cache the candidate volunteers with muted subjects filtered out', async () => {
-    const mutedPrealgebra = buildTextableVolunteer({
-      id: '1',
-      mutedSubjects: ['prealgebra'],
-      unlockedSubjects: ['prealgebra', 'algebraOne'],
-    })
-    const noneMuted = buildTextableVolunteer({
-      id: '3',
-      unlockedSubjects: ['prealgebra', 'algebraOne'],
-    })
-    const volunteers: TextableVolunteer[] = [mutedPrealgebra, noneMuted]
-    const expectedVolunteers = [
-      {
-        ...omit(mutedPrealgebra, 'mutedSubjects'),
-        unlockedSubjects: ['algebraOne'],
-      },
-      {
-        ...omit(noneMuted, 'mutedSubjects'),
-        unlockedSubjects: ['prealgebra', 'algebraOne'],
-      },
-    ]
-    mockVolunteerService.getVolunteersForTextNotifications.mockResolvedValue(
-      volunteers
-    )
-
-    await updateCachedVolunteersForTextNotifications()
-
-    expect(mockCacheService.save).toHaveBeenCalledTimes(1)
-    expect(mockCacheService.save).toHaveBeenCalledWith(
-      'VOLUNTEERS-FOR-TEXT-NOTIFICATIONS',
-      JSON.stringify(expectedVolunteers)
-    )
-  })
-})
-
-function buildTextableVolunteer(
-  overrides: Partial<TextableVolunteer> = {}
-): TextableVolunteer {
-  return {
-    id: getDbUlid(),
-    firstName: faker.string.alpha(),
-    mutedSubjects: [],
-    unlockedSubjects: ['prealgebra', 'algebraOne'],
-    ...overrides,
-  }
-}
+it.todo('todo.')
--- server/worker/jobs/updateCachedVolunteersForTextNotifications.ts
+++ server/worker/jobs/updateCachedVolunteersForTextNotifications.ts
@@ -2,7 +2,6 @@ import * as CacheService from '../../cache'
 import * as VolunteerService from '../../services/VolunteerService'
 import { TextableVolunteer } from '../../models/Volunteer'
 import logger from '../../logger'
-import { omit } from 'lodash'
 
 const CACHE_KEY = 'VOLUNTEERS-FOR-TEXT-NOTIFICATIONS'
 const LOG_PREFIX = 'Caching textable volunteers: '
@@ -13,34 +12,10 @@ export default async (): Promise<void> => {
     `${LOG_PREFIX}Found ${textableVolunteers.length} candidate volunteers.`
   )
 
-  const volunteersWithoutMutedSubjects: Omit<
-    TextableVolunteer,
-    'mutedSubjects'
-  >[] = removeMutedSubjects(textableVolunteers)
-
-  await saveToCache(volunteersWithoutMutedSubjects)
+  await saveToCache(textableVolunteers)
   logger.info(`${LOG_PREFIX}Saved volunteers to cache.`)
 }
 
-async function saveToCache(
-  volunteers: Omit<TextableVolunteer, 'mutedSubjects'>[]
-) {
+async function saveToCache(volunteers: TextableVolunteer[]) {
   await CacheService.save(CACHE_KEY, JSON.stringify(volunteers))
-}
-
-function removeMutedSubjects(
-  volunteers: TextableVolunteer[]
-): Omit<TextableVolunteer, 'mutedSubjects'>[] {
-  return volunteers.map((volunteer) => {
-    const mutedSubjects = volunteer.mutedSubjects
-    const updatedVolunteer = omit(volunteer, ['mutedSubjects']) as Omit<
-      TextableVolunteer,
-      'mutedSubjects'
-    >
-    updatedVolunteer.unlockedSubjects =
-      updatedVolunteer.unlockedSubjects.filter(
-        (subject) => !mutedSubjects.includes(subject)
-      )
-    return updatedVolunteer
-  })
 }
