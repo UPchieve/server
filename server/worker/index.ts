a469bbaecea5e6ae1a56cef9b0e3ea3963914980
Kasozi Wilson <kwilson05@users.noreply.github.com>
2025-10-14 10:04:50 -0400

    move handlers

--- server/worker/index.ts
+++ server/worker/index.ts
@@ -1,7 +1,4 @@
-import Queue from 'bull'
 import newrelic from 'newrelic'
-import Redis from 'ioredis'
-import config from '../config'
 import * as db from '../db'
 import logger from '../logger'
 import { addJobProcessors } from './jobs'
@@ -13,19 +10,6 @@ const main = async (): Promise<void> => {
     await db.connect()
     logger.info('Starting queue')
     const queue = QueueService.queue
-    queue.on('error', (error) => {
-      logger.error(`error in queue: ${error}`)
-      newrelic.noticeError(error)
-    })
-    queue.on('stalled', (job) => {
-      logger.info({ job: job.name }, 'Worker job stalled.')
-    })
-    queue.on('lock-extension-failed', (job, err) => {
-      logger.error({ err, job: job.name }, 'Worker job failed to extend lock.')
-    })
-    queue.on('cleaned', (jobs, type) => {
-      logger.info({ jobs, type }, 'Worker jobs cleaned from queue.')
-    })
 
     addJobProcessors(queue)
     registerListeners()
@@ -34,10 +18,11 @@ const main = async (): Promise<void> => {
     // handle redis connection errors; for whatever reason Redis.ReplyError type is not in the declarations file
     if ((error as any).code === 'ECONNREFUSED') {
       logger.error(
-        `could not connect to redis server: ${config.redisConnectionString}`
+        error,
+        `Could not connect to redis server; Check your redisConnectionString env var`
       )
     } else {
-      logger.error(`error from worker process: ${error}`)
+      logger.error(error, `Error from worker process`)
     }
   }
 }
