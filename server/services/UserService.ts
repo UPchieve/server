cb94f868bb4bd947c0092bf8d2c0c87827a8ad33
Chris <2966918+chrisshaw@users.noreply.github.com>
2025-10-14 11:49:57 -0400

    fix(admin): Validate email before updating
    This avoid getting an error updating user in admin, for example. The reason this occurs actually isn't due to the database but Sendgrid. Now we ensure an email it can understand.

--- server/services/UserService.ts
+++ server/services/UserService.ts
@@ -39,6 +39,7 @@ import {
   updateVolunteerReferenceStatus,
 } from '../models/Volunteer'
 import { asReferenceFormData } from '../utils/reference-utils'
+import { checkEmail } from '../utils/auth-utils'
 import {
   asBoolean,
   asEnum,
@@ -293,6 +294,9 @@ export async function adminUpdateUser(data: unknown) {
       partnerSchool,
       schoolId,
     } = asAdminUpdate(data)
+
+    // Validate email format
+    checkEmail(email)
 
     const userBeforeUpdate = await getUserContactInfo(userId, tc)
 
