b51fa9d0b358d6fa422dd1909ab93ba3d96fb2a0
Sammy Nave <sammy.nave@gmail.com>
2025-11-25 08:43:40 -0500

    Supports NTHS group invite flows

--- server/models/NTHSGroups/queries.ts
+++ server/models/NTHSGroups/queries.ts
@@ -1,9 +1,10 @@
-import { getRoClient } from '../../db'
+import { getClient, getRoClient, TransactionClient } from '../../db'
 import { RepoReadError } from '../Errors'
-import { makeRequired, Ulid } from '../pgUtils'
+import { makeRequired, makeSomeOptional, Ulid, Uuid } from '../pgUtils'
 import * as pgQueries from './pg.queries'
+import type { NTHSGroup, UserGroup } from './types'
 
-export async function getGroupsByUser(userId: Ulid) {
+export async function getGroupsByUser(userId: Ulid): Promise<UserGroup[]> {
   try {
     const results = await pgQueries.getGroupsByUser.run(
       {
@@ -13,6 +14,65 @@ export async function getGroupsByUser(userId: Ulid) {
     )
     return results.map(makeRequired)
   } catch (err) {
+    throw new RepoReadError(err)
+  }
+}
+
+export async function getInviteCodeForGroup(groupId: Ulid) {
+  try {
+    const results = await pgQueries.getInviteCodeForGroup.run(
+      { id: groupId },
+      getRoClient()
+    )
+    return results.map(makeRequired)[0]
+  } catch (err) {
+    throw new RepoReadError(err)
+  }
+}
+
+export async function getGroupByInviteCode(
+  inviteCode: Uuid,
+  tc: TransactionClient = getRoClient()
+): Promise<NTHSGroup> {
+  try {
+    const results = await pgQueries.getGroupByInviteCode.run(
+      {
+        inviteCode,
+      },
+      tc
+    )
+    return results.map(makeRequired)[0]
+  } catch (err) {
+    throw new RepoReadError(err)
+  }
+}
+
+export async function joinGroupById(
+  {
+    userId,
+    groupId,
+    title,
+  }: {
+    userId: Ulid
+    groupId: Ulid
+    title: string
+  },
+
+  tc: TransactionClient = getClient()
+) {
+  try {
+    const results = await pgQueries.joinGroupById.run(
+      {
+        userId,
+        groupId,
+        title,
+      },
+      tc
+    )
+
+    return results.map((row) => makeSomeOptional(row, ['deactivatedAt']))
+  } catch (err) {
+    console.error('WHAT!!!!!!!!!!!!!!!!!', err)
     throw new RepoReadError(err)
   }
 }
--- server/services/NTHSGroupsService.ts
+++ server/services/NTHSGroupsService.ts
@@ -1,6 +1,53 @@
-import { Ulid } from '../models/pgUtils'
+import { Ulid, Uuid } from '../models/pgUtils'
 import * as NTHSGroupsRepo from '../models/NTHSGroups'
+import config from '../config'
+import generateAlphanumericOfLength from '../utils/generate-alphanumeric'
+import { Transaction } from 'yjs'
+import { getClient, getRoClient, TransactionClient } from '../db'
 
 export async function getGroups(userId: Ulid) {
-  return NTHSGroupsRepo.getGroupsByUser(userId)
+  return await NTHSGroupsRepo.getGroupsByUser(userId)
+}
+
+export async function getInviteLinkForGroup(groupId: Ulid) {
+  const code = await NTHSGroupsRepo.getInviteCodeForGroup(groupId)
+  return `https://${config.client.host}/join-group/${code}`
+}
+
+export async function getNTHSGroupByInviteCode(
+  inviteCode: Uuid,
+  tc: TransactionClient = getRoClient()
+) {
+  return await NTHSGroupsRepo.getGroupByInviteCode(inviteCode, tc)
+}
+
+export async function joinGroupAsMemberByGroupId(
+  userId: Ulid,
+  groupId: Ulid,
+  tc: TransactionClient = getClient()
+) {
+  console.log('=============@@@@@@@@@@@@@@2', { userId, groupId })
+  return await NTHSGroupsRepo.joinGroupById(
+    {
+      userId,
+      groupId,
+      title: 'member',
+    },
+    tc
+  )
+}
+
+// Will use this when creating a group
+async function generateUniqueCode() {
+  let count = 5
+  while (count > 0) {
+    const code = generateAlphanumericOfLength(6)
+    const group = await NTHSGroupsRepo.getGroupByInviteCode(code)
+    if (!group) {
+      return code
+    }
+    count--
+  }
+
+  throw new Error('Could not generate unique class code.')
 }
