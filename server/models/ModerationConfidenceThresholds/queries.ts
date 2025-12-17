f12149d5f4283aec244c03a1b0cdaa5abb114f40
Liseidy Bueno <liseidybueno@gmail.com>
2025-12-17 11:54:42 -0500

    remove getConfidenceThreshold query

--- server/models/ModerationConfidenceThresholds/queries.ts
+++ server/models/ModerationConfidenceThresholds/queries.ts
@@ -6,32 +6,6 @@ import { makeRequired } from '../pgUtils'
 
 export type ModerationType = 'contextual' | 'realtime_image'
 
-export async function getConfidenceTreshold(
-  moderationCategory: string,
-  moderationType: ModerationType,
-  client: TransactionClient = getClient()
-): Promise<Number> {
-  try {
-    const result = await pgQueries.getConfidenceThreshold.run(
-      {
-        moderationCategory,
-        moderationType,
-      },
-      client
-    )
-
-    if (!result.length) {
-      if (moderationType === 'contextual')
-        return config.contextualModerationConfidenceThreshold
-      else return config.imageModerationMinConfidence
-    }
-
-    return Number(makeRequired(result[0]).threshold)
-  } catch (err) {
-    throw new RepoReadError(err)
-  }
-}
-
 export type ModerationTreshold = {
   name: string
   threshold: string
