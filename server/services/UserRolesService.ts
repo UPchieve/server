5c47f3b2124f4a2df1a0c19b2e0f21b7208ff172
Jamie Kasulis <jamie.kasulis@upchieve.org>
2025-12-18 11:39:11 -0500

    Add comment for PrimaryUserRole

--- server/services/UserRolesService.ts
+++ server/services/UserRolesService.ts
@@ -6,6 +6,13 @@ import * as CacheService from '../cache'
 import config from '../config'
 import { InputError } from '../models/Errors'
 
+/*
+ * - Right now, most of the app experience is driven by whether a user is a student, volunteer, or teacher, and
+ * these are what we serve to the client to use as the userType/activeRole.
+ * - But technically users can have other roles, like admin and ambassador, which don't dictate the overall in-app
+ * experience like the other 3 do. Furthermore, admins and ambassadors are also both volunteers.
+ * - So you can think of PrimaryUserRole as referring to the "main user types"
+ */
 export type PrimaryUserRole = Exclude<UserRole, 'admin' | 'ambassador'>
 export type SessionUserRole = 'student' | 'volunteer'
 
