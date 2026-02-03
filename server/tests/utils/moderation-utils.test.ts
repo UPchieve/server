22c9a7e424afb3314921d09ee602312884593964
Chris <chris.shaw@upchieve.org>
2026-02-03 10:30:23 -0500

    Simplify moderation-utils test coverage

--- server/tests/utils/moderation-utils.test.ts
+++ server/tests/utils/moderation-utils.test.ts
@@ -2,16 +2,8 @@ import { convertThresholdToPercentage } from '../../utils/moderation-utils'
 
 describe('moderation-utils', () => {
   describe('convertThresholdToPercentage', () => {
-    it('Converts decimal thresholds (0-1) to percentages (0-100)', () => {
+    it('Converts decimal thresholds to percentages', () => {
       expect(convertThresholdToPercentage(0.75)).toBe(75)
-      expect(convertThresholdToPercentage(0.5)).toBe(50)
-      expect(convertThresholdToPercentage(0.9)).toBe(90)
-      expect(convertThresholdToPercentage(1)).toBe(100)
-    })
-
-    it('Returns percentage values unchanged', () => {
-      expect(convertThresholdToPercentage(75)).toBe(75)
-      expect(convertThresholdToPercentage(50)).toBe(50)
       expect(convertThresholdToPercentage(100)).toBe(100)
     })
   })
