b0c0a5b2c70a45e60d1682c97b437fa1adba0fa4
Liseidy Bueno <liseidybueno@gmail.com>
2026-02-20 17:15:21 -0300

    reset db

--- database/db_init/schema.sql
+++ database/db_init/schema.sql
@@ -722,17 +722,6 @@ CREATE TABLE upchieve.legacy_availability_histories (
 
 
 --
--- Name: moderation_actions; Type: TABLE; Schema: upchieve; Owner: -
---
-
-CREATE TABLE upchieve.moderation_actions (
-    id integer NOT NULL,
-    action_name character varying(30),
-    description character varying
-);
-
-
---
 -- Name: moderation_categories; Type: TABLE; Schema: upchieve; Owner: -
 --
 
@@ -3739,14 +3728,6 @@ ALTER TABLE ONLY upchieve.ip_addresses
 
 ALTER TABLE ONLY upchieve.legacy_availability_histories
     ADD CONSTRAINT legacy_availability_histories_pkey PRIMARY KEY (id);
-
-
---
--- Name: moderation_actions moderation_actions_pkey; Type: CONSTRAINT; Schema: upchieve; Owner: -
---
-
-ALTER TABLE ONLY upchieve.moderation_actions
-    ADD CONSTRAINT moderation_actions_pkey PRIMARY KEY (id);
 
 
 --
@@ -7252,7 +7233,6 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20260114171204'),
     ('20260114193023'),
     ('20260122195918'),
-    ('20260219143230'),
     ('20260129185914'),
     ('20260129190242'),
     ('20260203194147'),
