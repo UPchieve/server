103d4fc00c4b834913c1c5c29fe7f20f735935e0
Liseidy Bueno <liseidybueno@gmail.com>
2026-02-24 09:16:40 +0200

    add shadowban to actions table

--- database/db_init/test_seeds.sql
+++ database/db_init/test_seeds.sql
@@ -8901,7 +8901,7 @@ SELECT pg_catalog.setval('upchieve.ip_addresses_id_seq', 1, true);
 -- Name: moderation_actions_id_seq; Type: SEQUENCE SET; Schema: upchieve; Owner: admin
 --
 
-SELECT pg_catalog.setval('upchieve.moderation_actions_id_seq', 1, false);
+SELECT pg_catalog.setval('upchieve.moderation_actions_id_seq', 1, true);
 
 
 --
