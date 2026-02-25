937e35e0e558345645f7d832c280833998ff25ef
Kasozi Wilson <kwilson05@users.noreply.github.com>
2026-02-25 16:31:24 -0500

    remove console.log

--- server/router/api/sockets.ts
+++ server/router/api/sockets.ts
@@ -775,7 +775,6 @@ export function routeSockets(io: Server): void {
     socket.on('partner_shared_screen', async ({ sessionId }) => {
       const user = await extractSocketUser(socket)
 
-      console.log('here server event')
       io.to(getSessionRoom(sessionId))
         .except(user.id)
         .emit('partner_shared_screen')
