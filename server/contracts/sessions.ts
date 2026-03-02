57cc3440bfc3d9c7c4320ca8094052a75f6542bb
Trey Stevens <treykstevens.tks@gmail.com>
2026-03-02 22:19:54 +0100

    add contracts type for public current session

---
+++ server/contracts/sessions.ts
@@ -0,0 +1,39 @@
+import { Uuid } from '../models/pgUtils'
+
+export type SessionUserInfoPublic = {
+  // TODO: migrate all uses of `_id` to `id`
+  _id: Uuid
+  id: Uuid
+  // TODO: remove `firstname` in favor of `firstName`. The frontend must be refactored first
+  firstname: string
+  firstName: string
+  gradeLevel?: number
+}
+
+export type SessionMessagePublic = {
+  user: Uuid
+  contents: string
+  createdAt: Date
+}
+
+export type CurrentSessionPublic = {
+  _id: Uuid
+  id: Uuid
+  studentId: Uuid
+  volunteerId?: Uuid
+  student: SessionUserInfoPublic
+  volunteer?: SessionUserInfoPublic
+  volunteerJoinedAt?: Date
+  messages: SessionMessagePublic[]
+  toolType: string
+  docEditorVersion?: number
+  studentBannedFromLiveMedia?: boolean
+  volunteerBannedFromLiveMedia?: boolean
+  volunteerLanguages?: string[]
+  // TODO: Rename this property, this refers to a topic's name
+  type: string
+  subTopic: string
+  createdAt: Date
+  endedAt?: Date
+  endedBy?: Uuid
+}
