63e7ab072a0249424f55ebd2124fbc88df294166
Trey Stevens <treykstevens.tks@gmail.com>
2026-03-05 12:29:44 +0100

    add domain and contracts

---
+++ server/types/tutor-bot.ts
@@ -0,0 +1,68 @@
+import { Uuid } from '../models/pgUtils'
+
+export type TutorBotSenderType = 'student' | 'volunteer' | 'bot'
+export type TutorBotHumanSenderType = Exclude<TutorBotSenderType, 'bot'>
+
+export type TutorBotMessage = {
+  tutorBotConversationId: Uuid
+  userId: Uuid
+  senderUserType: TutorBotSenderType
+  message: string
+  createdAt: Date
+}
+
+export type TutorBotTranscript = {
+  conversationId: Uuid
+  subjectId: number
+  messages: TutorBotMessage[]
+  sessionId?: Uuid
+}
+
+export type TutorBotConversation = {
+  id: Uuid
+  userId: Uuid
+  sessionId?: Uuid
+  subjectId: number
+  createdAt: Date
+}
+
+export type TutorBotModelResponse = {
+  strategy: string
+  intention: string
+  response: string
+}
+
+export type AddMessageToConversationPayload = {
+  userId: Uuid
+  conversationId: Uuid
+  message: string
+  senderUserType: TutorBotSenderType
+  subjectName: string
+}
+
+export type TutorBotGeneratedMessage = TutorBotMessage & {
+  traceId: string
+  observationId: string | null
+  status: string
+}
+
+export type InsertTutorBotConversationPayload = {
+  userId: Uuid
+  subjectId: number
+  sessionId?: Uuid
+}
+
+export type InsertTutorBotConversationMessagePayload = {
+  conversationId: Uuid
+  userId: Uuid
+  senderUserType: TutorBotSenderType
+  message: string
+}
+
+export type TutorBotCreateConversationResult = {
+  conversationId: Uuid
+  userId: Uuid
+  sessionId?: Uuid
+  subjectId: number
+  messages: [TutorBotMessage, TutorBotGeneratedMessage]
+}
