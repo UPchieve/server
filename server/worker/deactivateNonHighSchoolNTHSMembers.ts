d13625d6a78d4190115b227a9609e9ccfc12a861
Jamie Kasulis <jamie.kasulis@upchieve.org>
2026-03-10 14:29:47 -0400

    Also email the user who got deactivated

--- server/worker/deactivateNonHighSchoolNTHSMembers.ts
+++ server/worker/deactivateNonHighSchoolNTHSMembers.ts
@@ -1,13 +1,13 @@
 import {
   getNonHighSchoolNTHSMembers,
   deactivateGroupMember,
-  getGroupAdminsContactInfo,
 } from '../models/NTHSGroups'
 import { getClient, runInTransaction } from '../db'
 import logger from '../logger'
 import { Ulid } from '../models/pgUtils'
 import QueueService from '../services/QueueService'
 import { Jobs } from './jobs'
+import { sendIndividualsDeactivationFromNTHSNotice } from '../services/MailService'
 
 const logPrefix = `NTHS Cleanup of Non-HS members: `
 export default async function () {
@@ -33,13 +33,18 @@ export default async function () {
         nthsGroupId: member.nthsGroupId,
         deactivatedUserId: member.userId,
       })
+      await sendIndividualsDeactivationFromNTHSNotice(
+        member.firstName,
+        member.email,
+        member.chapterName
+      )
 
       logger.info(
         {
           userId: member.userId,
           groupId: member.nthsGroupId,
         },
-        `${logPrefix}Deactivated a non-HS user and enqueued email to group admins`
+        `${logPrefix}Removed a non-HS user from NTHS`
       )
     }
   }, client)
