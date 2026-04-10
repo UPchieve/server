6392dcd288378a0965ae83b31597f7a7be523f00
Trey Stevens <treykstevens.tks@gmail.com>
2026-04-10 12:36:33 -0600

    add calendar contract

--- server/router/api/calendar.ts
+++ server/router/api/calendar.ts
@@ -1,25 +1,29 @@
 import { updateSchedule } from '../../controllers/CalendarCtrl'
 import { resError } from '../res-error'
 import { InputError } from '../../models/Errors'
-import { Router } from 'express'
+import type { Router, Response } from 'express'
 import { extractUser } from '../extract-user'
+import { SaveCalendarResponse } from '../../contracts/calendar'
 
 export function routeCalendar(router: Router): void {
-  router.post('/calendar/save', async function (req, res) {
-    try {
-      const user = extractUser(req)
-      if (!req.body.hasOwnProperty('availability'))
-        throw new InputError('No availability object specified')
-      await updateSchedule({
-        ...req.body,
-        user: user,
-        ip: req.ip,
-      })
-      res.json({
-        msg: 'Schedule saved',
-      })
-    } catch (err) {
-      resError(res, err)
+  router.post(
+    '/calendar/save',
+    async function (req, res: Response<SaveCalendarResponse>) {
+      try {
+        const user = extractUser(req)
+        if (!req.body.hasOwnProperty('availability'))
+          throw new InputError('No availability object specified')
+        await updateSchedule({
+          ...req.body,
+          user: user,
+          ip: req.ip,
+        })
+        res.json({
+          msg: 'Schedule saved',
+        })
+      } catch (err) {
+        resError(res, err)
+      }
     }
-  })
+  )
 }
