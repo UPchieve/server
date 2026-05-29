f4227b3258713106a1e0f852b5eb75333cb0a8b7
Jamie Kasulis <jamie.kasulis@upchieve.org>
2026-05-29 11:15:31 -0400

    Stub out email send

---
+++ server/worker/jobs/sendInvitationToCoachEmail.ts
@@ -0,0 +1,53 @@
+import { Ulid } from '../../types/shared'
+import { Job } from 'bull'
+import * as UserService from '../../services/UserService'
+import logger from '../../logger'
+import { sendInvitationToCoachEmail } from '../../services/MailService'
+
+interface SendInvitationToCoachEmailJobData {
+  invitingUserId: Ulid
+  invitedUserId: Ulid
+  personalization: any // @TODO: skills, personal note, etc
+}
+
+const logPrefix = `SendInvitationToCoachEmail job: `
+
+export default async function (
+  job: Job<SendInvitationToCoachEmailJobData>
+): Promise<any> {
+  const invitedUser = await UserService.getUserContactInfo(
+    job.data.invitedUserId
+  )
+  if (!invitedUser) {
+    logger.error(
+      {
+        invitedUserId: job.data.invitedUserId,
+        invitingUserId: job.data.invitingUserId,
+      },
+      `${logPrefix}Could not find contact info for invited user`
+    )
+    throw new Error('Could not find contact info for user invited to coach')
+  }
+
+  await sendInvitationToCoachEmail(invitedUser.email, job.data.personalization)
+  logger.info(
+    {
+      invitedUserId: job.data.invitedUserId,
+      invitingUserId: job.data.invitingUserId,
+    },
+    `${logPrefix}Sent invitation to coach email to invitee's primary email`
+  )
+  if (invitedUser.proxyEmail && invitedUser.proxyEmail !== invitedUser.email) {
+    await sendInvitationToCoachEmail(
+      invitedUser.proxyEmail,
+      job.data.personalization
+    )
+    logger.info(
+      {
+        invitedUserId: job.data.invitedUserId,
+        invitingUserId: job.data.invitingUserId,
+      },
+      `${logPrefix}Sent invitation to coach email to invitee's proxy email`
+    )
+  }
+}
